1875424f8b44bba00c30ef47f4e6e9d2fc7c1c7d
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2024-03-01 14:21:04 +0000

    fix(packages/event-client): add schema validation for all service bus messages (boas-1585)

--- packages/event-client/src/event-client.js
+++ packages/event-client/src/event-client.js
@@ -1,6 +1,8 @@
 import { LocalEventClient } from './local-event-client.js';
 import { ServiceBusEventClient } from './service-bus-event-client.js';
 
+const NSIP_S51_ADVICE = 'nsip-s51-advice';
+
 /**
  * @typedef {Function} InfoFunction
  * @param {string} content
@@ -19,4 +21,25 @@ export const getEventClient = (serviceBusEnabled, logger, serviceBusHostname = '
 	return serviceBusEnabled
 		? new ServiceBusEventClient(logger, serviceBusHostname)
 		: new LocalEventClient(logger);
+};
+
+/**
+ * gets the name of the schema matching the topic
+ *
+ * @param {*} topic
+ * @returns {string}
+ */
+export const getSchemaNameFromTopic = (topic) => {
+	let schemaName = '';
+
+	switch (topic) {
+		case NSIP_S51_ADVICE:
+			schemaName = 's51-advice.schema.json';
+			break;
+		default:
+			schemaName = topic + '.schema.json';
+			break;
+	}
+
+	return schemaName;
 };
