617b084482d2ab329972766ae35072d3ba196cb2
Alex Moulin <alex.moulin@planninginspectorate.gov.uk>
2024-04-12 11:14:02 +0100

    feat(web): resolve comments from James

--- appeals/web/src/server/appeals/appeal-details/service-user/service-user.router.js
+++ appeals/web/src/server/appeals/appeal-details/service-user/service-user.router.js
@@ -2,12 +2,17 @@ import { Router as createRouter } from 'express';
 import asyncRoute from '#lib/async-route.js';
 import * as controller from './service-user.controller.js';
 import * as validators from './service-user.validators.js';
+import { validateAppeal } from '../appeal-details.middleware.js';
 
 const router = createRouter({ mergeParams: true });
 
 router
 	.route('/change/:userType')
-	.get(asyncRoute(controller.getChangeServiceUser))
-	.post(validators.validateChangeServiceUser, asyncRoute(controller.postChangeServiceUser));
+	.get(validateAppeal, asyncRoute(controller.getChangeServiceUser))
+	.post(
+		validateAppeal,
+		validators.validateChangeServiceUser,
+		asyncRoute(controller.postChangeServiceUser)
+	);
 
 export default router;
