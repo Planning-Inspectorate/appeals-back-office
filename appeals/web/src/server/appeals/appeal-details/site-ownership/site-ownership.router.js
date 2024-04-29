e0eb73415f253c0a377548e4b7ef019b8aeef637
Alex Moulin <alex.moulin@planninginspectorate.gov.uk>
2024-04-29 16:00:04 +0100

    feat(appeals): add amendments as requested by PR review comments

--- appeals/web/src/server/appeals/appeal-details/site-ownership/site-ownership.router.js
+++ appeals/web/src/server/appeals/appeal-details/site-ownership/site-ownership.router.js
@@ -1,7 +1,6 @@
 import { Router as createRouter } from 'express';
 import asyncRoute from '#lib/async-route.js';
 import * as controllers from './site-ownership.controller.js';
-//import * as validators from './site-ownership.validator.js';
 import { validateAppeal } from '../appeal-details.middleware.js';
 
 const router = createRouter({ mergeParams: true });
