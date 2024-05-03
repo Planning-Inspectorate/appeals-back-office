7510c2866f5dd6f6d16942b2082a9a3b25d13df0
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-05-03 10:24:55 +0100

    feat(infrastructure): core resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,3 +4,45 @@ module "primary_region" {
 
   azure_region = local.primary_location
 }
+
+resource "azurerm_resource_group" "primary" {
+  name     = "${local.org}-rg-${local.resource_suffix}"
+  location = module.primary_region.location
+
+  tags = local.tags
+}
+
+resource "azurerm_key_vault" "main" {
+  #checkov:skip=CKV_AZURE_109: TODO: consider firewall settings, route traffic via VNet
+  name                        = "${local.org}-kv-${local.resource_suffix}"
+  location                    = module.primary_region.location
+  resource_group_name         = azurerm_resource_group.primary.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
+  enable_rbac_authorization   = true
+
+  sku_name = "standard"
+
+  tags = local.tags
+}
+
+# secrets to be manually populated
+resource "azurerm_key_vault_secret" "manual_secrets" {
+  #checkov:skip=CKV_AZURE_41: expiration not valid
+  for_each = toset(local.secrets)
+
+  key_vault_id = azurerm_key_vault.main.id
+  name         = each.value
+  value        = "<terraform_placeholder>"
+  content_type = "plaintext"
+
+  tags = local.tags
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+}
