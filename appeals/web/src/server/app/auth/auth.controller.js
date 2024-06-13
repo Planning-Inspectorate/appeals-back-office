983e6e190b0dbd60bc84dc21c028a25abe5f8e46
Gavin Lilley <Gavin.Lilley@planninginspectorate.gov.uk>
2024-06-13 09:41:24 +0100

    chore(deps): bump azure deps

--- appeals/web/src/server/app/auth/auth.controller.js
+++ appeals/web/src/server/app/auth/auth.controller.js
@@ -1,4 +1,4 @@
-import msalNode from '@azure/msal-node';
+import * as msalNode from '@azure/msal-node';
 import { promisify } from 'node:util';
 import config from '#environment/config.js';
 import pino from '#lib/logger.js';
