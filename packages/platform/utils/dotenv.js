bad03d7c77ba2bbdeef3ecd946fdbdfac0277b95
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-06-27 16:02:17 +0100

    test(api): fix debugging api tests

--- packages/platform/utils/dotenv.js
+++ packages/platform/utils/dotenv.js
@@ -4,16 +4,17 @@ import path from 'node:path';
 /**
  * Load .env into process.env
  *
- * @param {string} [environment='development']
+ * @param {string} [environment]
+ * @param {string} [rootDir] - directory to load .env files from
  * @returns {Record<string, string | undefined>}
  */
-export function loadEnvironment(environment = 'development') {
+export function loadEnvironment(environment, rootDir = process.cwd()) {
 	const isTest = environment === 'test';
 
 	// either load .env.test for tests, or just .env regardless of environment
 	const sourceFile = isTest ? '.env.test' : '.env';
 	// load into process.env (process.env takes precendence)
-	dotenv.config({ path: path.resolve(sourceFile) });
+	dotenv.config({ path: path.join(rootDir, sourceFile) });
 
 	return process.env;
 }
