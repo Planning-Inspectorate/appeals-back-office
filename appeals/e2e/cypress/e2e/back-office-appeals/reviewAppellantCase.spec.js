986bb6ddd8aa692eb87ac6aa71d658de6081f0a0
Peter Bridger <peter.bridger@kinandcarta.com>
2024-07-02 17:00:38 +0100

    feat(appeals): fix appellant e2e tests

--- appeals/e2e/cypress/e2e/back-office-appeals/reviewAppellantCase.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/reviewAppellantCase.spec.js
@@ -15,75 +15,84 @@ describe('Appeals feature', () => {
 	});
 
 	it('Validate appellant case', () => {
-		let dueDate = new Date(2024, 5, 10); // TODO Should this stay static or be dynamically calculated?
+		let dueDate = new Date();
 
-		cy.visit(urlPaths.appealsList);
-		appealsListPage.clickAppealFromList(10); // TODO Change to use page.clickAppealByRef(ref)
-		appealsListPage.clickReviewAppellantCase();
-		appealsListPage.selectRadioButtonByValue('Valid');
-		appealsListPage.clickButtonByText('Continue');
-		updateDueDatePage.enterValidDate(dueDate);
-		appealsListPage.clickButtonByText('Submit');
-		appealsListPage.clickLinkByText('Go back to case details');
-		const status = 'Valid';
-		const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
-		appealsListPage.verifyTableCellText(testData);
+		cy.createCase().then((caseRef) => {
+			cy.visit(urlPaths.appealsList);
+			appealsListPage.clickAppealByRef(caseRef);
+			appealsListPage.clickReviewAppellantCase();
+			appealsListPage.selectRadioButtonByValue('Valid');
+			appealsListPage.clickButtonByText('Continue');
+			updateDueDatePage.enterValidDate(dueDate);
+			appealsListPage.clickButtonByText('Confirm');
+			appealsListPage.clickLinkByText('Go back to case details');
+			const status = 'Valid';
+			const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
+			appealsListPage.verifyTableCellText(testData);
+		});
 	});
 
 	it('Invalidate appellant case', () => {
-		cy.visit(urlPaths.appealsList);
-		appealsListPage.clickAppealFromList(11); // TODO Change to use page.clickAppealByRef(ref)
-		appealsListPage.clickReviewAppellantCase();
-		appealsListPage.selectRadioButtonByValue('Invalid');
-		appealsListPage.clickButtonByText('Continue');
-		appealsListPage.chooseCheckboxByIndex(1);
-		appealsListPage.clickButtonByText('Continue');
-		appealsListPage.clickButtonByText('Confirm');
-		appealsListPage.clickLinkByText('Go back to case details');
-		const status = 'Invalid';
-		const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
-		appealsListPage.verifyTableCellText(testData);
+		cy.createCase().then((caseRef) => {
+			cy.visit(urlPaths.appealsList);
+			appealsListPage.clickAppealByRef(caseRef);
+			appealsListPage.clickReviewAppellantCase();
+			appealsListPage.selectRadioButtonByValue('Invalid');
+			appealsListPage.clickButtonByText('Continue');
+			appealsListPage.chooseCheckboxByIndex(1);
+			appealsListPage.clickButtonByText('Continue');
+			appealsListPage.clickButtonByText('Confirm');
+			appealsListPage.clickLinkByText('Go back to case details');
+			const status = 'Invalid';
+			const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
+			appealsListPage.verifyTableCellText(testData);
+		});
 	});
 
 	it('incomplete appellant case', () => {
-		let dueDate = new Date(2024, 11, 30); // TODO Should this stay static or be dynamically calculated?
+		let dueDate = new Date();
+		dueDate.setMonth(dueDate.getMonth() + 6);
 
-		cy.visit(urlPaths.appealsList);
-		appealsListPage.clickAppealFromList(12); // TODO Change to use page.clickAppealByRef(ref)
-		appealsListPage.clickReviewAppellantCase();
-		appealsListPage.selectRadioButtonByValue('Incomplete');
-		appealsListPage.clickButtonByText('Continue');
-		appealsListPage.chooseCheckboxByIndex(3);
-		appealsListPage.fillInput1('Hello here is some extra info, have a nice day 7384_+!£ =');
-		appealsListPage.clickButtonByText('Continue');
-		updateDueDatePage.enterValidDate(dueDate);
-		appealsListPage.clickButtonByText('Save and Continue');
-		appealsListPage.clickButtonByText('Confirm');
-		appealsListPage.clickLinkByText('Go back to case details');
-		const status = 'Incomplete';
-		const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
-		appealsListPage.verifyTableCellText(testData);
+		cy.createCase().then((caseRef) => {
+			cy.visit(urlPaths.appealsList);
+			appealsListPage.clickAppealByRef(caseRef);
+			appealsListPage.clickReviewAppellantCase();
+			appealsListPage.selectRadioButtonByValue('Incomplete');
+			appealsListPage.clickButtonByText('Continue');
+			appealsListPage.chooseCheckboxByIndex(3);
+			appealsListPage.fillInput('Hello here is some extra info, have a nice day 7384_+!£ =', 1);
+			appealsListPage.clickButtonByText('Continue');
+			updateDueDatePage.enterDate(dueDate);
+			appealsListPage.clickButtonByText('Save and Continue');
+			appealsListPage.clickButtonByText('Confirm');
+			appealsListPage.clickLinkByText('Go back to case details');
+			const status = 'Incomplete';
+			const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
+			appealsListPage.verifyTableCellText(testData);
+		});
 	});
 
 	it('incomplete appellant case reason: add another', () => {
 		let dueDate = new Date(2024, 11, 30); // TODO Should this stay static or be dynamically calculated?
 
-		cy.visit(urlPaths.appealsList);
-		appealsListPage.clickAppealFromList(15); // TODO Change to use page.clickAppealByRef(ref)
-		appealsListPage.clickReviewAppellantCase();
-		appealsListPage.selectRadioButtonByValue('Incomplete');
-		appealsListPage.clickButtonByText('Continue');
-		appealsListPage.chooseCheckboxByIndex(3);
-		appealsListPage.fillInput1('Hello here is some extra info, have a nice day 7384_+!£ =');
-		appealsListPage.addAnotherButton();
-		appealsListPage.fillInput2('Hello here is some extra info, have a nice day 7384_+!£ =');
-		appealsListPage.clickButtonByText('Continue');
-		updateDueDatePage.enterValidDate(dueDate);
-		appealsListPage.clickButtonByText('Save and Continue');
-		appealsListPage.clickButtonByText('Confirm');
-		appealsListPage.clickLinkByText('Go back to case details');
-		const status = 'Incomplete';
-		const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
-		appealsListPage.verifyTableCellText(testData);
+		cy.createCase().then((caseRef) => {
+			cy.visit(urlPaths.appealsList);
+			appealsListPage.clickAppealByRef(caseRef);
+			appealsListPage.clickReviewAppellantCase();
+			appealsListPage.selectRadioButtonByValue('Incomplete');
+			appealsListPage.clickButtonByText('Continue');
+			appealsListPage.chooseCheckboxByIndex(3);
+			appealsListPage.fillInput('Hello here is some extra info, have a nice day 7384_+!£ =', 1);
+			appealsListPage.addAnotherButton();
+			appealsListPage.fillInput('Hello here is some extra info, have a nice day 7384_+!£ =', 2);
+			appealsListPage.clickButtonByText('Continue');
+			updateDueDatePage.enterDate(dueDate);
+			appealsListPage.clickButtonByText('Save and Continue');
+			appealsListPage.clickButtonByText('Confirm');
+			appealsListPage.clickLinkByText('Go back to case details');
+			const status = 'Incomplete';
+			const testData = { rowIndex: 0, cellIndex: 0, textToMatch: status, strict: true };
+			appealsListPage.verifyTableCellText(testData);
+		});
 	});
 });
