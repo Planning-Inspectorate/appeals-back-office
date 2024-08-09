29b975a897ed5fbc5a16f2202cf376b218995645
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2024-08-09 10:01:55 +0100

    feat(appeals): adds an additional training LPA

--- appeals/api/src/database/seed/LPAs/training.js
+++ appeals/api/src/database/seed/LPAs/training.js
@@ -8,5 +8,10 @@ export const localPlanningDepartmentList = [
 		lpaCode: 'Q9999',
 		name: 'System Test Borough Council',
 		email: 'appealplanningdecisiontest@planninginspectorate.gov.uk'
+	},
+	{
+		lpaCode: 'Q9998',
+		name: 'System Test Borough Council 2',
+		email: 'example@example.com'
 	}
 ];
