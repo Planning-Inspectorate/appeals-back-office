4cf9700bf417624f26fd8b73bb28e43319c465be
Rachel Harvey <rachelharvey@rachel-harvey.lan>
2024-09-10 10:43:02 +0100

    test(appeals): added to issue desicion e2e tests

---
+++ appeals/e2e/cypress/e2e/back-office-appeals/issueDecision.spec.js
@@ -0,0 +1,43 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { CaseDetailsPage } from '../../page_objects/caseDetailsPage.js';
+import { ListCasesPage } from '../../page_objects/listCasesPage';
+import { DateTimeSection } from '../../page_objects/dateTimeSection';
+import { urlPaths } from '../../support/urlPaths.js';
+import { tag } from '../../support/tag';
+import { happyPathHelper } from '../../support/happyPathHelper.js';
+
+const listCasesPage = new ListCasesPage();
+const dateTimeSection = new DateTimeSection();
+const caseDetailsPage = new CaseDetailsPage();
+
+describe('Issue Decision', () => {
+	beforeEach(() => {
+		cy.login(users.appeals.caseAdmin);
+	});
+
+	it.skip('Issue Decision Allowed ', { tags: tag.smoke }, () => {
+		let futureDate = new Date();
+		futureDate.setDate(futureDate.getDate() + 28);
+
+		cy.createCase().then((caseRef) => {
+			cy.addLpaqSubmissionToCase(caseRef);
+			happyPathHelper.assignCaseOfficer(caseRef);
+			happyPathHelper.reviewAppellantCase(caseRef);
+			happyPathHelper.startCase(caseRef);
+			happyPathHelper.reviewLpaq(caseRef);
+			caseDetailsPage.clickIssueDecision(caseRef);
+			caseDetailsPage.selectRadioButtonByValue('Allowed');
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.uploadOneDocument();
+			caseDetailsPage.clickButtonByText('Continue');
+			dateTimeSection.enterDate(futureDate);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.fillInput();
+			caseDetailsPage.clickButtonByText('Send Decision');
+			caseDetailsPage.clickLinkByText('Go back to case details');
+		});
+	});
+});
