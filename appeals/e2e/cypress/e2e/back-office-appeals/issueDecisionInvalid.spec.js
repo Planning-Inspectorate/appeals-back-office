53925c9a433efce5f7572fa32bf177895e8a7e60
Jacob Mullaly <jakemullaly@gmail.com>
2024-09-10 15:52:00 +0100

    test(appeals): add test for issue invalid decision

---
+++ appeals/e2e/cypress/e2e/back-office-appeals/issueDecisionInvalid.spec.js
@@ -0,0 +1,42 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { CaseDetailsPage } from '../../page_objects/caseDetailsPage.js';
+import { ListCasesPage } from '../../page_objects/listCasesPage';
+import { DateTimeSection } from '../../page_objects/dateTimeSection';
+import { urlPaths } from '../../support/urlPaths.js';
+import { tag } from '../../support/tag';
+import { happyPathHelper } from '../../support/happyPathHelper.js';
+
+const listCasesPage = new ListCasesPage();
+const dateTimeSection = new DateTimeSection();
+const caseDetailsPage = new CaseDetailsPage();
+
+describe('Issue Decision', () => {
+	beforeEach(() => {
+		cy.login(users.appeals.caseAdmin);
+	});
+
+	it('Issue Decision is Invalid ', { tags: tag.smoke }, () => {
+		let futureDate = new Date();
+		futureDate.setDate(futureDate.getDate() + 28);
+
+		cy.createCase().then((caseRef) => {
+			cy.addLpaqSubmissionToCase(caseRef);
+			happyPathHelper.assignCaseOfficer(caseRef);
+			happyPathHelper.reviewAppellantCase(caseRef);
+			happyPathHelper.startCase(caseRef);
+			happyPathHelper.reviewLpaq(caseRef);
+			caseDetailsPage.clickIssueDecision(caseRef);
+			caseDetailsPage.selectRadioButtonByValue('Invalid');
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.fillTextArea('The appeal is invalid because of X reason', 0);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.chooseCheckboxByIndex(0);
+			caseDetailsPage.clickButtonByText('Send Decision');
+			caseDetailsPage.clickLinkByText('Go back to case details');
+			caseDetailsPage.checkStatusOfCase('Invalid', 0);
+		});
+	});
+});
