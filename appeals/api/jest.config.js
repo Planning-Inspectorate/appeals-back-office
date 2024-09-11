db18c4b9cfff451ce6208c877152be81540a4691
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-09-11 15:22:36 +0100

    chore(tooling): add code coverage thresholds

--- appeals/api/jest.config.js
+++ appeals/api/jest.config.js
@@ -1,5 +1,20 @@
 export default {
 	transform: {},
 	moduleNameMapper: { '^uuid$': 'uuid' },
-	setupFilesAfterEnv: ['<rootDir>/setup-tests.js']
+	setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
+	coverageThreshold: {
+		global: {
+			branches: 50,
+			functions: 65,
+			lines: 70,
+			statements: 70
+		},
+		// to improve and increase the coverage thresholds
+		'./src/server/endpoints/integrations': {
+			branches: 5.85,
+			functions: 10.86,
+			lines: 26.05,
+			statements: 25.51
+		}
+	}
 };
