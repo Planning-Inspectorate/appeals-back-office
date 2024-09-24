527b00bdfeaf1f9338d2ae1b70622f4c18f41a60
Rachel Harvey <rachel.harvey@kinandcarta.com>
2024-09-24 14:29:13 +0100

    test(appeals): e2e test for uplaoding an updated version of a doc
    e2e test for uplaoding an updated version of a doc on the appellant case

---
+++ appeals/e2e/cypress/e2e/back-office-appeals/manageDocsAppellantCase.spec.js
@@ -0,0 +1,37 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { CaseDetailsPage } from '../../page_objects/caseDetailsPage.js';
+import { ListCasesPage } from '../../page_objects/listCasesPage';
+import { DateTimeSection } from '../../page_objects/dateTimeSection';
+import { urlPaths } from '../../support/urlPaths.js';
+import { tag } from '../../support/tag';
+import { happyPathHelper } from '../../support/happyPathHelper.js';
+
+const listCasesPage = new ListCasesPage();
+const dateTimeSection = new DateTimeSection();
+const caseDetailsPage = new CaseDetailsPage();
+
+describe('manage docs on appellant case', () => {
+	beforeEach(() => {
+		cy.login(users.appeals.caseAdmin);
+	});
+
+	it('upload new version of document on appellant case', { tags: tag.smoke }, () => {
+		cy.createCase().then((caseRef) => {
+			happyPathHelper.uploadDocAppellantCase(caseRef);
+			cy.reload();
+			caseDetailsPage.clickManageAgreementToChangeDescriptionEvidence();
+			caseDetailsPage.clickLinkByText('View and edit');
+			//caseDetailsPage.clickEditOrViewAndEditDocuments();
+			//caseDetailsPage.clickRefreshVirusScanningBannerDocumentFile();
+			caseDetailsPage.clickButtonByText('upload a new version');
+			caseDetailsPage.uploadTestPdf();
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.validateBannerMessage('Document updated');
+		});
+	});
+});
