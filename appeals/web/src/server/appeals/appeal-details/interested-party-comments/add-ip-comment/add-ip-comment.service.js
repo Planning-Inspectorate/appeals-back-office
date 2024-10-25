982e913dacd0ad7201fb9ddfa19a7157d409f274
Chris Howarth <chris.howarth@kinandcarta.com>
2024-10-25 15:55:15 +0100

    feat(web): add ip comments api integration
    Implementing the api integration for the add ip comments journey
    
    A2-654

--- appeals/web/src/server/appeals/appeal-details/interested-party-comments/add-ip-comment/add-ip-comment.service.js
+++ appeals/web/src/server/appeals/appeal-details/interested-party-comments/add-ip-comment/add-ip-comment.service.js
@@ -16,3 +16,32 @@ export const getAttachmentsFolder = async (apiClient, appealId) => {
 
 	return folders[0];
 };
+
+/**
+ * @param {{ firstName: string, lastName: string, emailAddress: string, addressLine1: string, addressLine2: string, town: string, county: string, postCode: string, redactionStatus: boolean, 'date-day': string, 'date-month': string, 'date-year': string }} comment
+ * @param {import('got').Got} apiClient
+ * @param {number} appealId
+ * @returns {Promise<import('@pins/appeals.api').Appeals.FolderInfo>}
+ * */
+export const createIPComment = async (comment, apiClient, appealId) => {
+	const json = {
+		ipDetails: {
+			firstName: comment.firstName,
+			lastName: comment.lastName,
+			email: comment.emailAddress
+		},
+		ipAddress: {
+			addressLine1: comment.addressLine1,
+			addressLine2: comment.addressLine2,
+			town: comment.town,
+			county: comment.county,
+			postCode: comment.postCode
+		},
+		attachments: [],
+		redactionStatus: comment.redactionStatus
+	};
+
+	const IPcomment = await apiClient.post(`appeals/${appealId}/reps/comments`, { json }).json();
+
+	return IPcomment;
+};
