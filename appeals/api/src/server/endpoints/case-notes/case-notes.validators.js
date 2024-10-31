a7ce34199078fe8b9664bbe0b5eb23915c49437c
Biggs Thorarensen <biggs.thorarensen.st@planninginspectorate.gov.uk>
2024-10-31 09:16:02 +0000

    fix(appeals): case notes validation web and api validation aligned

--- appeals/api/src/server/endpoints/case-notes/case-notes.validators.js
+++ appeals/api/src/server/endpoints/case-notes/case-notes.validators.js
@@ -1,10 +1,10 @@
-import { validateStringParameter } from '#common/validators/string-parameter.js';
+import { validateTextAreaParameter } from '#common/validators/string-parameter.js';
 import { composeMiddleware } from '@pins/express';
 import validateIdParameter from '#common/validators/id-parameter.js';
 import { validationErrorHandler } from '#middleware/error-handler.js';
 
 export const postCaseNoteValidator = composeMiddleware(
 	validateIdParameter('appealId'),
-	validateStringParameter('comment'),
+	validateTextAreaParameter('comment', 300),
 	validationErrorHandler
 );
