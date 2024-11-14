6520c8d42b5e29ed4bd8f4c6a74a6bf2e2e59049
Rachel Harvey <rachel.harvey@kinandcarta.com>
2024-11-14 11:22:32 +0000

    test(appeals): added e2e tests for changing site visit from the case timetable and site section

---
+++ appeals/e2e/cypress/e2e/back-office-appeals/changeSiteVisit.spec.js
@@ -0,0 +1,86 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { DateTimeSection } from '../../page_objects/dateTimeSection';
+import { CaseDetailsPage } from '../../page_objects/caseDetailsPage';
+import { happyPathHelper } from '../../support/happyPathHelper';
+import { tag } from '../../support/tag';
+
+const dateTimeSection = new DateTimeSection();
+const caseDetailsPage = new CaseDetailsPage();
+
+describe('Change site visit', () => {
+	beforeEach(() => {
+		cy.login(users.appeals.caseAdmin);
+	});
+
+	it(`Change visit from Site details`, () => {
+		let visitDate = happyPathHelper.validVisitDate();
+
+		cy.createCase().then((caseRef) => {
+			happyPathHelper.assignCaseOfficer(caseRef);
+			happyPathHelper.reviewAppellantCase(caseRef);
+			happyPathHelper.startCase(caseRef);
+			caseDetailsPage.clickChangeVisitTypeHasSiteDetails();
+			caseDetailsPage.selectRadioButtonByValue('Unaccompanied');
+			dateTimeSection.enterVisitDate(visitDate);
+			dateTimeSection.enterVisitStartTime('08', '00');
+			dateTimeSection.enterVisitEndTime('12', '00');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.validateConfirmationPanelMessage(
+				'Site visit scheduled',
+				'Appeal reference ' + caseRef
+			);
+			caseDetailsPage.clickLinkByText('Go back to case details');
+			caseDetailsPage.validateAnswer('Visit Type', 'Unaccompanied');
+			caseDetailsPage.elements.changeSetVisitType().click();
+			caseDetailsPage.clickButtonByText('Manage the site visit');
+			caseDetailsPage.selectRadioButtonByValue('Access Required');
+			dateTimeSection.enterVisitDate(visitDate);
+			dateTimeSection.enterVisitStartTime('08', '00');
+			dateTimeSection.enterVisitEndTime('12', '00');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.validateConfirmationPanelMessage(
+				'Site visit scheduled',
+				'Appeal reference ' + caseRef
+			);
+			caseDetailsPage.clickLinkByText('Go back to case details');
+			caseDetailsPage.validateAnswer('Visit Type', 'Access required');
+		});
+	});
+
+	it(`Change visit from case timetable`, () => {
+		let visitDate = happyPathHelper.validVisitDate();
+
+		cy.createCase().then((caseRef) => {
+			happyPathHelper.assignCaseOfficer(caseRef);
+			happyPathHelper.reviewAppellantCase(caseRef);
+			happyPathHelper.startCase(caseRef);
+			caseDetailsPage.clickChangeVisitTypeHasSiteDetails();
+			caseDetailsPage.selectRadioButtonByValue('Access required');
+			dateTimeSection.enterVisitDate(visitDate);
+			dateTimeSection.enterVisitStartTime('08', '00');
+			dateTimeSection.enterVisitEndTime('12', '00');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.validateConfirmationPanelMessage(
+				'Site visit scheduled',
+				'Appeal reference ' + caseRef
+			);
+			caseDetailsPage.clickLinkByText('Go back to case details');
+			caseDetailsPage.validateAnswer('Visit Type', 'Access required');
+			caseDetailsPage.clickChangeVisitTypeHasCaseTimetable();
+			caseDetailsPage.selectRadioButtonByValue('Unaccompanied');
+			dateTimeSection.enterVisitDate(visitDate);
+			dateTimeSection.enterVisitStartTime('08', '00');
+			dateTimeSection.enterVisitEndTime('12', '00');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.validateConfirmationPanelMessage(
+				'Site visit scheduled',
+				'Appeal reference ' + caseRef
+			);
+			caseDetailsPage.clickLinkByText('Go back to case details');
+			caseDetailsPage.validateAnswer('Visit Type', 'Unaccompanied');
+		});
+	});
+});
