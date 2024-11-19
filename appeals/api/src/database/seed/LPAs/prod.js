f3b656202b8eb94e064a5a72d8810c519411be1e
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2024-11-19 11:51:07 +0000

    feat(api): adds 4 additional LPAs on prod

--- appeals/api/src/database/seed/LPAs/prod.js
+++ appeals/api/src/database/seed/LPAs/prod.js
@@ -13,5 +13,25 @@ export const localPlanningDepartmentList = [
 		lpaCode: 'N5090',
 		name: 'Barnet',
 		email: 'planning.appeals@barnet.gov.uk'
+	},
+	{
+		lpaCode: 'G5180',
+		name: 'Bromley',
+		email: 'planningappeals@bromley.gov.uk'
+	},
+	{
+		lpaCode: 'E5330',
+		name: 'Greenwich',
+		email: 'planning-enforcement@royalgreenwich.gov.uk'
+	},
+	{
+		lpaCode: 'B5480',
+		name: 'Havering',
+		email: 'planning.appeals@havering.gov.uk'
+	},
+	{
+		lpaCode: 'L5810',
+		name: 'Richmond',
+		email: 'planningappeals@richmond.gov.uk'
 	}
 ];
