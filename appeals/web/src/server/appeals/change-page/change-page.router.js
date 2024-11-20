f35b0e3fc5d7908649c5b261d090ef788929d10e
james-hill-kc <james.hill@kinandcarta.com>
2024-11-20 17:47:36 +0000

    refactor(web): remove obsolete generic change page code

--- appeals/web/src/server/appeals/change-page/change-page.router.js
+++
@@ -1,9 +0,0 @@
-import { Router as createRouter } from 'express';
-import { asyncHandler } from '@pins/express';
-import * as controller from './change-page.controller.js';
-
-const router = createRouter({ mergeParams: true });
-
-router.route('/:question').get(asyncHandler(controller.getChangePage));
-
-export default router;
