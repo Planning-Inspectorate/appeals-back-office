500188757f5652cc05608c84cbe8aa77ac39c157
Gavin Lilley <Gavin.Lilley@planninginspectorate.gov.uk>
2024-11-20 11:58:32 +0000

    fix(api): remove duplicate function

--- appeals/web/src/server/appeals/appeal-details/interested-party-comments/add-ip-comment/add-ip-comment.service.js
+++ appeals/web/src/server/appeals/appeal-details/interested-party-comments/add-ip-comment/add-ip-comment.service.js
@@ -5,22 +5,6 @@ export const DOCUMENT_TYPE = 'appellantStatement';
 /**
  * @param {import('got').Got} apiClient
  * @param {number} appealId
- * @returns {Promise<import('@pins/appeals.api').Appeals.FolderInfo>}
- * */
-export const getAttachmentsFolder = async (apiClient, appealId) => {
-	const folders = await apiClient
-		.get(`appeals/${appealId}/document-folders?path=${DOCUMENT_STAGE}/${DOCUMENT_TYPE}`)
-		.json();
-	if (!(folders && folders.length > 0)) {
-		throw new Error(`failed to find folder for appeal ID ${appealId}`);
-	}
-
-	return folders[0];
-};
-
-/**
- * @param {import('got').Got} apiClient
- * @param {number} appealId
  * @param {RepresentationRequest} payload
  * @returns {Promise<any>}
  */
