c57add2c89569ff6a20aacd157d03a872043f442
Rachel Harvey <rachel.harvey@kinandcarta.com>
2024-11-22 11:39:00 +0000

    test(appeals): added e2e test for unlinking a child appeal

---
+++ appeals/e2e/cypress/e2e/back-office-appeals/unlinkAppeals.spec.js
@@ -0,0 +1,41 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { CaseDetailsPage } from '../../page_objects/caseDetailsPage.js';
+import { ListCasesPage } from '../../page_objects/listCasesPage';
+import { urlPaths } from '../../support/urlPaths';
+import { tag } from '../../support/tag';
+import { happyPathHelper } from '../../support/happyPathHelper.js';
+
+const listCasesPage = new ListCasesPage();
+const caseDetailsPage = new CaseDetailsPage();
+
+describe('unlink appeals', () => {
+	beforeEach(() => {
+		cy.login(users.appeals.caseAdmin);
+	});
+
+	it('Unlink the only linked appeal from a child appeal', () => {
+		cy.createCase().then((caseRef) => {
+			cy.createCase().then((caseRefToLink) => {
+				happyPathHelper.assignCaseOfficer(caseRef);
+				caseDetailsPage.clickAccordionByButton('Overview');
+				caseDetailsPage.clickAddLinkedAppeal();
+				caseDetailsPage.fillInput(caseRefToLink);
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.selectRadioButtonByValue('Yes, make this the lead appeal for ');
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.validateBannerMessage('This appeal is now a child appeal of');
+				caseDetailsPage.checkStatusOfCase('Child', 1);
+				caseDetailsPage.clickManageLinkedAppeals();
+				caseDetailsPage.clickLinkByText('Unlink');
+				caseDetailsPage.selectRadioButtonByValue('Yes');
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.validateBannerMessage(
+					'You have unlinked this appeal from appeal  ' + caseRef
+				);
+			});
+		});
+	});
+});
