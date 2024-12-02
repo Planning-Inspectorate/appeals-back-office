2f085927901b292bf1ec7a0241949eabfd4d8f8f
Gavin Lilley <Gavin.Lilley@planninginspectorate.gov.uk>
2024-12-02 14:42:27 +0000

    feat(web): resolve code review comments (rebased)

--- appeals/web/src/server/appeals/appeal-details/interested-party-comments/view-and-review/view-and-review.controller.js
+++ appeals/web/src/server/appeals/appeal-details/interested-party-comments/view-and-review/view-and-review.controller.js
@@ -6,6 +6,16 @@ import {
 	viewInterestedPartyCommentPage
 } from './view-and-review.mapper.js';
 import { patchInterestedPartyCommentStatus } from './view-and-review.service.js';
+import {
+	postChangeDocumentDetails,
+	postChangeDocumentFileName,
+	postDeleteDocument,
+	renderChangeDocumentDetails,
+	renderChangeDocumentFileName,
+	renderDeleteDocument,
+	renderManageDocument,
+	renderManageFolder
+} from '#appeals/appeal-documents/appeal-documents.controller.js';
 
 /** @typedef {import("../../appeal-details.types.js").WebAppeal} Appeal */
 /** @typedef {import("../interested-party-comments.types.js").Representation} Representation */
@@ -87,4 +97,119 @@ export const postReviewInterestedPartyComment = async (request, response, next) 
 		logger.error(error);
 		return response.status(500).render('app/500.njk');
 	}
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getManageFolder = async (request, response) => {
+	const {
+		currentFolder,
+		params: { appealId, commentId }
+	} = request;
+
+	if (!currentFolder) {
+		return response.status(404).render('app/404');
+	}
+
+	await renderManageFolder({
+		request,
+		response,
+		backLinkUrl: `/appeals-service/appeal-details/${appealId}/interested-party-comments/${commentId}/review`,
+		viewAndEditUrl: `/appeals-service/appeal-details/${appealId}/interested-party-comments/${commentId}/manage-documents/{{folderId}}/{{documentId}}`,
+		pageHeadingTextOverride: 'Supporting documents'
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getManageDocument = async (request, response) => {
+	const {
+		params: { commentId }
+	} = request;
+	await renderManageDocument({
+		request,
+		response,
+		backLinkUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/{{folderId}}`,
+		uploadUpdatedDocumentUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/add-documents/{{folderId}}/{{documentId}}`,
+		removeDocumentUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/{{folderId}}/{{documentId}}/{{versionId}}/delete`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getChangeDocumentVersionDetails = async (request, response) => {
+	const {
+		params: { commentId }
+	} = request;
+
+	await renderChangeDocumentDetails({
+		request,
+		response,
+		backButtonUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/${request.params.folderId}/${request.params.documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postChangeDocumentVersionDetails = async (request, response) => {
+	const {
+		params: { commentId }
+	} = request;
+
+	await postChangeDocumentDetails({
+		request,
+		response,
+		backButtonUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/${request.params.folderId}/${request.params.documentId}`,
+		nextPageUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/${request.params.folderId}/${request.params.documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getChangeDocumentFileNameDetails = async (request, response) => {
+	const {
+		params: { commentId }
+	} = request;
+
+	await renderChangeDocumentFileName({
+		request,
+		response,
+		backButtonUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/${request.params.folderId}/${request.params.documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postChangeDocumentFileNameDetails = async (request, response) => {
+	const {
+		params: { commentId }
+	} = request;
+
+	await postChangeDocumentFileName({
+		request,
+		response,
+		backButtonUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/${request.params.folderId}/${request.params.documentId}`,
+		nextPageUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/${request.params.folderId}/${request.params.documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getDeleteDocument = async (request, response) => {
+	const {
+		params: { commentId }
+	} = request;
+
+	await renderDeleteDocument({
+		request,
+		response,
+		backButtonUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/{{folderId}}/{{documentId}}`
+	});
+};
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postDeleteDocumentPage = async (request, response) => {
+	const {
+		params: { commentId }
+	} = request;
+
+	await postDeleteDocument({
+		request,
+		response,
+		returnUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments`,
+		cancelUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/${commentId}/manage-documents/{{folderId}}/{{documentId}}`,
+		uploadNewDocumentUrl: `/appeals-service/appeal-details/${request.params.appealId}/interested-party-comments/add-documents/{{folderId}}`
+	});
 };
