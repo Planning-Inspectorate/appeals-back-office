0e25d6c01858c28331f65bcde5d7f6e611b4ef2d
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-12-04 12:53:40 +0000

    refactor(web): change calls to representations API to match new contract

--- appeals/web/src/server/appeals/appeal-details/interested-party-comments/add-ip-comment/add-ip-comment.service.js
+++ appeals/web/src/server/appeals/appeal-details/interested-party-comments/add-ip-comment/add-ip-comment.service.js
@@ -10,7 +10,7 @@ export const DOCUMENT_TYPE = 'appellantStatement';
  */
 export async function postRepresentationComment(apiClient, appealId, payload) {
 	try {
-		const response = await apiClient.post(`appeals/${appealId}/reps/comments`, {
+		const response = await apiClient.post(`appeals/${appealId}/reps?type=comments`, {
 			json: payload
 		});
 		return response.body;
