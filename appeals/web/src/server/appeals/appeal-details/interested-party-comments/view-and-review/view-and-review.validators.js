5a4e108add2761d8e243af4d0d63385d95e213b1
Chris Howarth <chris.howarth@kinandcarta.com>
2024-12-05 15:35:56 +0000

    fix(web): a2-1685 interested party copy updates

--- appeals/web/src/server/appeals/appeal-details/interested-party-comments/view-and-review/view-and-review.validators.js
+++ appeals/web/src/server/appeals/appeal-details/interested-party-comments/view-and-review/view-and-review.validators.js
@@ -5,6 +5,8 @@ import { body } from 'express-validator';
 export const validateReviewComment = createValidator(
 	body('status')
 		.notEmpty()
+		.withMessage('Select the outcome of your review')
+		.bail()
 		.isIn([COMMENT_STATUS.VALID, COMMENT_STATUS.INVALID, COMMENT_STATUS.VALID_REQUIRES_REDACTION])
-		.withMessage('Something went wrong')
+		.withMessage('Select the outcome of your review')
 );
