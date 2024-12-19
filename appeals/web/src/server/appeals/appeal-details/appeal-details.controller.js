8caf2a97d3583f2b71b1b8b13e8bfdd589dc768a
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-12-19 16:11:10 +0000

    refactor(web): improve readability when deciding which banners to show

--- appeals/web/src/server/appeals/appeal-details/appeal-details.controller.js
+++ appeals/web/src/server/appeals/appeal-details/appeal-details.controller.js
@@ -13,42 +13,44 @@ import { APPEAL_TYPE } from '@pins/appeals/constants/common.js';
  * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
  */
 export const viewAppealDetails = async (request, response) => {
-	const appealDetails = request.currentAppeal;
-	const session = request.session;
-	const { errors } = request;
+	const { errors, session, currentAppeal } = request;
+
+	if (!currentAppeal) {
+		return response.status(404).render('app/404.njk');
+	}
+
 	try {
-		if (!appealDetails) {
-			return response.status(404).render('app/404.njk');
-		}
-
 		/** @type {import('./accordions/index.js').RepresentationTypesAwaitingReview} */
-		const representationTypesAwaitingReview = {
-			ipComments: false,
-			appellantFinalComments: false,
-			lpaFinalComments: false
-		};
+    const representationTypesAwaitingReview = await (async () => {
+      if (currentAppeal.appealType !== APPEAL_TYPE.W) {
+        return {
+          ipComments: false,
+          appellantFinalComments: false,
+          lpaFinalComments: false,
+        };
+      }
 
-		if (appealDetails.appealType === APPEAL_TYPE.W) {
-			const counts = await getRepresentationCounts(
-				request.apiClient,
-				appealDetails.appealId.toString(),
-				APPEAL_REPRESENTATION_STATUS.AWAITING_REVIEW
-			);
-			representationTypesAwaitingReview.ipComments = !!counts[APPEAL_REPRESENTATION_TYPE.COMMENT];
-			representationTypesAwaitingReview.appellantFinalComments =
-				!!counts[APPEAL_REPRESENTATION_TYPE.APPELLANT_FINAL_COMMENT];
-			representationTypesAwaitingReview.lpaFinalComments =
-				!!counts[APPEAL_REPRESENTATION_TYPE.LPA_FINAL_COMMENT];
-		}
+      const counts = await getRepresentationCounts(
+        request.apiClient,
+        currentAppeal.appealId.toString(),
+        APPEAL_REPRESENTATION_STATUS.AWAITING_REVIEW
+      );
+
+      return {
+        ipComments: counts[APPEAL_REPRESENTATION_TYPE.COMMENT] > 0,
+        appellantFinalComments: counts[APPEAL_REPRESENTATION_TYPE.APPELLANT_FINAL_COMMENT] > 0,
+        lpaFinalComments: counts[APPEAL_REPRESENTATION_TYPE.LPA_FINAL_COMMENT] > 0,
+      };
+    })();
 
 		const appealCaseNotes = await getAppealCaseNotes(
 			request.apiClient,
-			appealDetails.appealId.toString()
+			currentAppeal.appealId.toString()
 		);
 
 		const currentUrl = request.originalUrl;
 		const mappedPageContent = await appealDetailsPage(
-			appealDetails,
+			currentAppeal,
 			appealCaseNotes,
 			currentUrl,
 			session,
