51e324dc5c488f617eb4a0b3e35f6cc6a41d3a2d
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-12-20 11:48:32 +0000

    fix(web): correct copy for LPAQ review errors

--- appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/has-protected-species/has-protected-species.validator.js
+++ appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/has-protected-species/has-protected-species.validator.js
@@ -2,5 +2,5 @@ import { createYesNoRadioValidator } from '#lib/validators/radio.validator.js';
 
 export const validateAffectsProtectedSpecies = createYesNoRadioValidator(
 	'protectedSpeciesRadio',
-	'Select yes if a protected species is affected'
+	'Select whether protected species are affected'
 );
