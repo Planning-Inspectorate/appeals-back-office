352892f07ab97d0f1150f59b6554722837fddaee
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-01-02 15:49:50 +0000

    chore(api): add compound unique constraint for rejection reasons

--- appeals/api/src/database/migrations/20241219115657_representation_rejection_types/migration.sql
+++ appeals/api/src/database/migrations/20241219115657_representation_rejection_types/migration.sql
@@ -5,6 +5,12 @@ BEGIN TRAN;
 -- AlterTable
 ALTER TABLE [dbo].[RepresentationRejectionReason] ADD [representationType] NVARCHAR(1000) NOT NULL CONSTRAINT [RepresentationRejectionReason_representationType_df] DEFAULT 'comment';
 
+-- DropIndex
+ALTER TABLE [dbo].[RepresentationRejectionReason] DROP CONSTRAINT [RepresentationRejectionReason_name_key];
+
+-- CreateIndex
+ALTER TABLE [dbo].[RepresentationRejectionReason] ADD CONSTRAINT [RepresentationRejectionReason_name_representationType_key] UNIQUE NONCLUSTERED ([name], [representationType]);
+
 COMMIT TRAN;
 
 END TRY
