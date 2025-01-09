a16ddcd430d554e8958a47bbd4939141e8530740
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-01-09 15:16:16 +0000

    feat(appeals): implement uploading eia documents (a2-1883)

---
+++ appeals/web/src/server/appeals/appeal-details/environmental-assessment/environmental-assessment.controller.js
@@ -0,0 +1,403 @@
+import {
+	postChangeDocumentDetails,
+	postChangeDocumentFileName,
+	postDeleteDocument,
+	postDocumentDetails,
+	postDocumentUpload,
+	postUploadDocumentsCheckAndConfirm,
+	postUploadDocumentVersionCheckAndConfirm,
+	renderChangeDocumentDetails,
+	renderChangeDocumentFileName,
+	renderDeleteDocument,
+	renderDocumentDetails,
+	renderDocumentUpload,
+	renderManageDocument,
+	renderManageFolder,
+	renderUploadDocumentsCheckAndConfirm
+} from '#appeals/appeal-documents/appeal-documents.controller.js';
+import { APPEAL_CASE_STAGE } from 'pins-data-model';
+import { addNotificationBannerToSession } from '#lib/session-utilities.js';
+import logger from '#lib/logger.js';
+
+/**
+ * @param {number|string} appealId
+ * @returns {`/appeals-service/appeal-details/${string}`}
+ */
+const appealUrl = (appealId) => `/appeals-service/appeal-details/${appealId}`;
+
+/**
+ * @param {number|string} appealId
+ * @returns {`/appeals-service/appeal-details/${string}`}
+ */
+const environmentalAssessmentUrl = (appealId) => `${appealUrl(appealId)}/environmental-assessment`;
+
+/** @type {import('@pins/express').RequestHandler<Response>}  */
+export const getDocumentUpload = async (request, response) => {
+	const { currentAppeal, currentFolder } = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	const uploadPageHeadingText = 'Upload environmental assessment documents';
+
+	await renderDocumentUpload({
+		request,
+		response,
+		appealDetails: currentAppeal,
+		backButtonUrl: appealUrl(currentAppeal.appealId),
+		nextPageUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/add-document-details/${
+			currentFolder.folderId
+		}`,
+		pageHeadingTextOverride: uploadPageHeadingText,
+		// TODO: APPEAL_DOCUMENT_TYPE.ENVIRONMENTAL_ASSESSMENT needs to be added to "pins-data-model"
+		// documentType: `${APPEAL_CASE_STAGE.APPELLANT_CASE}/${APPEAL_DOCUMENT_TYPE.ENVIRONMENTAL_ASSESSMENT}`
+		documentType: `${APPEAL_CASE_STAGE.APPELLANT_CASE}/environmentalAssessment`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postDocumentUploadPage = async (request, response) => {
+	const { currentAppeal, currentFolder } = request;
+
+	await postDocumentUpload({
+		request,
+		response,
+		nextPageUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/add-document-details/${
+			currentFolder.folderId
+		}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getDocumentVersionUpload = async (request, response) => {
+	const {
+		currentAppeal,
+		currentFolder,
+		params: { documentId }
+	} = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	await renderDocumentUpload({
+		request,
+		response,
+		appealDetails: currentAppeal,
+		backButtonUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/manage-documents/${
+			currentFolder.folderId
+		}/${documentId}`,
+		nextPageUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/add-document-details/${
+			currentFolder.folderId
+		}/${documentId}`,
+		allowMultipleFiles: false
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postDocumentVersionUpload = async (request, response) => {
+	const {
+		currentAppeal,
+		currentFolder,
+		params: { documentId }
+	} = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404');
+	}
+
+	await postDocumentUpload({
+		request,
+		response,
+		nextPageUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/add-document-details/${
+			currentFolder.folderId
+		}/${documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getAddDocumentDetails = async (request, response) => {
+	const {
+		currentAppeal,
+		currentFolder,
+		params: { documentId }
+	} = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	const pageHeadingText = 'Environmental assessment documents';
+
+	const documentIdFragment = documentId ? `/${documentId}` : '';
+
+	await renderDocumentDetails({
+		request,
+		response,
+		backLinkUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/upload-documents/${
+			currentFolder?.folderId
+		}${documentIdFragment}`,
+		pageHeadingTextOverride: pageHeadingText,
+		documentId
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postAddDocumentDetails = async (request, response) => {
+	const {
+		currentAppeal,
+		currentFolder,
+		params: { documentId }
+	} = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	const pageHeadingText = 'Environmental assessment documents';
+
+	const documentIdFragment = documentId ? `/${documentId}` : '';
+
+	await postDocumentDetails({
+		request,
+		response,
+		backLinkUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/upload-documents/${
+			currentFolder?.folderId
+		}`,
+		nextPageUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/check-your-answers/${
+			currentFolder?.folderId
+		}${documentIdFragment}`,
+		pageHeadingTextOverride: pageHeadingText
+	});
+};
+
+/**
+ *
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const getAddDocumentsCheckAndConfirm = async (request, response) => {
+	const {
+		currentAppeal,
+		currentFolder,
+		params: { documentId }
+	} = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404');
+	}
+
+	const addDocumentDetailsPageUrl = `${environmentalAssessmentUrl(
+		currentAppeal.appealId
+	)}/add-document-details/${currentFolder.folderId}${documentId ? `/${documentId}` : ''}`;
+
+	await renderUploadDocumentsCheckAndConfirm({
+		request,
+		response,
+		backLinkUrl: addDocumentDetailsPageUrl,
+		changeFileLinkUrl: `${environmentalAssessmentUrl(currentAppeal.appealId)}/upload-documents/${
+			currentFolder.folderId
+		}${documentId ? `/${documentId}` : ''}`,
+		changeDateLinkUrl: addDocumentDetailsPageUrl,
+		changeRedactionStatusLinkUrl: addDocumentDetailsPageUrl
+	});
+};
+
+/**
+ *
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const postAddDocumentsCheckAndConfirm = async (request, response) => {
+	const { currentAppeal, session } = request;
+
+	if (!currentAppeal) {
+		return response.status(404).render('app/404');
+	}
+
+	try {
+		await postUploadDocumentsCheckAndConfirm({
+			request,
+			response,
+			nextPageUrl: appealUrl(currentAppeal.appealId),
+			successCallback: () => {
+				addNotificationBannerToSession(
+					session,
+					'environmentalAssessmentDocumentAdded',
+					currentAppeal.appealId,
+					'',
+					`Environmental assessment documents uploaded`
+				);
+			}
+		});
+	} catch (error) {
+		logger.error(
+			error,
+			error instanceof Error
+				? error.message
+				: 'Something went wrong when adding environmental assessment documents'
+		);
+
+		return response.status(500).render('app/500.njk');
+	}
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postAddDocumentVersionCheckAndConfirm = async (request, response) => {
+	const { currentAppeal } = request;
+
+	if (!currentAppeal) {
+		return response.status(404).render('app/404');
+	}
+
+	try {
+		await postUploadDocumentVersionCheckAndConfirm({
+			request,
+			response,
+			nextPageUrl: appealUrl(currentAppeal.appealId)
+		});
+	} catch (error) {
+		logger.error(
+			error,
+			error instanceof Error
+				? error.message
+				: 'Something went wrong when adding environmental assessment document version'
+		);
+
+		return response.status(500).render('app/500.njk');
+	}
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>}  */
+export const getManageFolder = async (request, response) => {
+	const { currentAppeal, currentFolder } = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	const pageHeadingText = 'Environmental assessment documents';
+
+	await renderManageFolder({
+		request,
+		response,
+		backLinkUrl: appealUrl(request.params.appealId),
+		viewAndEditUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			currentFolder.folderId
+		}/{{documentId}}`,
+		pageHeadingTextOverride: pageHeadingText
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>}  */
+export const getManageDocument = async (request, response) => {
+	const { currentAppeal, currentFolder } = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	await renderManageDocument({
+		request,
+		response,
+		backLinkUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			currentFolder.folderId
+		}`,
+		uploadUpdatedDocumentUrl: `${environmentalAssessmentUrl(
+			currentAppeal.appealId
+		)}/upload-documents/${currentFolder?.folderId}/{{documentId}}`,
+		removeDocumentUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			currentFolder.folderId
+		}/{{documentId}}/{{versionId}}/delete`,
+		pageTitleTextOverride: 'Manage environmental assessment document'
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getDeleteCostsDocument = async (request, response) => {
+	const { currentFolder } = request;
+
+	if (!currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	await renderDeleteDocument({
+		request,
+		response,
+		backButtonUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			currentFolder.folderId
+		}/{{documentId}}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postDeleteCostsDocument = async (request, response) => {
+	const { currentAppeal, currentFolder } = request;
+
+	if (!currentAppeal || !currentFolder) {
+		return response.status(404).render('app/404.njk');
+	}
+
+	await postDeleteDocument({
+		request,
+		response,
+		returnUrl: appealUrl(request.params.appealId),
+		cancelUrl: `${environmentalAssessmentUrl(
+			request.params.appealId
+		)}/manage-documents/{{folderId}}/{{documentId}}`,
+		uploadNewDocumentUrl: `${environmentalAssessmentUrl(
+			currentAppeal.appealId
+		)}/upload-documents/{{folderId}}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getChangeDocumentFileNameDetails = async (request, response) => {
+	await renderChangeDocumentFileName({
+		request,
+		response,
+		backButtonUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			request.params.folderId
+		}/${request.params.documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postChangeDocumentFileNameDetails = async (request, response) => {
+	await postChangeDocumentFileName({
+		request,
+		response,
+		backButtonUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			request.params.folderId
+		}/${request.params.documentId}`,
+		nextPageUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			request.params.folderId
+		}/${request.params.documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const getChangeDocumentVersionDetails = async (request, response) => {
+	await renderChangeDocumentDetails({
+		request,
+		response,
+		backButtonUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			request.params.folderId
+		}/${request.params.documentId}`
+	});
+};
+
+/** @type {import('@pins/express').RequestHandler<Response>} */
+export const postChangeDocumentVersionDetails = async (request, response) => {
+	await postChangeDocumentDetails({
+		request,
+		response,
+		backButtonUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			request.params.folderId
+		}/${request.params.documentId}`,
+		nextPageUrl: `${environmentalAssessmentUrl(request.params.appealId)}/manage-documents/${
+			request.params.folderId
+		}/${request.params.documentId}`
+	});
+};
---
+++ appeals/web/src/server/appeals/appeal-details/environmental-assessment/environmental-assessment.router.js
@@ -0,0 +1,165 @@
+import { Router as createRouter } from 'express';
+import { assertUserHasPermission } from '#app/auth/auth.guards.js';
+import { permissionNames } from '#environment/permissions.js';
+import { asyncHandler } from '@pins/express';
+import { validateAppeal } from '#appeals/appeal-details/appeal-details.middleware.js';
+import {
+	validateCaseDocumentId,
+	validateCaseFolderId
+} from '#appeals/appeal-documents/appeal-documents.middleware.js';
+import * as controller from '#appeals/appeal-details/environmental-assessment/environmental-assessment.controller.js';
+import * as documentsValidators from '#appeals/appeal-documents/appeal-documents.validators.js';
+
+const router = createRouter({ mergeParams: true });
+
+router
+	.route(['/upload-documents/:folderId'])
+	.get(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.getDocumentUpload)
+	)
+	.post(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.postDocumentUploadPage)
+	);
+
+router
+	.route('/upload-documents/:folderId/:documentId')
+	.get(validateAppeal, validateCaseFolderId, asyncHandler(controller.getDocumentVersionUpload))
+	.post(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.postDocumentVersionUpload)
+	);
+
+router
+	.route(['/add-document-details/:folderId', '/add-document-details/:folderId/:documentId'])
+	.get(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.getAddDocumentDetails)
+	)
+	.post(
+		validateAppeal,
+		validateCaseFolderId,
+		documentsValidators.validateDocumentDetailsBodyFormat,
+		documentsValidators.validateDocumentDetailsReceivedDatesFields,
+		documentsValidators.validateDocumentDetailsReceivedDateValid,
+		documentsValidators.validateDocumentDetailsReceivedDateIsNotFutureDate,
+		documentsValidators.validateDocumentDetailsRedactionStatuses,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.postAddDocumentDetails)
+	);
+
+router
+	.route('/check-your-answers/:folderId')
+	.get(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.getAddDocumentsCheckAndConfirm)
+	)
+	.post(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.postAddDocumentsCheckAndConfirm)
+	);
+
+router
+	.route('/check-your-answers/:folderId/:documentId')
+	.get(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.getAddDocumentsCheckAndConfirm)
+	)
+	.post(
+		validateAppeal,
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.postAddDocumentVersionCheckAndConfirm)
+	);
+
+router
+	.route('/manage-documents/:folderId')
+	.get(validateCaseFolderId, asyncHandler(controller.getManageFolder));
+
+router
+	.route('/manage-documents/:folderId/:documentId')
+	.get(validateCaseFolderId, asyncHandler(controller.getManageDocument));
+
+router
+	.route('/manage-documents/:folderId/:documentId/:versionId/delete')
+	.get(
+		validateCaseFolderId,
+		validateCaseDocumentId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.getDeleteCostsDocument)
+	)
+	.post(
+		validateCaseFolderId,
+		validateCaseDocumentId,
+		assertUserHasPermission(permissionNames.updateCase),
+		documentsValidators.validateDocumentDeleteAnswer,
+		asyncHandler(controller.postDeleteCostsDocument)
+	);
+
+router
+	.route('/change-document-name/:folderId/:documentId')
+	.get(
+		validateAppeal,
+		assertUserHasPermission(permissionNames.updateCase),
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.getChangeDocumentFileNameDetails)
+	)
+	.post(
+		validateAppeal,
+		assertUserHasPermission(permissionNames.updateCase),
+		validateCaseFolderId,
+		documentsValidators.validateDocumentNameBodyFormat,
+		documentsValidators.validateDocumentName,
+		asyncHandler(controller.postChangeDocumentFileNameDetails)
+	);
+
+router
+	.route('/change-document-details/:folderId/:documentId')
+	.get(
+		validateAppeal,
+		assertUserHasPermission(permissionNames.updateCase),
+		validateCaseFolderId,
+		asyncHandler(controller.getChangeDocumentVersionDetails)
+	)
+	.post(
+		validateAppeal,
+		assertUserHasPermission(permissionNames.updateCase),
+		validateCaseFolderId,
+		documentsValidators.validateDocumentDetailsBodyFormat,
+		documentsValidators.validateDocumentDetailsReceivedDatesFields,
+		documentsValidators.validateDocumentDetailsReceivedDateValid,
+		documentsValidators.validateDocumentDetailsReceivedDateIsNotFutureDate,
+		documentsValidators.validateDocumentDetailsRedactionStatuses,
+		asyncHandler(controller.postChangeDocumentVersionDetails)
+	);
+
+router
+	.route('/check-and-confirm/:folderId/:documentId')
+	.get(
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.getAddDocumentsCheckAndConfirm)
+	)
+	.post(
+		validateCaseFolderId,
+		assertUserHasPermission(permissionNames.updateCase),
+		asyncHandler(controller.postAddDocumentVersionCheckAndConfirm)
+	);
+
+export default router;
