73b6831a44b8dc4236745f33451a27eda0b6b479
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-01-09 14:51:14 +0000

    feat(web): perform API request to redact and accept LPA statement

--- appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/redact/redact.controller.js
+++ appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/redact/redact.controller.js
@@ -1,5 +1,7 @@
+import { patchRepresentationRedaction } from '#lib/api/representation.api.js';
 import { render } from '../../common/render.js';
 import { redactLpaStatementPage, redactConfirmPage } from './redact.mapper.js';
+import { setRepresentationStatus } from '../../representations.service.js';
 
 export const renderRedact = render(redactLpaStatementPage, 'patterns/display-page.pattern.njk');
 
@@ -21,4 +23,31 @@ export function postRedact(request, response) {
 	return response.redirect(
 		`/appeals-service/appeal-details/${appealId}/lpa-statement/redact/confirm`
 	);
+}
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ * */
+export async function postConfirm(request, response) {
+	const {
+		params: { appealId },
+		session,
+		apiClient,
+		currentRepresentation
+	} = request;
+
+	await Promise.all([
+		patchRepresentationRedaction(
+			apiClient,
+			parseInt(appealId),
+			currentRepresentation.id,
+			session.redactedRepresentation
+		),
+		setRepresentationStatus(apiClient, parseInt(appealId), currentRepresentation.id, 'valid')
+	]);
+
+	delete session.redactedRepresentation;
+
+	return response.redirect(`/appeals-service/appeal-details/${appealId}/lpa-statement`);
 }
