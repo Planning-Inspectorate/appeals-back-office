cda4d7ae4295c80e3d87d02526439b1b5dee9792
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-01-09 14:51:14 +0000

    feat(web): perform API request to redact and accept LPA statement

--- appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/redact/redact.router.js
+++ appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/redact/redact.router.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { renderConfirm, renderRedact, postRedact } from './redact.controller.js';
+import { renderConfirm, renderRedact, postConfirm, postRedact } from './redact.controller.js';
 
 const router = Router({ mergeParams: true });
 
 router.route('/').get(renderRedact).post(postRedact);
 
-router.route('/confirm').get(renderConfirm);
+router.route('/confirm').get(renderConfirm).post(postConfirm);
 
 export default router;
