db58f44723532d1a69f782e618026df20d08a46b
Jacob Mullaly <jakemullaly@gmail.com>
2025-01-13 17:53:24 +0000

    test: add p3 tests for costs decision

--- appeals/e2e/cypress/e2e/back-office-appeals/addCostDecisionRedacted.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/addCostDecisionRedacted.spec.js
@@ -20,7 +20,7 @@ describe('add cost decision and redact', () => {
 
 	let sampleFiles = caseDetailsPage.sampleFiles;
 
-	it('add costs decsion and redact', { tags: tag.smoke }, () => {
+	/*it('add costs decsion and redact', { tags: tag.smoke }, () => {
 		cy.createCase().then((caseRef) => {
 			happyPathHelper.assignCaseOfficer(caseRef);
 			happyPathHelper.reviewAppellantCase(caseRef);
@@ -34,6 +34,64 @@ describe('add cost decision and redact', () => {
 			caseDetailsPage.checkEmailRelevantParties(0);
 			caseDetailsPage.clickButtonByText('Confirm');
 			caseDetailsPage.validateBannerMessage('Costs decision uploaded');
+		});
+	});
+
+	it('change the redaction status of a cost decsion', () => {
+		cy.createCase().then((caseRef) => {
+			happyPathHelper.assignCaseOfficer(caseRef);
+			happyPathHelper.reviewAppellantCase(caseRef);
+			happyPathHelper.startCase(caseRef);
+			caseDetailsPage.clickAccordionByButton('Costs');
+			caseDetailsPage.clickAddCostsDecision();
+			caseDetailsPage.uploadSampleFile(sampleFiles.document);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.selectRadioButtonByValue('Redacted');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.checkEmailRelevantParties(0);
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.clickManageDocsCostDecision();
+			cy.reloadUntilVirusCheckComplete();
+			caseDetailsPage.clickLinkByText('View and edit');
+			caseDetailsPage.clickChangeRedactionStatus();
+			caseDetailsPage.selectRadioButtonByValue('Unredacted');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.validateBannerMessage('Success', 'Document details updated');
+			caseDetailsPage.checkRedactionStatus('Unredacted');
+		});
+	});*/
+
+	it('Upload new version and change filename', () => {
+		cy.createCase().then((caseRef) => {
+			happyPathHelper.assignCaseOfficer(caseRef);
+			happyPathHelper.reviewAppellantCase(caseRef);
+			happyPathHelper.startCase(caseRef);
+			caseDetailsPage.clickAccordionByButton('Costs');
+			caseDetailsPage.clickAddCostsDecision();
+			caseDetailsPage.uploadSampleFile(sampleFiles.document);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.selectRadioButtonByValue('Redacted');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.checkEmailRelevantParties(0);
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.clickManageDocsCostDecision();
+			cy.reloadUntilVirusCheckComplete();
+			caseDetailsPage.clickLinkByText('View and edit');
+			caseDetailsPage.clickButtonByText('upload a new version');
+			caseDetailsPage.uploadSampleFile(sampleFiles.img);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.selectRadioButtonByValue('Unredacted');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.checkEmailRelevantParties(0);
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.clickManageDocsCostDecision();
+			cy.reloadUntilVirusCheckComplete();
+			caseDetailsPage.clickLinkByText('View and edit');
+			caseDetailsPage.checkDocVersionNumber('2');
+			caseDetailsPage.clickChangeFileName();
+			caseDetailsPage.updateFileName('sample-file123.img');
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.checkFileName('sample-file123.img');
 		});
 	});
 });
