70050bdf917cfb76003d3b53acbd48b4d53302db
Michael Turner <109961022+pinsMT@users.noreply.github.com>
2025-01-17 18:07:26 +0000

    feat(appeals): added unit tests for accept final comments flow

--- appeals/web/src/server/appeals/appeal-details/final-comments/accept/__tests__/accept-final-comments.test.js
+++ appeals/web/src/server/appeals/appeal-details/final-comments/accept/__tests__/accept-final-comments.test.js
@@ -21,7 +21,7 @@ describe('final-comments', () => {
 			.reply(200, {
 				...appealDataFullPlanning,
 				appealId: 2,
-				appealStatus: 'statements'
+				appealStatus: 'final_comments'
 			})
 			.persist();
 	});
@@ -41,14 +41,14 @@ describe('final-comments', () => {
 
 	describe('GET /', () => {
 		for (const finalCommentsType of finalCommentsTypes) {
-			it(`should render the redact ${finalCommentsType.type} final comments page with the expected content`, async () => {
+			it(`should render the accept ${finalCommentsType.type} final comments page with the expected content`, async () => {
 				nock('http://test/')
 					.get(`/appeals/2/reps?type=${finalCommentsType.type}_final_comment`)
 					.reply(200, finalCommentsForReview)
 					.persist();
 
 				const response = await request.get(
-					`${baseUrl}/2/final-comments/${finalCommentsType.type}/redact`
+					`${baseUrl}/2/final-comments/${finalCommentsType.type}/accept`
 				);
 
 				const element = parseHtml(response.text);
@@ -64,105 +64,41 @@ describe('final-comments', () => {
 					`<a href="/appeals-service/appeal-details/2/final-comments/${finalCommentsType.type}" class="govuk-back-link">Back</a>`
 				);
 				expect(unprettifiedHTML).toContain('Appeal 351062</span>');
-				expect(unprettifiedHTML).toContain(`Redact ${finalCommentsType.label} final comments</h1>`);
-				expect(unprettifiedHTML).toContain('Original final comments:</p>');
-				expect(unprettifiedHTML).toContain(
-					`id="original-comment" class="govuk-inset-text govuk-!-margin-top-2"><div class="pins-show-more" data-label="Read more" data-mode="text">Awaiting final comments review</div></div>`
-				);
-				expect(unprettifiedHTML).toContain('Awaiting final comments review</textarea>');
-				expect(unprettifiedHTML).toContain('Redact selected text</button>');
-				expect(unprettifiedHTML).toContain('Undo all changes</button>');
-				expect(unprettifiedHTML).toContain('Continue</button>');
+				expect(unprettifiedHTML).toContain(`Check details and accept ${finalCommentsType.label} final comments</h1>`);
+				expect(unprettifiedHTML).toContain('Original final comments</dt>');
+				expect(unprettifiedHTML).toContain('Awaiting final comments review</div>');
+				expect(unprettifiedHTML).toContain('Supporting documents</dt>');
+				expect(unprettifiedHTML).toContain('Review decisions</dt>');
+				expect(unprettifiedHTML).toContain('Accept final comments</dd>');
+				expect(unprettifiedHTML).toContain(`href="/appeals-service/appeal-details/2/final-comments/${finalCommentsType.type}"> Change</a>`);
+				expect(unprettifiedHTML).toContain(`Accept ${finalCommentsType.label} final comments</button>`);
 			});
+
 		}
 	});
 
 	describe('POST /', () => {
 		for (const finalCommentsType of finalCommentsTypes) {
-			it(`should redirect to the check your answers page`, async () => {
+			it(`should call the patch rep status endpoint and redirect to the case details page`, async () => {
 				nock('http://test/')
 					.get(`/appeals/2/reps?type=${finalCommentsType.type}_final_comment`)
 					.reply(200, finalCommentsForReview)
+					.persist();
+
+				const mockedPatchFinalCommentStatusEndpoint = nock('http://test/')
+					.patch(`/appeals/2/reps/3670/status`)
+					.reply(200, finalCommentsForReview.items[0])
 					.persist();
 
 				const response = await request
-					.post(`${baseUrl}/2/final-comments/${finalCommentsType.type}/redact`)
+					.post(`${baseUrl}/2/final-comments/${finalCommentsType.type}/accept`)
 					.send({
-						redactedRepresentation: 'Test redacted final comment text'
+						status: 'valid'
 					});
-
+				expect(mockedPatchFinalCommentStatusEndpoint.isDone()).toBe(true);
 				expect(response.statusCode).toBe(302);
 				expect(response.text).toBe(
-					`Found. Redirecting to /appeals-service/appeal-details/2/final-comments/${finalCommentsType.type}/redact/confirm`
-				);
-			});
-		}
-	});
-
-	describe('GET /confirm', () => {
-		for (const finalCommentsType of finalCommentsTypes) {
-			it(`should render a 500 error page if redactedRepresentation is not present in the session`, async () => {
-				nock('http://test/')
-					.get(`/appeals/2/reps?type=${finalCommentsType.type}_final_comment`)
-					.reply(200, finalCommentsForReview)
-					.persist();
-
-				const response = await request.get(
-					`${baseUrl}/2/final-comments/${finalCommentsType.type}/redact/confirm`
-				);
-
-				expect(response.statusCode).toBe(500);
-
-				const unprettifiedHTML = parseHtml(response.text, { skipPrettyPrint: true }).innerHTML;
-
-				expect(unprettifiedHTML).toContain('Sorry, there is a problem with the service</h1>');
-			});
-
-			it(`should render the check your answers page with the expected content if redactedRepresentation is present in the session`, async () => {
-				nock('http://test/')
-					.get(`/appeals/2/reps?type=${finalCommentsType.type}_final_comment`)
-					.reply(200, finalCommentsForReview)
-					.persist();
-
-				const redactedRepresentation = 'Test redacted final comment text';
-				const redactPagePostResponse = await request
-					.post(`${baseUrl}/2/final-comments/${finalCommentsType.type}/redact`)
-					.send({
-						redactedRepresentation
-					});
-
-				expect(redactPagePostResponse.statusCode).toBe(302);
-
-				const response = await request.get(
-					`${baseUrl}/2/final-comments/${finalCommentsType.type}/redact/confirm`
-				);
-
-				expect(response.statusCode).toBe(200);
-
-				const unprettifiedHTML = parseHtml(response.text, { skipPrettyPrint: true }).innerHTML;
-
-				expect(unprettifiedHTML).toContain(
-					`Check details and accept ${finalCommentsType.label} final comments</h1>`
-				);
-				expect(unprettifiedHTML).toContain('Original final comments</dt>');
-				expect(unprettifiedHTML).toContain(
-					'class="pins-show-more" data-label="Read more" data-mode="text">Awaiting final comments review</div>'
-				);
-				expect(unprettifiedHTML).toContain('Redacted final comments</dt>');
-				expect(unprettifiedHTML).toContain(
-					`class="pins-show-more" data-label="Read more" data-mode="text">${redactedRepresentation}</div>`
-				);
-				expect(unprettifiedHTML).toContain(
-					`href="/appeals-service/appeal-details/2/final-comments/${finalCommentsType.type}/redact"> Change</a></dd>`
-				);
-				expect(unprettifiedHTML).toContain('Supporting documents</dt>');
-				expect(unprettifiedHTML).toContain('Review decision</dt>');
-				expect(unprettifiedHTML).toContain('Redact and accept final comments</dd>');
-				expect(unprettifiedHTML).toContain(
-					`href="/appeals-service/appeal-details/2/final-comments/${finalCommentsType.type}"> Change</a></dd>`
-				);
-				expect(unprettifiedHTML).toContain(
-					`Accept ${finalCommentsType.label} final comments</button></form>`
+					`Found. Redirecting to /appeals-service/appeal-details/2`
 				);
 			});
 		}
