7412f5dabaf2708c1c9442eb3ccdcea8b7740ded
Rachel Harvey <rachel.harvey@kinandcarta.com>
2025-01-21 11:06:48 +0000

    test(appeals): amended final step of issue decision test to verify view decision letter is a link

--- appeals/e2e/cypress/e2e/back-office-appeals/issueDecision.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/issueDecision.spec.js
@@ -44,7 +44,7 @@ describe('Issue Decision', () => {
 				caseDetailsPage.clickLinkByText('Go back to case details');
 				caseDetailsPage.checkStatusOfCase('Complete', 0);
 				caseDetailsPage.checkDecisionOutcome(issueDecision);
-				caseDetailsPage.checkDecisionOutcome('View decision letter');
+				caseDetailsPage.viewDecisionLetter('View decision letter');
 			});
 		});
 	});
