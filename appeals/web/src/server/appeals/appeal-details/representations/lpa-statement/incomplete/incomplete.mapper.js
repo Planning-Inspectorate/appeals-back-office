0dd94d1b0ca474468c1fa96778d765d0bfab772b
Chris Howarth <chris.howarth@kinandcarta.com>
2025-01-23 16:41:38 +0000

    feat(web): lpa statments set new date page addition

--- appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/incomplete/incomplete.mapper.js
+++ appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/incomplete/incomplete.mapper.js
@@ -1,4 +1,5 @@
 import { appealShortReference } from '#lib/appeals-formatter.js';
+import { dateISOStringToDisplayDate, addBusinessDays } from '#lib/dates.js';
 
 /** @typedef {import("#appeals/appeal-details/appeal-details.types.js").WebAppeal} Appeal */
 
@@ -11,10 +12,53 @@ export function rejectLpaStatementPage(appealDetails) {
 
 	const pageContent = {
 		heading: 'Why is the statement incomplete?',
-		backLinkUrl: `/appeals-service/appeal-details/${appealDetails.appealId}/lpa-statement/incomplete/confirm`,
+		backLinkUrl: `/appeals-service/appeal-details/${appealDetails.appealId}/lpa-statement`,
 		preHeading: `Appeal ${shortReference}`,
 		hint: 'Select all that apply.',
 		headingClasses: 'govuk-heading-l'
+	};
+
+	return pageContent;
+}
+
+/**
+ * @param {import('got').Got} apiClient
+ * @param {Appeal} appealDetails
+ * @returns {Promise<PageContent>}
+ * */
+export async function setNewDatePage(apiClient, appealDetails) {
+	const extendedDeadline = await addBusinessDays(apiClient, new Date(), 7);
+	const deadlineString = dateISOStringToDisplayDate(extendedDeadline.toISOString());
+
+	/** @type {PageContent} */
+	const pageContent = {
+		backLinkUrl: `/appeals-service/appeal-details/${appealDetails.appealId}/lpa-statement/incomplete/reasons`,
+		heading: 'Do you want to allow the LPA to resubmit their statement?',
+		headingClasses: 'govuk-heading-l',
+		hint: `The LPA can resubmit their comments by ${deadlineString}`,
+		preHeading: `Appeal ${appealShortReference(appealDetails.appealReference)}`,
+		submitButtonProperties: {
+			text: 'Continue'
+		},
+		pageComponents: [
+			{
+				type: 'radios',
+				parameters: {
+					name: 'setNewDate',
+					idPrefix: 'set-new-date',
+					items: [
+						{
+							value: 'yes',
+							text: 'Yes'
+						},
+						{
+							value: 'no',
+							text: 'No'
+						}
+					]
+				}
+			}
+		]
 	};
 
 	return pageContent;
