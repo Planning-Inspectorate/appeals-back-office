2aef8428d8b1d3f039da3006da0c97c59daa1e32
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-02-07 14:07:21 +0000

    feat(web): render 'View' link once IP comments are published

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/ip-comments.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/ip-comments.mapper.js
@@ -2,13 +2,26 @@ import { mapDocumentStatus } from '#lib/display-page-formatter.js';
 import { documentationFolderTableItem } from '#lib/mappers/index.js';
 
 /** @type {import('../mapper.js').SubMapper} */
-export const mapIpComments = ({ appealDetails, currentRoute }) =>
-	documentationFolderTableItem({
+export const mapIpComments = ({ appealDetails, currentRoute }) => {
+	const actionText = (() => {
+		const { status, counts } = appealDetails?.documentationSummary?.ipComments ?? {};
+
+		if (status !== 'received') {
+			return 'Add';
+		}
+
+		if (counts?.published === 0) {
+			return 'Review';
+		}
+
+		return 'View';
+	})();
+
+	return documentationFolderTableItem({
 		id: 'ip-comments',
 		text: 'Interested party comments',
 		statusText: mapDocumentStatus(appealDetails?.documentationSummary?.ipComments?.status),
 		receivedText: 'Not applicable',
-		actionHtml: `<a href="${currentRoute}/interested-party-comments" data-cy="review-ip-comments" class="govuk-link">${
-			appealDetails?.documentationSummary?.ipComments?.status === 'received' ? 'Review' : 'Add'
-		} <span class="govuk-visually-hidden">Interested party comments</span></a>`
+		actionHtml: `<a href="${currentRoute}/interested-party-comments" data-cy="review-ip-comments" class="govuk-link">${actionText}<span class="govuk-visually-hidden"> interested party comments</span></a>`
 	});
+};
