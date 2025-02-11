9fee6c6ac4b7ce34ad306c9eb76e321b6565dbe5
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-02-11 10:54:29 +0000

    refactor(web): move share final comments code inline with share LPA
    Currently there are separate implementations for LPA statements/comments
    and final comments. These can be brought under a single implementation.

--- appeals/web/src/server/appeals/appeal-details/progress-case/progress-case.router.js
+++
@@ -1,9 +0,0 @@
-import { Router as createRouter } from 'express';
-import { validateAppeal } from '#appeals/appeal-details/appeal-details.middleware.js';
-import fromFinalCommentsRouter from './from-final-comments/from-final-comments.router.js';
-
-const router = createRouter({ mergeParams: true });
-
-router.use('/from-final-comments', validateAppeal, fromFinalCommentsRouter);
-
-export default router;
