c6d338ab0393dd4826d2732ac2c0991f8cd07bf4
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2025-02-13 12:02:44 +0000

    feat(api): exposes `affectsListedBuilding` boolean in `listedBuildingsDetails`

--- appeals/api/src/server/utils/format-listed-building.js
+++ appeals/api/src/server/utils/format-listed-building.js
@@ -2,14 +2,15 @@
 /** @typedef {import('@pins/appeals.api').Api.ListedBuilding} ListedBuilding */
 
 /**
- * @param {boolean} affectsListedBuilding
  * @param {ListedBuildingDetails[] | null | undefined} values
  * @returns {ListedBuilding[] | null}
  */
-export const formatListedBuildingDetails = (affectsListedBuilding, values) =>
+export const formatListedBuildingDetails = (values) =>
 	// @ts-ignore
 	(values &&
-		values
-			.filter((value) => value.affectsListedBuilding === affectsListedBuilding)
-			.map(({ listEntry, id }) => ({ id, listEntry }))) ||
+		values.map(({ listEntry, id, affectsListedBuilding }) => ({
+			id,
+			listEntry,
+			affectsListedBuilding
+		}))) ||
 	null;
