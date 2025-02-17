d4838bc61b4873a784684b31c7cd7cc45394f1e8
Lucy Potter <si3rr4wow@gmail.com>
2025-02-17 15:54:53 +0000

    fix(web): update final comment copy

--- appeals/web/src/server/appeals/appeal-details/final-comments/reject/reject.validators.js
+++ appeals/web/src/server/appeals/appeal-details/final-comments/reject/reject.validators.js
@@ -1,9 +1,20 @@
 import { createCheckboxTextItemsValidator } from '#lib/validators/checkbox-text-items.validator.js';
 import { createValidator } from '@pins/express';
 import { body } from 'express-validator';
+import { formatFinalCommentsTypeText } from '../view-and-review/view-and-review.mapper.js';
 
-export const validateRejectReason = createValidator(
-	body('rejectionReason').exists().withMessage('Select why you are rejecting the comment')
-);
+/**
+ * @param {string} finalCommentsType
+ */
+export const validateRejectReason = (finalCommentsType) =>
+	createValidator(
+		body('rejectionReason')
+			.exists()
+			.withMessage(
+				`Select why you are rejecting the ${formatFinalCommentsTypeText(
+					finalCommentsType
+				)}'s final comment`
+			)
+	);
 
 export const validateRejectionReasonTextItems = createCheckboxTextItemsValidator('rejectionReason');
