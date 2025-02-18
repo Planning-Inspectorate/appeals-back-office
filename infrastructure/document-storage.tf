288327fbf43ffc133384657bc0af9d0b4fdbb494
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2025-02-18 11:15:49 +0000

    chore(tooling): adding importing and moving malware scanning resource block

--- infrastructure/document-storage.tf
+++ infrastructure/document-storage.tf
@@ -63,6 +63,14 @@ resource "azurerm_eventgrid_topic" "document_scan_results" {
   }
 }
 
+removed {
+  from = azurerm_resource_group_template_deployment.document_storage_malware_scanning_settings
+
+  lifecycle {
+    destroy = false
+  }
+}
+
 import {
   to = azurerm_security_center_storage_defender.malware_scanning
   id = azurerm_storage_account.documents.id
