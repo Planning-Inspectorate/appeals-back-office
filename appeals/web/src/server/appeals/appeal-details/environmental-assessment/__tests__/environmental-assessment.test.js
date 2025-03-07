35979385b2815122aedd774ed43f3759858e12a7
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-03-07 15:07:23 +0000

    chore(appeals): increase unit test coverage for environmental assessment uploads (a2-2554)

--- appeals/web/src/server/appeals/appeal-details/environmental-assessment/__tests__/environmental-assessment.test.js
+++ appeals/web/src/server/appeals/appeal-details/environmental-assessment/__tests__/environmental-assessment.test.js
@@ -4,6 +4,8 @@ import { createTestEnvironment } from '#testing/index.js';
 import {
 	activeDirectoryUsersData,
 	appealData,
+	documentFileInfo,
+	documentFileVersionsInfo,
 	fileUploadInfo
 } from '#testing/app/fixtures/referencedata.js';
 import { parseHtml } from '@pins/platform';
@@ -17,6 +19,7 @@ const request = supertest(app);
 const baseUrl = '/appeals-service/appeal-details';
 const appealId = appealData.appealId.toString();
 const folderId = folderInfoResponse.folderId;
+const documentId = 111;
 
 describe('environmental-assessment', () => {
 	beforeEach(() => {
@@ -30,8 +33,20 @@ describe('environmental-assessment', () => {
 		nock('http://test/').get(`/appeals/${appealId}`).reply(200, appealData).persist();
 		nock('http://test/').post(`/appeals/${appealId}/documents`).reply(200, {}).persist();
 		nock('http://test/')
+			.get(`/appeals/${appealId}/documents/${documentId}`)
+			.reply(200, documentFileInfo)
+			.persist();
+		nock('http://test/')
+			.post(`/appeals/${appealId}/documents/${documentId}`)
+			.reply(200, {})
+			.persist();
+		nock('http://test/')
 			.get(`/appeals/${appealId}/document-folders/${folderId}`)
 			.reply(200, folderInfoResponse)
+			.persist();
+		nock('http://test/')
+			.get(`/appeals/${appealId}/documents/${documentId}/versions`)
+			.reply(200, documentFileVersionsInfo)
 			.persist();
 		nock('http://test/')
 			.get('/appeals/document-redaction-statuses')
@@ -41,6 +56,19 @@ describe('environmental-assessment', () => {
 
 	afterEach(() => {
 		teardown();
+	});
+
+	describe('GET /environmental-assessment/upload-documents/:folderId', () => {
+		it('should render the upload documents page', async () => {
+			const response = await request.get(
+				`${baseUrl}/${appealId}/environmental-assessment/upload-documents/${folderId}`
+			);
+			expect(response.statusCode).toBe(200);
+
+			const headingElement = parseHtml(response.text, { rootElement: '.govuk-main-wrapper' });
+			expect(headingElement.innerHTML).toMatchSnapshot();
+			expect(headingElement.innerHTML).toContain('Upload environmental assessment documents</h1>');
+		});
 	});
 
 	describe('POST /environmental-assessment/upload-documents/:folderId', () => {
@@ -57,12 +85,26 @@ describe('environmental-assessment', () => {
 				`Found. Redirecting to ${baseUrl}/${appealId}/environmental-assessment/check-your-answers/${folderId}`
 			);
 
-			const checkYourAnswersResponse = await request
+			const checkYourAnswersResponse = await request.get(
+				`${baseUrl}/${appealId}/environmental-assessment/check-your-answers/${folderId}`
+			);
+
+			expect(checkYourAnswersResponse.statusCode).toBe(200);
+
+			const headingElement = parseHtml(checkYourAnswersResponse.text, {
+				rootElement: '.govuk-main-wrapper'
+			});
+			expect(headingElement.innerHTML).toMatchSnapshot();
+			expect(headingElement.innerHTML).toContain('Check your answers</h1>');
+
+			const postCheckYourAnswersResponse = await request
 				.post(`${baseUrl}/${appealId}/environmental-assessment/check-your-answers/${folderId}`)
 				.send({});
 
-			expect(checkYourAnswersResponse.statusCode).toBe(302);
-			expect(checkYourAnswersResponse.text).toBe(`Found. Redirecting to ${baseUrl}/${appealId}`);
+			expect(postCheckYourAnswersResponse.statusCode).toBe(302);
+			expect(postCheckYourAnswersResponse.text).toBe(
+				`Found. Redirecting to ${baseUrl}/${appealId}`
+			);
 
 			const caseDetailsResponse = await request.get(`${baseUrl}/${appealId}`);
 
@@ -74,6 +116,99 @@ describe('environmental-assessment', () => {
 			expect(element.innerHTML).toMatchSnapshot();
 			expect(element.innerHTML).toContain('Success</h3>');
 			expect(element.innerHTML).toContain('Environmental assessment documents uploaded</p>');
+		});
+	});
+
+	describe('GET /environmental-assessment/upload-documents/:folderId/:documentId', () => {
+		it('should render the upload documents page', async () => {
+			const response = await request.get(
+				`${baseUrl}/${appealId}/environmental-assessment/upload-documents/${folderId}/${documentId}`
+			);
+			expect(response.statusCode).toBe(200);
+
+			const headingElement = parseHtml(response.text, { rootElement: '.govuk-main-wrapper' });
+			expect(headingElement.innerHTML).toMatchSnapshot();
+			expect(headingElement.innerHTML).toContain('Upload an updated document</h1>');
+		});
+	});
+
+	describe('POST /environmental-assessment/upload-documents/:folderId/:documentId', () => {
+		it(`should eventually redirect to case details page displaying the success banner`, async () => {
+			const addDocumentsResponse = await request
+				.post(
+					`${baseUrl}/${appealId}/environmental-assessment/upload-documents/${folderId}/${documentId}`
+				)
+				.send({
+					'upload-info': fileUploadInfo
+				});
+
+			expect(addDocumentsResponse.statusCode).toBe(302);
+
+			expect(addDocumentsResponse.text).toBe(
+				`Found. Redirecting to ${baseUrl}/${appealId}/environmental-assessment/check-your-answers/${folderId}/${documentId}`
+			);
+
+			const checkYourAnswersResponse = await request.get(
+				`${baseUrl}/${appealId}/environmental-assessment/check-your-answers/${folderId}/${documentId}`
+			);
+
+			expect(checkYourAnswersResponse.statusCode).toBe(200);
+
+			const headingElement = parseHtml(checkYourAnswersResponse.text, {
+				rootElement: '.govuk-main-wrapper'
+			});
+			expect(headingElement.innerHTML).toMatchSnapshot();
+			expect(headingElement.innerHTML).toContain('Check your answers</h1>');
+
+			const postCheckYourAnswersResponse = await request
+				.post(
+					`${baseUrl}/${appealId}/environmental-assessment/check-your-answers/${folderId}/${documentId}`
+				)
+				.send({});
+
+			expect(postCheckYourAnswersResponse.statusCode).toBe(302);
+			expect(postCheckYourAnswersResponse.text).toBe(
+				`Found. Redirecting to ${baseUrl}/${appealId}`
+			);
+
+			const caseDetailsResponse = await request.get(`${baseUrl}/${appealId}`);
+
+			expect(caseDetailsResponse.statusCode).toBe(200);
+
+			const element = parseHtml(caseDetailsResponse.text, {
+				rootElement: '.govuk-notification-banner'
+			});
+			expect(element.innerHTML).toMatchSnapshot();
+			expect(element.innerHTML).toContain('Success</h3>');
+			expect(element.innerHTML).toContain('Document updated</p>');
+		});
+	});
+
+	describe('GET /environmental-assessment/manage-documents/:folderId/', () => {
+		it('should render the manage documents page', async () => {
+			const response = await request.get(
+				`${baseUrl}/${appealId}/environmental-assessment/manage-documents/${folderId}`
+			);
+			expect(response.statusCode).toBe(200);
+
+			const headingElement = parseHtml(response.text, { rootElement: '.govuk-main-wrapper' });
+			expect(headingElement.innerHTML).toMatchSnapshot();
+			expect(headingElement.innerHTML).toContain('Manage folder</span>');
+			expect(headingElement.innerHTML).toContain('Environmental assessment documents</h1>');
+		});
+	});
+
+	describe('GET /environmental-assessment/manage-documents/:folderId/:documentId', () => {
+		it('should render the manage documents page', async () => {
+			const response = await request.get(
+				`${baseUrl}/${appealId}/environmental-assessment/manage-documents/${folderId}/${documentId}`
+			);
+			expect(response.statusCode).toBe(200);
+
+			const headingElement = parseHtml(response.text, { rootElement: '.govuk-main-wrapper' });
+			expect(headingElement.innerHTML).toMatchSnapshot();
+			expect(headingElement.innerHTML).toContain('Manage environmental assessment document</span>');
+			expect(headingElement.innerHTML).toContain('View and remove versions of this document</p>');
 		});
 	});
 });
