0fcce3f0a06663dcc44947a9e1f3f2888e193b6b
Lucy Potter <si3rr4wow@gmail.com>
2025-03-11 17:56:13 +0000

    fix(api): fix moar api types

---
+++ appeals/api/swagger-autogen.d.ts
@@ -0,0 +1,11 @@
+type Generate = (specFile: string, endpointsFiles: string[], spec: unknown) => false | {
+    success: boolean;
+    data: any;
+}
+
+declare module 'swagger-autogen' {
+	export default function Init(options: {
+		openapi:string;
+		disableLogs: boolean;
+	}): Generate
+}
