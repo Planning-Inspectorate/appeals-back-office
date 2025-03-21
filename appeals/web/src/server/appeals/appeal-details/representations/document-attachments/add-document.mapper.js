d9bc9e6ac44be81f03b387c5b8c8170eb734b222
Dave Ferguson <dave.ferguson.g3@planninginspectorate.gov.uk>
2025-03-21 14:00:59 +0000

    fix(web): default the document submitted date to today

--- appeals/web/src/server/appeals/appeal-details/representations/document-attachments/add-document.mapper.js
+++ appeals/web/src/server/appeals/appeal-details/representations/document-attachments/add-document.mapper.js
@@ -1,4 +1,5 @@
 import { appealShortReference } from '#lib/appeals-formatter.js';
+import { dateISOStringToDayMonthYearHourMinute } from '#lib/dates.js';
 import { dateInput } from '#lib/mappers/index.js';
 
 /** @typedef {import("../../appeal-details.types.js").WebAppeal} Appeal */
@@ -20,7 +21,10 @@ export const dateSubmitted = (appealDetails, errors, date, backLinkUrl) => ({
 		dateInput({
 			id: 'date',
 			name: 'date',
-			value: date,
+			value:
+				date.day && date.month && date.year
+					? date
+					: dateISOStringToDayMonthYearHourMinute(new Date().toISOString()),
 			legendText: 'When was the supporting document submitted?',
 			legendIsPageHeading: true,
 			hint: 'For example, 27 3 2024'
