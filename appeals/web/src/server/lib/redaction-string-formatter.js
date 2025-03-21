251824c161d55c3d1bd883b343fdfe1ff9c496c2
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-03-21 10:42:45 +0000

    feat(web): amending the highlightin method to reduce number of mark tags rather than each char

--- appeals/web/src/server/lib/redaction-string-formatter.js
+++ appeals/web/src/server/lib/redaction-string-formatter.js
@@ -14,9 +14,33 @@ export function addInvisibleSpacesAfterRedactionCharacters(redactedString) {
  * @param {string} originalString
  * @returns {string}
  */
-export function highlightRedactedSections(redactedString, originalString) {
-	return redactedString
-		.split('')
-		.map((char, index) => (char === '█' ? `<mark>${originalString[index]}</mark>` : char))
-		.join('');
-}
+export const highlightRedactedSections = (redactedString, originalString) => {
+	let result = '';
+	let inMarkTag = false;
+	let markContent = '';
+
+	for (let i = 0; i < redactedString.length; i++) {
+		if (redactedString[i] === '█') {
+			if (!inMarkTag) {
+				inMarkTag = true;
+				markContent = originalString[i];
+			} else {
+				markContent += originalString[i];
+			}
+		} else {
+			if (inMarkTag) {
+				result += `<mark>${markContent}</mark>`;
+				inMarkTag = false;
+				markContent = '';
+			}
+			result += redactedString[i];
+		}
+	}
+
+	// Close any remaining open <mark> tag
+	if (inMarkTag) {
+		result += `<mark>${markContent}</mark>`;
+	}
+
+	return result;
+};
