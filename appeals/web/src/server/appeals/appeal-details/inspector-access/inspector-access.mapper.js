3174b4a581b9c14d99e12b16b12100c9a1c3db13
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-03-31 12:17:02 +0100

    refactor(web): refactoring the inspector access question bc shared across lpaq and appellant case

--- appeals/web/src/server/appeals/appeal-details/inspector-access/inspector-access.mapper.js
+++ appeals/web/src/server/appeals/appeal-details/inspector-access/inspector-access.mapper.js
@@ -15,7 +15,8 @@ import { yesNoInput } from '#lib/mappers/index.js';
 export const changeInspectorAccessPage = (appealData, storedSessionData, origin, source) => {
 	const shortAppealReference = appealShortReference(appealData.appealReference);
 
-	const sourceKey = source === 'lpa' ? 'lpaQuestionnaire' : 'appellantCase';
+	const isSourceLpa = source === 'lpa';
+	const sourceKey = isSourceLpa ? 'lpaQuestionnaire' : 'appellantCase';
 
 	const currentRadioValue =
 		storedSessionData?.radio ?? appealData.inspectorAccess[sourceKey].isRequired ?? '';
@@ -24,19 +25,23 @@ export const changeInspectorAccessPage = (appealData, storedSessionData, origin,
 
 	/** @type {PageContent} */
 	const pageContent = {
-		title: `Will an inspector need to access your land or property?`,
+		title: isSourceLpa
+			? 'Might the inspector need access to the appellant’s land or property?'
+			: 'Will an inspector need to access your land or property?',
 		backLinkUrl: origin,
 		preHeading: `Appeal ${shortAppealReference}`,
 		pageComponents: [
 			yesNoInput({
 				name: 'inspectorAccessRadio',
 				value: currentRadioValue,
-				legendText: `Will an inspector need to access your land or property?`,
+				legendText: isSourceLpa
+					? 'Might the inspector need access to the appellant’s land or property?'
+					: 'Will an inspector need to access your land or property?',
 				legendIsPageHeading: true,
 				yesConditional: {
 					id: 'inspector-access-details',
 					name: 'inspectorAccessDetails',
-					hint: `Enter reason`,
+					hint: isSourceLpa ? 'Enter the reason' : 'Enter reason',
 					details: currentDetailsValue
 				}
 			})
