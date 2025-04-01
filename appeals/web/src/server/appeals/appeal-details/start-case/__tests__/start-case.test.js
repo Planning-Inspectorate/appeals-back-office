f27b5432a8c82671026a006fdeb7b4cf73e36d64
james-hill-kc <james.hill@kinandcarta.com>
2025-04-01 15:16:51 +0100

    feat(web): add s78 start case select procedure type page and entry points

--- appeals/web/src/server/appeals/appeal-details/start-case/__tests__/start-case.test.js
+++ appeals/web/src/server/appeals/appeal-details/start-case/__tests__/start-case.test.js
@@ -4,6 +4,7 @@ import supertest from 'supertest';
 import { createTestEnvironment } from '#testing/index.js';
 import { appealData } from '#testing/appeals/appeals.js';
 import { dateISOStringToDisplayDate } from '#lib/dates.js';
+import featureFlags from '#common/feature-flags.js';
 
 const { app, teardown } = createTestEnvironment();
 const request = supertest(app);
@@ -17,10 +18,18 @@ describe('start-case', () => {
 	afterEach(teardown);
 
 	describe('GET /start-case/add', () => {
-		it('should render the start case page with the expected content', async () => {
-			nock('http://test/').get('/appeals/1').reply(200, appealDataWithoutStartDate);
+		it('should render the start case page with the expected content if the appeal type is Householder', async () => {
+			nock('http://test/')
+				.get('/appeals/1')
+				.reply(200, {
+					...appealDataWithoutStartDate,
+					appealType: 'Householder'
+				});
 
 			const response = await request.get(`${baseUrl}/1/start-case/add`);
+
+			expect(response.statusCode).toBe(200);
+
 			const element = parseHtml(response.text);
 
 			expect(element.innerHTML).toContain('Start case</h1>');
@@ -33,6 +42,70 @@ describe('start-case', () => {
 				)}. Start letters will be sent to the relevant parties.`
 			);
 			expect(unprettifiedElement.innerHTML).toContain('Confirm</button>');
+		});
+
+		it('should render the start case page with the expected content if the appeal type is S78 and the S78 hearing feature flag is disabled', async () => {
+			featureFlags.isFeatureActive = () => false;
+
+			nock('http://test/')
+				.get('/appeals/1')
+				.reply(200, {
+					...appealDataWithoutStartDate,
+					appealType: 'Planning appeal'
+				});
+
+			const response = await request.get(`${baseUrl}/1/start-case/add`);
+
+			expect(response.statusCode).toBe(200);
+
+			const element = parseHtml(response.text);
+
+			expect(element.innerHTML).toContain('Start case</h1>');
+
+			const unprettifiedElement = parseHtml(response.text, { skipPrettyPrint: true });
+
+			expect(unprettifiedElement.innerHTML).toContain(
+				`Warning</span> Confirming will activate the timetable on ${dateISOStringToDisplayDate(
+					new Date().toISOString()
+				)}. Start letters will be sent to the relevant parties.`
+			);
+			expect(unprettifiedElement.innerHTML).toContain('Confirm</button>');
+		});
+
+		it('should redirect to the select procedure page if the appeal type is S78 and the S78 hearing feature flag is enabled', async () => {
+			featureFlags.isFeatureActive = () => true;
+
+			nock('http://test/')
+				.get('/appeals/1')
+				.reply(200, {
+					...appealDataWithoutStartDate,
+					appealType: 'Planning appeal'
+				});
+
+			const response = await request.get(`${baseUrl}/1/start-case/add`);
+
+			expect(response.statusCode).toBe(302);
+			expect(response.text).toBe(
+				'Found. Redirecting to /appeals-service/appeal-details/1/start-case/select-procedure'
+			);
+		});
+
+		it('should redirect to the select procedure page if the appeal type is S78 and the S78 hearing feature flag is enabled, passing the backLink query parameter if provided', async () => {
+			featureFlags.isFeatureActive = () => true;
+
+			nock('http://test/')
+				.get('/appeals/1')
+				.reply(200, {
+					...appealDataWithoutStartDate,
+					appealType: 'Planning appeal'
+				});
+
+			const response = await request.get(`${baseUrl}/1/start-case/add?backUrl=/test/back/url`);
+
+			expect(response.statusCode).toBe(302);
+			expect(response.text).toBe(
+				'Found. Redirecting to /appeals-service/appeal-details/1/start-case/select-procedure?backUrl=/test/back/url'
+			);
 		});
 	});
 
@@ -77,6 +150,82 @@ describe('start-case', () => {
 
 			expect(response.statusCode).toBe(302);
 			expect(response.text).toBe('Found. Redirecting to /appeals-service/appeal-details/1');
+		});
+	});
+
+	describe('GET /start-case/select-procedure', () => {
+		it('should render the select procedure page with the expected content', async () => {
+			nock('http://test/')
+				.get('/appeals/1')
+				.reply(200, {
+					...appealDataWithoutStartDate,
+					appealType: 'Planning appeal'
+				});
+
+			const response = await request.get(
+				'/appeals-service/appeal-details/1/start-case/select-procedure'
+			);
+
+			expect(response.statusCode).toBe(200);
+
+			const unprettifiedElement = parseHtml(response.text, {
+				rootElement: 'body',
+				skipPrettyPrint: true
+			});
+
+			expect(unprettifiedElement.innerHTML).toContain('href="/" class="govuk-back-link">Back</a>');
+			expect(unprettifiedElement.innerHTML).toContain(
+				'<span class="govuk-caption-l">Appeal 351062 - start case</span>'
+			);
+			expect(unprettifiedElement.innerHTML).toContain('Appeal procedure</h1>');
+			expect(unprettifiedElement.innerHTML).toContain(
+				'name="appealProcedure" type="radio" value="written">'
+			);
+			expect(unprettifiedElement.innerHTML).toContain(
+				'name="appealProcedure" type="radio" value="hearing">'
+			);
+			expect(unprettifiedElement.innerHTML).toContain(
+				'name="appealProcedure" type="radio" value="inquiry">'
+			);
+			expect(unprettifiedElement.innerHTML).toContain('Continue</button>');
+		});
+
+		it('should render the select procedure page with the expected back link URL, if the backLink query parameter was passed', async () => {
+			nock('http://test/')
+				.get('/appeals/1')
+				.reply(200, {
+					...appealDataWithoutStartDate,
+					appealType: 'Planning appeal'
+				});
+
+			const response = await request.get(
+				'/appeals-service/appeal-details/1/start-case/select-procedure?backUrl=/test/back/url'
+			);
+
+			expect(response.statusCode).toBe(200);
+
+			const unprettifiedElement = parseHtml(response.text, {
+				rootElement: 'body',
+				skipPrettyPrint: true
+			});
+
+			expect(unprettifiedElement.innerHTML).toContain(
+				'href="/test/back/url" class="govuk-back-link">Back</a>'
+			);
+			expect(unprettifiedElement.innerHTML).toContain(
+				'<span class="govuk-caption-l">Appeal 351062 - start case</span>'
+			);
+			expect(unprettifiedElement.innerHTML).toContain('Appeal procedure</h1>');
+			expect(unprettifiedElement.innerHTML).toContain(
+				'name="appealProcedure" type="radio" value="written">'
+			);
+			expect(unprettifiedElement.innerHTML).toContain(
+				'name="appealProcedure" type="radio" value="hearing">'
+			);
+			expect(unprettifiedElement.innerHTML).toContain(
+				'name="appealProcedure" type="radio" value="inquiry">'
+			);
+			expect(unprettifiedElement.innerHTML).toContain('Continue</button>');
 		});
 	});
 });
