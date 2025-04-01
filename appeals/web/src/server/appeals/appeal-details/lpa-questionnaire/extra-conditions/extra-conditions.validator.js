ea0c0d540956115972866271136bbe626e79637d
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-04-01 15:46:45 +0100

    feat(web): updating char limit for new conditions question and amending content

--- appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/extra-conditions/extra-conditions.validator.js
+++ appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/extra-conditions/extra-conditions.validator.js
@@ -2,15 +2,15 @@ import { createValidator } from '@pins/express';
 import { body } from 'express-validator';
 import { textInputCharacterLimits } from '#appeals/appeal.constants.js';
 
-const charLimitForTextarea = textInputCharacterLimits.defaultTextareaLength;
+const charLimitForTextarea = textInputCharacterLimits.expandedTextareaLength;
 
 export const validateChangeExtraConditions = createValidator(
 	body('extraConditionsDetails')
 		.if(body('extraConditionsRadio').equals('yes'))
 		.trim()
 		.isLength({ min: 1 })
-		.withMessage('Enter extra conditions details')
+		.withMessage('Enter new conditions details')
 		.bail()
 		.isLength({ max: charLimitForTextarea })
-		.withMessage(`Extra conditions details must be ${charLimitForTextarea} characters or less`)
+		.withMessage(`New conditions must be ${charLimitForTextarea} characters or less`)
 );
