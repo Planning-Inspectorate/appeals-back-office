083057f6389ea74c73e98b4f8797ec2a55bd87c1
Callum-T-Dev <callum.taylor.2r@planninginspectorate.gov.uk>
2025-04-02 10:57:13 +0100

    fix(web): amending supporting documents section to say No documents instead of Not provided

--- appeals/web/src/server/appeals/appeal-details/representations/interested-party-comments/view-and-review/page-components/common.js
+++ appeals/web/src/server/appeals/appeal-details/representations/interested-party-comments/view-and-review/page-components/common.js
@@ -136,7 +136,7 @@ export function generateCommentSummaryList(
 			: []),
 		{
 			key: { text: 'Supporting documents' },
-			value: attachmentsList ? { html: attachmentsList } : { text: 'Not provided' },
+			value: attachmentsList ? { html: attachmentsList } : { text: 'No documents' },
 			actions: {
 				items: [
 					...(filteredAttachments?.length
