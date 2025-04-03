0e806e0f0971c1322fb0702f1774df90c6ad8112
james-hill-kc <james.hill@kinandcarta.com>
2025-04-03 12:14:35 +0100

    fix(web): quick fix for inability to start s78 cases due to select procedure page

--- appeals/web/src/server/appeals/appeal-details/start-case/start-case.router.js
+++ appeals/web/src/server/appeals/appeal-details/start-case/start-case.router.js
@@ -1,6 +1,7 @@
 import { Router as createRouter } from 'express';
 import * as controller from './start-case.controller.js';
 import { asyncHandler } from '@pins/express';
+import { saveBodyToSession } from '#lib/middleware/save-body-to-session.js';
 
 const router = createRouter({ mergeParams: true });
 
@@ -14,6 +15,12 @@ router
 	.get(asyncHandler(controller.getChangeDate))
 	.post(asyncHandler(controller.postChangeDate));
 
-router.route('/select-procedure').get(asyncHandler(controller.getSelectProcedure));
+router
+	.route('/select-procedure')
+	.get(asyncHandler(controller.getSelectProcedure))
+	.post(
+		saveBodyToSession('startCaseAppealProcedure', { scopeToAppeal: true }),
+		asyncHandler(controller.postSelectProcedure)
+	);
 
 export default router;
