41c2183e2925b87c0358f1e8b60d4d5298c2da47
james-hill-kc <james.hill@kinandcarta.com>
2025-04-07 15:44:51 +0100

    feat(appeals): hearing check details and start case page

--- appeals/web/src/server/appeals/appeal-details/start-case/start-case.router.js
+++ appeals/web/src/server/appeals/appeal-details/start-case/start-case.router.js
@@ -2,6 +2,7 @@ import { Router as createRouter } from 'express';
 import * as controller from './start-case.controller.js';
 import { asyncHandler } from '@pins/express';
 import { saveBodyToSession } from '#lib/middleware/save-body-to-session.js';
+import * as validators from './start-case.validators.js';
 
 const router = createRouter({ mergeParams: true });
 
@@ -19,8 +20,14 @@ router
 	.route('/select-procedure')
 	.get(asyncHandler(controller.getSelectProcedure))
 	.post(
+		validators.validateAppealProcedure,
 		saveBodyToSession('startCaseAppealProcedure', { scopeToAppeal: true }),
 		asyncHandler(controller.postSelectProcedure)
 	);
+
+router
+	.route('/select-procedure/check-and-confirm')
+	.get(asyncHandler(controller.getConfirmProcedure))
+	.post(asyncHandler(controller.postConfirmProcedure));
 
 export default router;
---
+++ appeals/web/src/server/appeals/appeal-details/start-case/start-case.validators.js
@@ -0,0 +1,12 @@
+import { createValidator } from '@pins/express';
+import { body } from 'express-validator';
+import { APPEAL_CASE_PROCEDURE } from 'pins-data-model';
+
+export const validateAppealProcedure = createValidator(
+	body('appealProcedure')
+		.exists()
+		.withMessage('Select an appeal procedure')
+		.bail()
+		.isIn(Object.values(APPEAL_CASE_PROCEDURE))
+		.withMessage('Select an appeal procedure')
+);
