cf0ed6d4ff71d001924a43e8a48c785c5db05204
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-04-08 11:30:07 +0100

    fix(web): case overview copy updates

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/lpa-reference.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/lpa-reference.mapper.js
@@ -4,7 +4,7 @@ import { textSummaryListItem } from '#lib/mappers/index.js';
 export const mapLpaReference = ({ appealDetails, currentRoute, userHasUpdateCasePermission }) =>
 	textSummaryListItem({
 		id: 'lpa-reference',
-		text: 'LPA application reference',
+		text: 'Planning application reference',
 		value:
 			appealDetails.planningApplicationReference || 'No LPA application reference for this appeal',
 		link: `${currentRoute}/lpa-reference/change`,
