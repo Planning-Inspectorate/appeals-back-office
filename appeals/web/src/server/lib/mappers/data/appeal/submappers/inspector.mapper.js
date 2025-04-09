a4e57278e77cc625d51aaeba02312eaf7d511839
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-04-09 12:08:32 +0100

    fix(web): team accordion copy updates

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/inspector.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/inspector.mapper.js
@@ -1,22 +1,27 @@
 import { surnameFirstToFullName } from '#lib/person-name-formatter.js';
 import { textSummaryListItem } from '#lib/mappers/index.js';
 
-/** @type {import('../mapper.js').SubMapper} */ export const mapInspector = ({
+/** @type {import('../mapper.js').SubMapper} */
+export const mapInspector = ({
 	appealDetails,
 	currentRoute,
 	skipAssignedUsersData,
 	inspectorUser,
 	userHasUpdateCasePermission
 }) => {
-	let inspectorRowValue = '';
+	const inspectorRowValue = (() => {
+		if (skipAssignedUsersData) {
+			return '';
+		}
 
-	if (inspectorUser && !skipAssignedUsersData) {
-		inspectorRowValue = inspectorUser
-			? `<ul class="govuk-list"><li>${surnameFirstToFullName(inspectorUser?.name)}</li><li>${
-					inspectorUser?.email
-			  }</li></ul>`
-			: '<p class="govuk-body">An inspector is assigned but their user account was not found</p>';
-	}
+		if (!inspectorUser) {
+			return '<p class="govuk-body">Not assigned</p>';
+		}
+
+		return `<ul class="govuk-list"><li>${surnameFirstToFullName(inspectorUser.name)}</li><li>${
+			inspectorUser.email
+		}</li></ul>`;
+	})();
 
 	return textSummaryListItem({
 		id: 'inspector',
