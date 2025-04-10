52ea8b2f2e2f077b5169ac720b2ac0062c9168b7
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-04-10 15:19:14 +0100

    fix(web): showing supporting docs for ip comments redact flow and refactor

--- appeals/web/src/server/appeals/appeal-details/representations/interested-party-comments/redact/components/summary-list.js
+++ appeals/web/src/server/appeals/appeal-details/representations/interested-party-comments/redact/components/summary-list.js
@@ -1,41 +1,75 @@
 /** @typedef {import("#appeals/appeal-details/appeal-details.types.js").WebAppeal} Appeal */
 /** @typedef {import("#appeals/appeal-details/representations/types.js").Representation} Representation */
 
+import { getAttachmentList } from '#appeals/appeal-details/representations/common/document-attachment-list.js';
+
 /**
  * @param {Appeal} appealDetails
  * @param {Representation} comment
  * @param {import('express-session').Session & Record<string, string>} [session]
  * @returns {PageComponent}
  */
-export const summaryList = (appealDetails, comment, session) => ({
-	type: 'summary-list',
-	wrapperHtml: {
-		opening: '<div class="govuk-grid-row"><div class="govuk-grid-column-full">',
-		closing: '</div></div>'
-	},
-	parameters: {
-		rows: [
-			{
-				key: { text: 'Interested party' },
-				value: { text: comment.author }
-			},
-			{
-				key: { text: 'Original comment' },
-				value: { text: comment.originalRepresentation }
-			},
-			{
-				key: { text: 'Redacted comment' },
-				value: { text: session?.redactedRepresentation },
-				actions: {
-					items: [
-						{
-							text: 'Change',
-							href: `/appeals-service/appeal-details/${appealDetails.appealId}/interested-party-comments/${comment.id}/redact`,
-							visuallyHiddenText: 'redacted comment'
-						}
-					]
+export const summaryList = (appealDetails, comment, session) => {
+	const attachmentsList = getAttachmentList(comment);
+
+	const folderId = comment.attachments?.[0]?.documentVersion?.document?.folderId ?? null;
+	const baseUrl = `/appeals-service/appeal-details/${appealDetails.appealId}/interested-party-comments/${comment.id}`;
+
+	/** @type {PageComponent} */
+	const pageComponents = {
+		type: 'summary-list',
+		wrapperHtml: {
+			opening: '<div class="govuk-grid-row"><div class="govuk-grid-column-full">',
+			closing: '</div></div>'
+		},
+		parameters: {
+			rows: [
+				{
+					key: { text: 'Interested party' },
+					value: { text: comment.author }
+				},
+				{
+					key: { text: 'Original comment' },
+					value: { text: comment.originalRepresentation }
+				},
+				{
+					key: { text: 'Redacted comment' },
+					value: { text: session?.redactedRepresentation },
+					actions: {
+						items: [
+							{
+								text: 'Change',
+								href: `/appeals-service/appeal-details/${appealDetails.appealId}/interested-party-comments/${comment.id}/redact`,
+								visuallyHiddenText: 'redacted comment'
+							}
+						]
+					}
+				},
+				{
+					key: { text: 'Supporting documents' },
+					value: attachmentsList ? { html: attachmentsList } : { text: 'Not provided' },
+					actions: {
+						items: [
+							...(comment.attachments?.length > 0
+								? [
+										{
+											text: 'Manage',
+											href: `${baseUrl}/manage-documents/${folderId}?backUrl=/interested-party-comments/${comment.id}/redact/confirm`,
+											visuallyHiddenText: 'supporting documents'
+										}
+								  ]
+								: []),
+							{
+								text: 'Add',
+								href: `${baseUrl}/add-document?backUrl=/interested-party-comments/${comment.id}/redact/confirm`,
+								visuallyHiddenText: 'supporting documents'
+							}
+						]
+					}
 				}
-			}
-		]
-	}
-});
+			]
+		}
+	};
+
+	return pageComponents;
+};
