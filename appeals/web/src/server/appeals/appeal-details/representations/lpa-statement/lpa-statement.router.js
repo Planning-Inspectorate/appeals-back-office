cbf5effe7096ff401e5ec748a70ab7701d3617c2
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-04-11 15:38:02 +0100

    fix(web): clearing uncommitted files from session on get for top-level pages

--- appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/lpa-statement.router.js
+++ appeals/web/src/server/appeals/appeal-details/representations/lpa-statement/lpa-statement.router.js
@@ -8,12 +8,13 @@ import redactRouter from './redact/redact.router.js';
 import { getRepresentationAttachmentsFolder } from '#appeals/appeal-details/representations/final-comments/final-comments.middleware.js';
 import addDocumentRouter from '../document-attachments/add-document.router.js';
 import manageDocumentsRouter from '../document-attachments/manage-documents.router.js';
+import { clearUncommittedFilesFromSession } from '#appeals/appeal-documents/appeal-documents.middleware.js';
 
 const router = Router({ mergeParams: true });
 
 router
 	.route('/')
-	.get(renderReviewLpaStatement)
+	.get(clearUncommittedFilesFromSession, renderReviewLpaStatement)
 	.post(
 		validateStatus,
 		saveBodyToSession('lpaStatement', { scopeToAppeal: true }),
