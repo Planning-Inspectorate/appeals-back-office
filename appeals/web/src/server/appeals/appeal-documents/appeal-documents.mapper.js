44cfe5d5f9e0e3fb2257ecd47947991637634e00
Ed Packard <edward.packard@kinandcarta.com>
2025-04-14 10:58:32 +0100

    fix(web): a2-3031 change case for current version tag

--- appeals/web/src/server/appeals/appeal-documents/appeal-documents.mapper.js
+++ appeals/web/src/server/appeals/appeal-documents/appeal-documents.mapper.js
@@ -1116,7 +1116,7 @@ function mapDocumentNameHtmlProperty(document, documentVersion) {
 		htmlProperty.pageComponents.push({
 			type: 'html',
 			parameters: {
-				html: `<strong class="govuk-tag govuk-tag--blue single-line govuk-!-margin-bottom-2">CURRENT VERSION</strong>`
+				html: `<strong class="govuk-tag govuk-tag--blue single-line govuk-!-margin-bottom-2">Current version</strong>`
 			}
 		});
 	}
