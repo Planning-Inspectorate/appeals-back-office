cc40a946fbe824e8486dc761613ac8ca8e10ce23
Ed Packard <edward.packard@kinandcarta.com>
2025-04-17 12:37:55 +0100

    fix(web): a2-2605 updates after rebase

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/local-planning-authority.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/local-planning-authority.mapper.js
@@ -1,13 +1,17 @@
 import { textSummaryListItem } from '#lib/mappers/index.js';
 
 /** @type {import('../mapper.js').SubMapper} */
-export const mapLocalPlanningAuthority = ({ appealDetails, currentRoute }) =>
+export const mapLocalPlanningAuthority = ({
+	appealDetails,
+	currentRoute,
+	userHasUpdateCasePermission
+}) =>
 	textSummaryListItem({
 		id: 'local-planning-authority',
 		text: 'LPA',
 		value: appealDetails.localPlanningDepartment,
 		link: `${currentRoute}/change-appeal-details/local-planning-authority`,
-		editable: false, //todo: check
+		editable: userHasUpdateCasePermission,
 		actionText: 'Change',
 		classes: 'appeal-local-planning-authority'
 	});
