dac47219b52959eb62c4cc7a025dea96150fc886
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-04-25 14:48:09 +0100

    feat(web): create new issue decision and check your answers pages
    refactor: remove redundant try-catch blocks and extra functions
    feat: bring issue decision page inline with new design
    feat: redirect from issue decision to check your answers page
    feat: bring check your answers page inline with new design
    test: update unit tests for the issue decision journey

--- appeals/web/src/server/appeals/appeal-details/issue-decision/issue-decision.router.js
+++ appeals/web/src/server/appeals/appeal-details/issue-decision/issue-decision.router.js
@@ -13,7 +13,7 @@ router
 	.route('/decision')
 	.get(
 		assertUserHasPermission(permissionNames.setCaseOutcome),
-		asyncHandler(controller.getIssueDecision)
+		asyncHandler(controller.renderIssueDecision)
 	)
 	.post(
 		validators.validateDecision,
@@ -26,7 +26,7 @@ router
 	.get(
 		validateAppeal,
 		assertUserHasPermission(permissionNames.setCaseOutcome),
-		asyncHandler(controller.getDecisionLetterUpload)
+		asyncHandler(controller.renderDecisionLetterUpload)
 	)
 	.post(
 		validateAppeal,
@@ -38,7 +38,7 @@ router
 	.route('/decision-letter-date')
 	.get(
 		assertUserHasPermission(permissionNames.setCaseOutcome),
-		asyncHandler(controller.getDateDecisionLetter)
+		asyncHandler(controller.renderDateDecisionLetter)
 	)
 	.post(
 		validators.validateVisitDateFields,
@@ -53,7 +53,7 @@ router
 	.route('/invalid-reason')
 	.get(
 		assertUserHasPermission(permissionNames.setCaseOutcome),
-		asyncHandler(controller.getInvalidReason)
+		asyncHandler(controller.renderInvalidReason)
 	)
 	.post(
 		createTextAreaSanitizer('decisionInvalidReason'),
@@ -67,7 +67,7 @@ router
 	.get(
 		validateAppeal,
 		assertUserHasPermission(permissionNames.setCaseOutcome),
-		asyncHandler(controller.getCheckDecision)
+		asyncHandler(controller.renderCheckDecision)
 	)
 	.post(
 		validateAppeal,
@@ -80,7 +80,7 @@ router
 	.route('/check-invalid-decision')
 	.get(
 		assertUserHasPermission(permissionNames.setCaseOutcome),
-		asyncHandler(controller.getCheckInvalidDecision)
+		asyncHandler(controller.renderCheckInvalidDecision)
 	)
 	.post(
 		validators.validateCheckDecision,
