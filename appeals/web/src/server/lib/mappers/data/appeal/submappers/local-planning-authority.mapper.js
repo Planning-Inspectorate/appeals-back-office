fee884d16527e404a1529e15e4da32a2aeda4921
Ed Packard <edward.packard@kinandcarta.com>
2025-05-01 14:59:40 +0100

    chore(web): a2-2605 disable change lpa link

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/local-planning-authority.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/local-planning-authority.mapper.js
@@ -3,15 +3,15 @@ import { textSummaryListItem } from '#lib/mappers/index.js';
 /** @type {import('../mapper.js').SubMapper} */
 export const mapLocalPlanningAuthority = ({
 	appealDetails,
-	currentRoute,
-	userHasUpdateCasePermission
+	currentRoute
+	//userHasUpdateCasePermission
 }) =>
 	textSummaryListItem({
 		id: 'local-planning-authority',
 		text: 'LPA',
 		value: appealDetails.localPlanningDepartment,
 		link: `${currentRoute}/change-appeal-details/local-planning-authority`,
-		editable: userHasUpdateCasePermission,
+		editable: false, //todo: a2-2605 userHasUpdateCasePermission,
 		actionText: 'Change',
 		classes: 'appeal-local-planning-authority'
 	});
