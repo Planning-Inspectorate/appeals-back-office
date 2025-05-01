bac8f8379160e7b9c5f7162a101d3543297f1192
Ed Packard <edward.packard@kinandcarta.com>
2025-05-01 16:03:05 +0100

    feat(web): a2-2605 restore change LPA links

--- appeals/web/src/server/appeals/appeal-details/change-appeal-details/local-planning-authority/local-planning-authority.validators.js
+++ appeals/web/src/server/appeals/appeal-details/change-appeal-details/local-planning-authority/local-planning-authority.validators.js
@@ -1,10 +1,9 @@
 import { createValidator } from '@pins/express';
 import { body } from 'express-validator';
 
-//todo: a2-2605 confirm validation error message with design
 export const validateLpa = createValidator(
 	body('localPlanningAuthority')
 		.trim()
 		.notEmpty()
-		.withMessage('Please choose a local planning authority.')
+		.withMessage('Select the local planning authority.')
 );
