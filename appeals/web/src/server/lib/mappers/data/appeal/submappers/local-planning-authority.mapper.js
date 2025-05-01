1c90539e7c4240f6df5ff5c40e5a80d2dee55830
Ed Packard <edward.packard@kinandcarta.com>
2025-05-01 16:03:05 +0100

    feat(web): a2-2605 restore change LPA links

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/local-planning-authority.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/local-planning-authority.mapper.js
@@ -3,15 +3,15 @@ import { textSummaryListItem } from '#lib/mappers/index.js';
 /** @type {import('../mapper.js').SubMapper} */
 export const mapLocalPlanningAuthority = ({
 	appealDetails,
-	currentRoute
-	//userHasUpdateCasePermission
+	currentRoute,
+	userHasUpdateCasePermission
 }) =>
 	textSummaryListItem({
 		id: 'local-planning-authority',
 		text: 'LPA',
 		value: appealDetails.localPlanningDepartment,
 		link: `${currentRoute}/change-appeal-details/local-planning-authority`,
-		editable: false, //todo: a2-2605 userHasUpdateCasePermission,
+		editable: userHasUpdateCasePermission, //todo: a2-2605 - add logic if LPAQ submitted
 		actionText: 'Change',
 		classes: 'appeal-local-planning-authority'
 	});
