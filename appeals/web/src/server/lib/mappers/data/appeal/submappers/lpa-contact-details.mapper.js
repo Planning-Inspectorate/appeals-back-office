e5e269b0490c3180540eee1d0eac3022943a8a71
Ed Packard <edward.packard@kinandcarta.com>
2025-05-01 14:59:40 +0100

    chore(web): a2-2605 disable change lpa link

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/lpa-contact-details.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/lpa-contact-details.mapper.js
@@ -3,8 +3,8 @@ import { textSummaryListItem } from '#lib/mappers/index.js';
 /** @type {import('../mapper.js').SubMapper} */
 export const mapLpaContactDetails = ({
 	appealDetails,
-	currentRoute,
-	userHasUpdateCasePermission
+	currentRoute
+	//userHasUpdateCasePermission
 }) =>
 	textSummaryListItem({
 		id: 'lpa-contact-details',
@@ -16,6 +16,6 @@ export const mapLpaContactDetails = ({
 					: 'Awaiting questionnaire'
 		},
 		link: `${currentRoute}/change-appeal-details/local-planning-authority`,
-		editable: userHasUpdateCasePermission,
+		editable: false, //todo: a2-2605 userHasUpdateCasePermission,
 		classes: 'appeal-lpa-contact-details'
 	});
