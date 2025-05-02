363e02434bea7ab1f345622213517517f63c8599
Ed Packard <edward.packard@kinandcarta.com>
2025-05-02 14:58:22 +0100

    feat(web): a2-2983 modify data-cy tag for case officer CTA

--- appeals/web/src/server/lib/mappers/utils/format-case-officer-details-for-case-summary.js
+++ appeals/web/src/server/lib/mappers/utils/format-case-officer-details-for-case-summary.js
@@ -1,5 +1,5 @@
 /**
- * Remove email address from Case Officer details if they exist
+ * Remove email address from Case Officer details if they exist and generate unique data-cy attribute
  * @param {SummaryListRowProperties} caseOfficerDetails
  * @returns {SummaryListRowProperties}
  */
@@ -8,10 +8,29 @@ export function formatCaseOfficerDetailsForCaseSummary(caseOfficerDetails) {
 		caseOfficerDetails.value.html.split('<li>').length > 1
 			? caseOfficerDetails.value.html.split('</li>')[0].split('<li>')[1] || ''
 			: caseOfficerDetails.value.html;
+
+	const formattedActionItems = caseOfficerDetails.actions.items.map(
+		(/** @type {ActionItemProperties} */ item) => ({
+			...item,
+			attributes: item.attributes
+				? {
+						...item.attributes,
+						['data-cy']: Object.prototype.hasOwnProperty.call(item.attributes, 'data-cy')
+							? item.attributes['data-cy'] + '-name'
+							: ''
+				  }
+				: {}
+		})
+	);
+
 	return {
 		...caseOfficerDetails,
 		value: {
 			html: formattedHtml
+		},
+		actions: {
+			...caseOfficerDetails.actions,
+			items: formattedActionItems
 		}
 	};
 }
