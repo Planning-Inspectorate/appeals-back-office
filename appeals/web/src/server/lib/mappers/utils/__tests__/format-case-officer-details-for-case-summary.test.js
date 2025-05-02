a74af6cb999ba766aee13adbef73a0022b4f11f1
Ed Packard <edward.packard@kinandcarta.com>
2025-05-02 14:58:22 +0100

    feat(web): a2-2983 modify data-cy tag for case officer CTA

--- appeals/web/src/server/lib/mappers/utils/__tests__/format-case-officer-details-for-case-summary.test.js
+++ appeals/web/src/server/lib/mappers/utils/__tests__/format-case-officer-details-for-case-summary.test.js
@@ -1,11 +1,11 @@
 import { formatCaseOfficerDetailsForCaseSummary } from '../format-case-officer-details-for-case-summary.js';
 
 describe('format-case-officer-details-for-case-summary', () => {
-	it('returns unmodified input if case officer not assigned', () => {
+	it('returns unmodified html and modified data-cy attribute if case officer not assigned', () => {
 		const input = {
 			key: { text: 'Case officer' },
 			value: { html: '<p class="govuk-body">Not assigned</p>' },
-			actions: { items: [['']] },
+			actions: { items: [{ attributes: { 'data-cy': 'assign-case-officer' } }] },
 			classes: 'appeal-case-officer'
 		};
 
@@ -13,26 +13,26 @@ describe('format-case-officer-details-for-case-summary', () => {
 
 		expect(result.key.text).toEqual(input.key.text);
 		expect(result.value.html).toEqual(input.value.html);
-		expect(result.actions.items).toEqual(input.actions.items);
 		expect(result.classes).toEqual(input.classes);
 		expect(result.key.text).toEqual(input.key.text);
+		expect(result.actions.items[0].attributes['data-cy']).toEqual('assign-case-officer-name');
 	});
-	it('returns modified input if case officer assigned', () => {
+	it('returns modified html and modified data-cy attribute if case officer assigned', () => {
 		const input = {
 			key: { text: 'Case officer' },
 			value: {
 				html: '<ul class="govuk-list"><li>A Name</li><li>test@example.com</li></ul>'
 			},
-			actions: { items: [[Object]] },
-			classes: 'appeal-case-officer'
+			classes: 'appeal-case-officer',
+			actions: { items: [{ attributes: { 'data-cy': 'change-case-officer' } }] }
 		};
 
 		const result = formatCaseOfficerDetailsForCaseSummary(input);
 
 		expect(result.key.text).toEqual(input.key.text);
 		expect(result.value.html).toEqual('A Name');
-		expect(result.actions.items).toEqual(input.actions.items);
 		expect(result.classes).toEqual(input.classes);
 		expect(result.key.text).toEqual(input.key.text);
+		expect(result.actions.items[0].attributes['data-cy']).toEqual('change-case-officer-name');
 	});
 });
