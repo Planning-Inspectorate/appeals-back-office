18c4e6a317eb6335b522192d7c25dafe4c2a2ee9
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2025-05-07 10:52:40 +0100

    fix(web): hides decision row temporarily when decision is issued

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/appeal-decision.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/appeal-decision.mapper.js
@@ -1,10 +1,11 @@
 import { generateIssueDecisionUrl } from '#appeals/appeal-details/issue-decision/issue-decision.mapper.js';
 import { APPEAL_CASE_STATUS } from 'pins-data-model';
 import { dateISOStringToDisplayDate } from '#lib/dates.js';
-import { documentationFolderTableItem } from '#lib/mappers/index.js';
+import { documentationFolderTableItem, textSummaryListItem } from '#lib/mappers/index.js';
 import { addBackLinkQueryToUrl } from '#lib/url-utilities.js';
 
 /** @type {import('../mapper.js').SubMapper} */
+// @ts-ignore
 export const mapAppealDecision = ({ appealDetails, request }) => {
 	const actionText = (() => {
 		switch (appealDetails.appealStatus) {
@@ -18,26 +19,40 @@ export const mapAppealDecision = ({ appealDetails, request }) => {
 		}
 	})();
 
-	return documentationFolderTableItem({
+	if (
+		appealDetails.appealStatus !== APPEAL_CASE_STATUS.COMPLETE &&
+		appealDetails.appealStatus !== APPEAL_CASE_STATUS.INVALID
+	) {
+		return documentationFolderTableItem({
+			id: 'appeal-decision',
+			text: 'Decision',
+			textClasses: 'appeal-decision-documentation',
+			statusText:
+				appealDetails.appealStatus === APPEAL_CASE_STATUS.COMPLETE ||
+				appealDetails.appealStatus === APPEAL_CASE_STATUS.INVALID
+					? 'Issued'
+					: 'Awaiting decision',
+			statusTextClasses: 'appeal-decision-status',
+			receivedText: appealDetails.decision?.letterDate
+				? dateISOStringToDisplayDate(appealDetails.decision.letterDate)
+				: 'Not applicable',
+			receivedTextClasses: 'appeal-decision-due-date',
+			actionHtml: actionText
+				? `<a class="govuk-link" href="${addBackLinkQueryToUrl(
+						request,
+						generateIssueDecisionUrl(appealDetails.appealId)
+				  )}">${actionText}<span class="govuk-visually-hidden"> decision</span></a>`
+				: '',
+			actionHtmlClasses: 'appeal-decision-actions'
+		});
+	}
+
+	return textSummaryListItem({
 		id: 'appeal-decision',
-		text: 'Decision',
-		textClasses: 'appeal-decision-documentation',
-		statusText:
-			appealDetails.appealStatus === APPEAL_CASE_STATUS.COMPLETE ||
-			appealDetails.appealStatus === APPEAL_CASE_STATUS.INVALID
-				? 'Issued'
-				: 'Awaiting decision',
-		statusTextClasses: 'appeal-decision-status',
-		receivedText: appealDetails.decision?.letterDate
-			? dateISOStringToDisplayDate(appealDetails.decision.letterDate)
-			: 'Not applicable',
-		receivedTextClasses: 'appeal-decision-due-date',
-		actionHtml: actionText
-			? `<a class="govuk-link" href="${addBackLinkQueryToUrl(
-					request,
-					generateIssueDecisionUrl(appealDetails.appealId)
-			  )}">${actionText}<span class="govuk-visually-hidden"> decision</span></a>`
-			: '',
-		actionHtmlClasses: 'appeal-decision-actions'
+		text: '',
+		value: '',
+		link: '',
+		actionText: '',
+		editable: false
 	});
 };
