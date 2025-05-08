ca534c54b22b3257ecc4e0c7fff3f658a6897003
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-05-08 12:30:27 +0100

    feat(api): add scopingOpinion folder to SingleLPAQuestionnaireResponse & generated new api-spec

--- appeals/api/src/server/openapi-types.ts
+++ appeals/api/src/server/openapi-types.ts
@@ -1651,6 +1651,22 @@ export interface SingleLPAQuestionnaireResponse {
 				caseId?: number;
 			}[];
 		};
+		scopingOpinion?: {
+			/** @example 1 */
+			folderId?: number;
+			/** @example "path/to/document/folder" */
+			path?: string;
+			documents?: {
+				/** @example "fdadc281-f686-40ee-97cf-9bafdd02b1cb" */
+				id?: string;
+				/** @example "an appeal related document.pdf" */
+				name?: string;
+				/** @example 1 */
+				folderId?: number;
+				/** @example 2 */
+				caseId?: number;
+			}[];
+		};
 		siteNotice?: {
 			/** @example 1 */
 			folderId?: number;
