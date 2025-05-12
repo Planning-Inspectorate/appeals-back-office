cb9eeb5b4b94cffb96a578715f5ab739e1e1bab0
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-05-12 15:02:01 +0100

    refactor(web): handle duplicated appellant case mappers between s78 and s20

--- appeals/web/src/server/lib/mappers/data/appellant-case/s78.js
+++ appeals/web/src/server/lib/mappers/data/appellant-case/s78.js
@@ -1,40 +1,12 @@
-import { submaps as hasSubmaps } from './has.js';
-import { mapInquiryNumberOfWitnesses } from './submappers/inquiry-number-of-witnesses.js';
-import { mapAdvertisedAppeal } from './submappers/advertised-appeal.js';
-import { mapProcedurePreferenceDuration } from './submappers/procedure-preference-duration.js';
-import { mapProcedurePreference } from './submappers/procedure-preference.js';
-import { mapRelatedAppeals } from './submappers/related-appeals.js';
-import { mapDesignAccessStatement } from './submappers/design-access-statement.js';
-import { mapNewPlansDrawings } from './submappers/new-plans-drawings.js';
-import { mapSupportingDocuments } from './submappers/supporting-documents.js';
-import { mapPlanningObligation } from './submappers/planning-obligation.js';
-import { mapOwnershipCertificate } from './submappers/ownership-certificate.js';
-import { mapProcedurePreferenceDetails } from './submappers/procedure-preference-details.js';
-import { mapOtherNewDocuments } from './submappers/other-new-documents.js';
-import { mapStatusPlanningObligation } from './submappers/status-planning-obligation.js';
+import { submaps as s20SubMaps } from './s20.js';
+import { mapOtherTenantsOfAgriculturalHolding } from './submappers/other-tenants-of-agricultural-holding.js';
 import { mapPartOfAgriculturalHolding } from './submappers/part-of-agricultural-holding.js';
 import { mapTenantOfAgriculturalHolding } from './submappers/tenant-of-agricultural-holding.js';
-import { mapOtherTenantsOfAgriculturalHolding } from './submappers/other-tenants-of-agricultural-holding.js';
-import { mapDevelopmentType } from './submappers/development-type.js';
 
 /** @type {Record<string, import('./mapper.js').SubMapper>} */
 export const submaps = {
-	...hasSubmaps,
-	inquiryNumberOfWitnesses: mapInquiryNumberOfWitnesses,
-	procedurePreferenceDuration: mapProcedurePreferenceDuration,
-	procedurePreference: mapProcedurePreference,
-	relatedAppeals: mapRelatedAppeals,
-	advertisedAppeal: mapAdvertisedAppeal,
-	designAccessStatement: mapDesignAccessStatement,
-	newPlansDrawings: mapNewPlansDrawings,
-	supportingDocuments: mapSupportingDocuments,
-	planningObligation: mapPlanningObligation,
-	ownershipCertificate: mapOwnershipCertificate,
-	otherNewDocuments: mapOtherNewDocuments,
-	statusPlanningObligation: mapStatusPlanningObligation,
+	...s20SubMaps,
 	partOfAgriculturalHolding: mapPartOfAgriculturalHolding,
 	tenantOfAgriculturalHolding: mapTenantOfAgriculturalHolding,
-	otherTenantsOfAgriculturalHolding: mapOtherTenantsOfAgriculturalHolding,
-	procedurePreferenceDetails: mapProcedurePreferenceDetails,
-	developmentType: mapDevelopmentType
+	otherTenantsOfAgriculturalHolding: mapOtherTenantsOfAgriculturalHolding
 };
