92a0a0e8c8727256d1553a49a914c7a479590edc
Michael Turner <109961022+pinsMT@users.noreply.github.com>
2025-05-14 13:45:38 +0100

    fix(api): added and amended tests (a2-2610)

--- appeals/web/src/server/appeals/appeal-details/audit/__tests__/audit.test.js
+++ appeals/web/src/server/appeals/appeal-details/audit/__tests__/audit.test.js
@@ -1,9 +1,12 @@
 import { tryMapDocument } from '#appeals/appeal-details/audit/audit.service.js';
+import { statusFormatMap } from '#appeals/appeal-details/representations/document-attachments/controller/redaction-status.js';
 
 describe('audit', () => {
 	describe('tryMapDocument', () => {
-		const auditTrailEntryText =
-			'Document 2288c506-bd6e-4a25-b0e3-05c10c350d9a_docstuff.docx uploaded (version 1)';
+		const redactionStatusKeys = Object.keys(statusFormatMap);
+		const randomKey = redactionStatusKeys[Math.floor(Math.random() * redactionStatusKeys.length)];
+		const randomRedactStatus = statusFormatMap[randomKey].toLowerCase();
+		const auditTrailEntryText = `Document 2288c506-bd6e-4a25-b0e3-05c10c350d9a_docstuff.docx uploaded (version 1, ${randomRedactStatus})`;
 		const docInfo = {
 			name: '2288c506-bd6e-4a25-b0e3-05c10c350d9a_docstuff.docx',
 			documentGuid: 'efac1b7f-71c6-4780-bf22-edd0b0531914',
@@ -12,11 +15,16 @@ describe('audit', () => {
 			documentType: 'representationAttachments'
 		};
 
-		it('should return audit trail entry with doc GUID removed from display name', async () => {
+		it('should return audit trail entry with doc GUID removed from a supporting doc display name', async () => {
 			const result = await tryMapDocument(1, auditTrailEntryText, docInfo, null);
 			expect(result).toEqual(
-				`Document <a class="govuk-link" href="#">docstuff.docx</a> uploaded (version 1)`
+				`Document <a class="govuk-link" href="#">docstuff.docx</a> uploaded (version 1, ${randomRedactStatus})`
 			);
+		});
+
+		it('should include the redaction status in the audit log display text for a supporting document', async () => {
+			const result = await tryMapDocument(1, auditTrailEntryText, docInfo, null);
+			expect(result).toMatch(/unredacted|no redaction required|redacted/);
 		});
 	});
 });
