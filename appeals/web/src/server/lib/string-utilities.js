1ec25a2cf2a0fd40f17c54a62a8521482f966d0c
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-05-16 15:42:04 +0100

    refactor(web): add an uncapitalizeFirstLetter function
    Also restructure some other functions for clarity

--- appeals/web/src/server/lib/string-utilities.js
+++ appeals/web/src/server/lib/string-utilities.js
@@ -1,27 +1,31 @@
 /**
- *
  * @param {string} str
  * @returns {boolean}
  */
-export const stringContainsDigitsOnly = (str) => {
-	return !!str.trim().match(/^\d+$/);
-};
+export const stringContainsDigitsOnly = (str) => Boolean(str.trim().match(/^\d+$/));
 
 /**
- *
  * @param {string} str
  * @returns {string}
  */
 export const capitalizeFirstLetter = (str) => {
-	if (str) {
-		if (str.length > 1) {
-			return str?.charAt(0)?.toUpperCase() + str?.slice(1);
-		} else {
-			return str?.toUpperCase();
-		}
-	} else {
-		return '';
+	if (str.length > 1) {
+		return str.charAt(0)?.toUpperCase() + str.slice(1);
 	}
+
+	return str.toUpperCase();
+};
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+export const uncapitalizeFirstLetter = (str) => {
+	if (str.length > 1) {
+		return str.charAt(0)?.toLowerCase() + str.slice(1);
+	}
+
+	return str.toLowerCase();
 };
 
 /**
