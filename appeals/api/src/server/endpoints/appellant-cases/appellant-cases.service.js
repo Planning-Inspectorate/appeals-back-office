19a265ee7c787ce062a98a8648cda15a8ffb5a23
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2025-05-22 12:17:46 +0100

    feat(api): update audit trail text around appellant case form
    refactor: make a function for case update audit trail detail
    refactor: improve documents controller code
    test: update tests to reflect changes to the audit log
    
    Remove awaits when creating audit trails. This can run in the background
    after the response is sent.

--- appeals/api/src/server/endpoints/appellant-cases/appellant-cases.service.js
+++ appeals/api/src/server/endpoints/appellant-cases/appellant-cases.service.js
@@ -1,3 +1,4 @@
+import * as CONSTANTS from '@pins/appeals/constants/support.js';
 import {
 	isOutcomeIncomplete,
 	isOutcomeInvalid,
@@ -17,10 +18,12 @@ import { createAuditTrail } from '#endpoints/audit-trails/audit-trails.service.j
 import stringTokenReplacement from '#utils/string-token-replacement.js';
 import formatDate from '#utils/date-formatter.js';
 import { getFormattedReasons } from '#utils/email-formatter.js';
+import { camelToScreamingSnake } from '#utils/string-utils.js';
 import * as documentRepository from '#repositories/document.repository.js';
 import { broadcasters } from '#endpoints/integrations/integrations.broadcasters.js';
 import { EventType } from '@pins/event-client';
 import { notifySend } from '#notify/notify-send.js';
+import { APPEAL_DEVELOPMENT_TYPES } from './appellant-cases.constants.js';
 
 /** @typedef {import('@pins/appeals.api').Appeals.UpdateAppellantCaseValidationOutcomeParams} UpdateAppellantCaseValidationOutcomeParams */
 /** @typedef {import('express').Request} Request */
@@ -33,7 +36,7 @@ import { notifySend } from '#notify/notify-send.js';
  * @param {NextFunction} next
  * @returns {Response | void}
  */
-const checkAppellantCaseExists = (req, res, next) => {
+export const checkAppellantCaseExists = (req, res, next) => {
 	const {
 		appeal,
 		params: { appellantCaseId }
@@ -51,7 +54,7 @@ const checkAppellantCaseExists = (req, res, next) => {
  * @param {UpdateAppellantCaseValidationOutcomeParams} param0
  * @param { import('#endpoints/appeals.js').NotifyClient } notifyClient
  */
-const updateAppellantCaseValidationOutcome = async (
+export const updateAppellantCaseValidationOutcome = async (
 	{ appeal, appellantCaseId, azureAdUserId, data, validationOutcome, validAt, siteAddress },
 	notifyClient
 ) => {
@@ -162,4 +165,81 @@ const updateAppellantCaseValidationOutcome = async (
 	}
 };
 
-export { checkAppellantCaseExists, updateAppellantCaseValidationOutcome };
+/**
+ * @param {Record<string, unknown>} data
+ * @returns {string}
+ * */
+export function renderAuditTrailDetail(data) {
+	const genericResult = CONSTANTS.AUDIT_TRAIL_APPELLANT_CASE_UPDATED;
+
+	const dataKeys = Object.keys(data).filter((key) => data[key] !== undefined);
+	if (dataKeys.length === 0) {
+		return genericResult;
+	}
+
+	if (dataKeys.includes('ownsSomeLand') && dataKeys.includes('ownsAllLand')) {
+		const parameter = (() => {
+			if (data.ownsAllLand) {
+				return 'Fully owned';
+			}
+
+			if (data.ownsSomeLand) {
+				return 'Partially owned';
+			}
+
+			return 'Not owned';
+		})();
+
+		return stringTokenReplacement(CONSTANTS.AUDIT_TRAIL_SITE_OWNERSHIP_UPDATED, [parameter]);
+	}
+
+	if (dataKeys.length !== 1) {
+		return genericResult;
+	}
+
+	const field = dataKeys[0];
+	const constantKey = `AUDIT_TRAIL_${camelToScreamingSnake(field)}_UPDATED`;
+
+	/** @type {Record<string, *>} */
+	const auditTrailParameters = {
+		AUDIT_TRAIL_DEVELOPMENT_TYPE_UPDATED: () =>
+			APPEAL_DEVELOPMENT_TYPES.find(({ value }) => value === data.developmentType)?.label ||
+			data.developmentType,
+		AUDIT_TRAIL_SITE_AREA_SQUARE_METRES_UPDATED: () => data.siteAreaSquareMetres,
+		AUDIT_TRAIL_IS_GREEN_BELT_UPDATED: () => (data.isGreenBelt ? 'Yes' : 'No'),
+		AUDIT_TRAIL_KNOWS_OTHER_OWNERS_UPDATED: () => data.knowsOtherOwners,
+		AUDIT_TRAIL_SITE_ACCESS_DETAILS_UPDATED: () =>
+			data.siteAccessDetails ? `Yes\n${data.siteAccessDetails}` : 'No',
+		AUDIT_TRAIL_SITE_SAFETY_DETAILS_UPDATED: () => (data.siteSafetyDetails ? 'Yes' : 'No'),
+		AUDIT_TRAIL_APPLICATION_DATE_UPDATED: () =>
+			data.applicationDate
+				? formatDate(new Date(/** @type {string} */ (data.applicationDate)))
+				: undefined,
+		// @ts-ignore
+		AUDIT_TRAIL_DEVELOPMENT_DESCRIPTION_UPDATED: () => data.developmentDescription?.details,
+		AUDIT_TRAIL_APPLICATION_DECISION_DATE_UPDATED: () =>
+			data.applicationDecisionDate
+				? formatDate(new Date(/** @type {string} */ (data.applicationDecisionDate)))
+				: undefined,
+		AUDIT_TRAIL_AGRICULTURAL_HOLDING_UPDATED: () => (data.agriculturalHolding ? 'Yes' : 'No'),
+		AUDIT_TRAIL_TENANT_AGRICULTURAL_HOLDING_UPDATED: () =>
+			data.tenantAgriculturalHolding ? 'Yes' : 'No',
+		AUDIT_TRAIL_OTHER_TENANTS_AGRICULTURAL_HOLDING_UPDATED: () =>
+			data.otherTenantsAgriculturalHolding,
+		AUDIT_TRAIL_APPLICATION_DECISION_UPDATED: () => data.applicationDecision,
+		AUDIT_TRAIL_APPELLANT_PROCEDURE_PREFERENCE_UPDATED: () => data.appellantProcedurePreference,
+		AUDIT_TRAIL_APPELLANT_PROCEDURE_PREFERENCE_DETAILS_UPDATED: () =>
+			data.appellantProcedurePreferenceDetails,
+		AUDIT_TRAIL_APPELLANT_PROCEDURE_PREFERENCE_DURATION_UPDATE: () =>
+			data.appellantProcedurePreferenceDuration,
+		AUDIT_TRAIL_APPELLANT_PROCEDURE_PREFERENCE_WITNESS_COUNT_UPDATED: () =>
+			data.appellantProcedurePreferenceWitnessCount,
+		AUDIT_TRAIL_STATUS_PLANNING_OBLIGATION_UPDATED: () => data.planningObligation
+	};
+
+	if (!auditTrailParameters[constantKey]) {
+		return constantKey in auditTrailParameters ? CONSTANTS[constantKey] : genericResult;
+	}
+
+	return stringTokenReplacement(CONSTANTS[constantKey], [auditTrailParameters[constantKey]()]);
+}
