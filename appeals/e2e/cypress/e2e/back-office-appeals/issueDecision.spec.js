ec560c6e665e12cc124d188257e935ab3ca9bf2f
Abiodun Ishola <abiodun.ishola@solirius.com>
2025-05-23 11:35:10 +0100

    test(appeals): refactor code and fix failing tests

--- appeals/e2e/cypress/e2e/back-office-appeals/issueDecision.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/issueDecision.spec.js
@@ -3,13 +3,10 @@
 
 import { users } from '../../fixtures/users';
 import { CaseDetailsPage } from '../../page_objects/caseDetailsPage.js';
-import { ListCasesPage } from '../../page_objects/listCasesPage';
 import { DateTimeSection } from '../../page_objects/dateTimeSection';
-import { urlPaths } from '../../support/urlPaths.js';
 import { tag } from '../../support/tag';
 import { happyPathHelper } from '../../support/happyPathHelper.js';
 
-const listCasesPage = new ListCasesPage();
 const dateTimeSection = new DateTimeSection();
 const caseDetailsPage = new CaseDetailsPage();
 
@@ -24,7 +21,7 @@ describe('Issue Decision', () => {
 
 	issueDecisionCompleteStatus.forEach((issueDecision, index) => {
 		it(`Change to ${issueDecision} type`, { tags: tag.smoke }, () => {
-			let todaysDate = new Date();
+			let today = new Date();
 
 			cy.createCase().then((caseRef) => {
 				cy.addLpaqSubmissionToCase(caseRef);
@@ -39,11 +36,11 @@ describe('Issue Decision', () => {
 				caseDetailsPage.clickIssueDecision(caseRef);
 				caseDetailsPage.selectRadioButtonByValue(caseDetailsPage.exactMatch(issueDecision));
 				caseDetailsPage.clickButtonByText('Continue');
-				caseDetailsPage.uploadSampleFile(sampleFiles.pdf);
+				caseDetailsPage.uploadSampleFile(sampleFiles.document);
 				caseDetailsPage.clickButtonByText('Continue');
-				//dateTimeSection.enterDecisionLetterDate(todaysDate);
-				//caseDetailsPage.clickButtonByText('Continue');
-				//caseDetailsPage.selectCheckbox();
+				dateTimeSection.enterDecisionLetterDate(today);
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.selectCheckbox();
 				caseDetailsPage.clickButtonByText('Send Decision');
 				caseDetailsPage.checkStatusOfCase('Complete', 0);
 				caseDetailsPage.checkDecisionOutcome(issueDecision);
