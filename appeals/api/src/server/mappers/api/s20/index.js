bfa03ad5b13bdb349751c5082b3e2a52930a7fa8
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-05-28 16:19:35 +0100

    feat(api): s20 lpaq mappers added & s78 mappers refactored to avoid duplicate code

---
+++ appeals/api/src/server/mappers/api/s20/index.js
@@ -0,0 +1,7 @@
+import { mapS20AppellantCase } from './map-appellant-case.js';
+import { mapS20LpaQuestionnaire } from './map-lpa-questionnaire.js';
+
+export const apiS20Mappers = {
+	appellantCase: mapS20AppellantCase,
+	lpaQuestionnaire: mapS20LpaQuestionnaire
+};
