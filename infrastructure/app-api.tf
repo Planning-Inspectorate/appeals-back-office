c9b1eb1476e07e61e7230411ddc5afdbae03d4ae
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-06-04 11:11:53 +0100

    feat(appeals): updated formatting of terraform

--- infrastructure/app-api.tf
+++ infrastructure/app-api.tf
@@ -66,7 +66,7 @@ module "app_api" {
     #feature flags
     FEATURE_FLAG_S78_WRITTEN    = var.apps_config.featureFlags.featureFlagS78Written
     FEATURE_FLAG_S78_HEARING    = var.apps_config.featureFlags.featureFlagS78Hearing
-		FEATURE_FLAG_S78_INQUIRY    = var.apps_config.featureFlags.featureFlagS78Inquiry
+    FEATURE_FLAG_S78_INQUIRY    = var.apps_config.featureFlags.featureFlagS78Inquiry
     FEATURE_FLAG_LINKED_APPEALS = var.apps_config.featureFlags.featureFlagLinkedAppeals
     FEATURE_FLAG_S20            = var.apps_config.featureFlags.featureFlagS20
   }
