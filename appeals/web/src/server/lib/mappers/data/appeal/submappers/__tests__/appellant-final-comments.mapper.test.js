f3507238f3957ea2b07e71ed9c934f1c0c3baf59
Michael Turner <109961022+pinsMT@users.noreply.github.com>
2025-06-10 16:18:23 +0100

    fix(web): added condition which removes rows for hearing cases (a2-3236)

---
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/__tests__/appellant-final-comments.mapper.test.js
@@ -0,0 +1,62 @@
+// @ts-nocheck
+import { mapAppellantFinalComments } from '#lib/mappers/data/appeal/submappers/appellant-final-comments.mapper.js';
+
+describe('appellant-final-comments.mapper', () => {
+	const data = {
+		currentRoute: '/test',
+		appealDetails: {
+			appealId: 8526,
+			appealReference: '6008526',
+			appealType: 'Planning appeal',
+			createdAt: '2025-06-09T10:54:51.201Z',
+			validAt: '2024-08-09T10:54:41.225Z',
+			startedAt: '2025-06-08T23:00:00.000Z',
+			documentationSummary: {
+				appellantCase: {
+					status: 'Valid',
+					dueDate: null,
+					receivedAt: '2025-06-09T10:54:51.201Z'
+				},
+				appellantFinalComments: {
+					status: 'not_received',
+					receivedAt: null,
+					representationStatus: null
+				}
+			}
+		},
+		request: null
+	};
+
+	it('should display appellant final comments row if the procedure type is written', () => {
+		data.appealDetails.procedureType = 'Written';
+		console.log(data);
+		const result = mapAppellantFinalComments(data);
+		expect(result).toEqual({
+			display: {
+				tableItem: [
+					{
+						text: 'Appellant final comments'
+					},
+					{
+						text: 'Awaiting final comments'
+					},
+					{
+						text: 'Due by '
+					},
+					{
+						classes: 'govuk-!-text-align-right',
+						html: ''
+					}
+				]
+			},
+			id: 'appellant-final-comments'
+		});
+	});
+
+	it('should not display appellant final comments row if the procedure type is hearing', () => {
+		data.appealDetails.procedureType = 'Hearing';
+		const result = mapAppellantFinalComments(data);
+		console.log(result);
+		expect(result).toEqual({ id: 'start-case-date', display: {} });
+	});
+});
