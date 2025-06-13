3368f90c0f97485c3afea33dfd8559624c26264d
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-06-13 10:18:03 +0100

    feat(appeals): added CAS to feature flag names

--- packages/appeals/constants/common.js
+++ packages/appeals/constants/common.js
@@ -9,7 +9,8 @@ export const FEATURE_FLAG_NAMES = Object.freeze({
 	SECTION_78: 'featureFlagS78Written',
 	SECTION_78_HEARING: 'featureFlagS78Hearing',
 	SECTION_78_INQUIRY: 'featureFlagS78Inquiry',
-	SECTION_20: 'featureFlagS20'
+	SECTION_20: 'featureFlagS20',
+	CAS: 'featureFlagCAS'
 });
 
 export const APPEAL_TYPE = Object.freeze({
