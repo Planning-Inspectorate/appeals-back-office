327e592f7487b98a57df79229801cff20a1cbe93
lachlandow1 <lachlan.dow@solirius.com>
2025-06-13 16:56:12 +0100

    feat(web): update decision letter correction notice page

---
+++ appeals/web/src/server/appeals/appeal-details/update-decision-letter/update-decision-letter.mapper.js
@@ -0,0 +1,40 @@
+import { appealShortReference } from '#lib/appeals-formatter.js';
+import { textareaInput } from '#lib/mappers/index.js';
+// import { preRenderPageComponents } from '#lib/nunjucks-template-builders/page-component-rendering.js';
+//import { buildDecisionDocumentLinkHtml } from '#lib/mappers/data/appeal/common.js';
+
+/** @typedef {import('@pins/appeals.api').Appeals.SingleAppellantCaseResponse} AppellantCase */
+/** @typedef {import('../appeal-details.types.js').WebAppeal} Appeal */
+/** @typedef {{folderId: number, documentId: string, documentName: string, letterDate: string, outcome: string}} Decision */
+
+/**
+ *
+ * @param {number} appealId
+ * @param {string} appealReference
+ * @param {string} [correctionNotice]
+ * @param {import('@pins/express').ValidationErrors|null|undefined} [errors]
+
+ * @returns {PageContent}
+ */
+export const correctionNoticePage = (appealId, appealReference, correctionNotice, errors) => {
+	// = lpaQuestionnaireData.lpaProcedurePreferenceDetails;
+
+	/** @type {PageContent} */
+	const pageContent = {
+		title: 'Appeal decision',
+		backLinkUrl: `/appeals-service/appeal-details/${appealId}`,
+		preHeading: `Appeal ${appealShortReference(appealReference)}`,
+		heading: 'Correction notice',
+		pageComponents: [
+			textareaInput({
+				name: 'correctionNotice',
+				id: 'correction-notice',
+				labelIsPageHeading: true,
+				value: correctionNotice || '',
+				errorMsg: errors?.correctionNotice.msg
+			})
+		]
+	};
+
+	return pageContent;
+};
