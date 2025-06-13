7f7ec6e462c73e45d17619a29d6e340102e288b8
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-06-13 15:13:19 +0100

    feat(appeals): updated functions to handle update and cancel

--- appeals/api/src/server/endpoints/integrations/integrations.broadcasters/event.js
+++ appeals/api/src/server/endpoints/integrations/integrations.broadcasters/event.js
@@ -6,16 +6,24 @@ import { schemas, validateFromSchema } from '../integrations.validators.js';
 import { databaseConnector } from '#utils/database-connector.js';
 import { ODW_SYSTEM_ID, EVENT_TYPE } from '@pins/appeals/constants/common.js';
 import { mapSiteVisitEntity, mapHearingEntity } from '#mappers/integration/map-event-entity.js';
+import { EventType } from '@pins/event-client';
 /** @typedef {import('pins-data-model').Schemas.AppealEvent} AppealEvent */
+/** @typedef {import('@pins/appeals.api').Schema.Hearing} Hearing */
 
 /**
  *
  * @param {number} eventId
  * @param {string} eventType
  * @param {string} updateType
+ * @param {Hearing | undefined | null} existingHearing
  * @returns
  */
-export const broadcastEvent = async (eventId, eventType, updateType) => {
+export const broadcastEvent = async (
+	eventId,
+	eventType,
+	updateType,
+	existingHearing = undefined
+) => {
 	if (!config.serviceBusEnabled && config.NODE_ENV !== 'development') {
 		return false;
 	}
@@ -47,7 +55,7 @@ export const broadcastEvent = async (eventId, eventType, updateType) => {
 	}
 
 	if (eventType === EVENT_TYPE.HEARING) {
-		const hearing = await databaseConnector.hearing.findUnique({
+		let hearing = await databaseConnector.hearing.findUnique({
 			where: { id: eventId },
 			include: {
 				address: true,
@@ -59,11 +67,38 @@ export const broadcastEvent = async (eventId, eventType, updateType) => {
 			}
 		});
 
-		if (!hearing) {
+		if (!hearing && updateType !== EventType.Delete) {
 			pino.error(
 				`Trying to broadcast info for event ${eventId} of type ${eventType}, but it was not found.`
 			);
 			return false;
+		}
+
+		// Handling Cancellation and deletion of hearing
+		if (updateType === EventType.Delete && existingHearing) {
+			const appeal = await databaseConnector.appeal.findUnique({
+				where: { id: existingHearing.appealId }
+			});
+
+			if (!appeal) {
+				pino.error(
+					`Trying to broadcast info for event ${eventId} of type ${eventType}, no appeal was found.`
+				);
+				return false;
+			}
+
+			hearing = {
+				id: eventId,
+				address: null,
+				appealId: appeal.id,
+				addressId: null,
+				// @ts-ignore
+				appeal: {
+					reference: appeal.reference
+				},
+				hearingStartTime: existingHearing.hearingStartTime,
+				hearingEndTime: null
+			};
 		}
 
 		// @ts-ignore
