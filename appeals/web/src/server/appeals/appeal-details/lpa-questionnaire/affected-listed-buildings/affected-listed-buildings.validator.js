1c81d5396b441c98267c32e494ca404df0414c48
zoebutterworth <zoe.butterworth.9f@planninginspectorate.gov.uk>
2025-06-16 17:25:42 +0100

    feat(web): check listed building number is 7 digits

--- appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/affected-listed-buildings/affected-listed-buildings.validator.js
+++ appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/affected-listed-buildings/affected-listed-buildings.validator.js
@@ -5,4 +5,8 @@ export const validateAffectedListedBuilding = createValidator(
 	body('affectedListedBuilding')
 		.notEmpty()
 		.withMessage('Provide a listed building entry list number')
+		.isNumeric()
+		.withMessage('Listed building entry number must be 7 digits')
+		.isLength({ min: 7, max: 7 })
+		.withMessage('Listed building entry number must be 7 digits')
 );
