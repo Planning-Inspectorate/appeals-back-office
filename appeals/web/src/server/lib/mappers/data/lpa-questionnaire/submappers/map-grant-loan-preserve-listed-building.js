a6e31b97e5eb3beaa6751972986b83d20130facd
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-06-17 09:21:42 +0100

    feat(web): copy update on preserve grant loan question

--- appeals/web/src/server/lib/mappers/data/lpa-questionnaire/submappers/map-grant-loan-preserve-listed-building.js
+++ appeals/web/src/server/lib/mappers/data/lpa-questionnaire/submappers/map-grant-loan-preserve-listed-building.js
@@ -4,7 +4,7 @@ import { booleanSummaryListItem } from '#lib/mappers/index.js';
 export const mapGrantLoanToPreserve = ({ lpaQuestionnaireData, currentRoute, userHasUpdateCase }) =>
 	booleanSummaryListItem({
 		id: 'grant-loan-preserve-listed-building',
-		text: 'Was a grant or loan made to preserve the listed building?',
+		text: 'Was a grant or loan made to preserve the listed building at the appeal site?',
 		value: lpaQuestionnaireData.preserveGrantLoan,
 		defaultText: '',
 		addCyAttribute: true,
