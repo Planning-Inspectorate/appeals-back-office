c0571348bde7f521ae4b46515dd0b1d1f6a2612b
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-06-18 14:13:01 +0100

    feat(appeals): added pages for inquiry setup address details

--- appeals/web/src/server/appeals/appeal-details/inquiry/setup/set-up-inquiry-validators.js
+++ appeals/web/src/server/appeals/appeal-details/inquiry/setup/set-up-inquiry-validators.js
@@ -3,10 +3,38 @@ import { createDateInputFieldsValidator } from '#lib/validators/date-input.valid
 import { createDateInputDateValidityValidator } from '#lib/validators/date-input.validator.js';
 import { createTimeInputValidator } from '#lib/validators/time-input.validator.js';
 import { createDateInputDateInFutureValidator } from '#lib/validators/date-input.validator.js';
+import { textInputCharacterLimits } from '#appeals/appeal.constants.js';
+import { createYesNoRadioValidator } from '#lib/validators/radio.validator.js';
+import { createTextInputOptionalValidator } from '#lib/validators/text-input-validator.js';
+import { createPostcodeValidator } from '#lib/validators/address.validator.js';
+import {
+	createAddressLine1Validator,
+	createAddressLine2Validator,
+	createTownValidator
+} from '#lib/validators/address.validator.js';
 
 export const validateInquiryDateTime = createValidator(
 	createDateInputFieldsValidator('inquiry-date', 'Inquiry date'),
 	createDateInputDateValidityValidator('inquiry-date', 'Inquiry date'),
 	createDateInputDateInFutureValidator('inquiry-date', 'Inquiry date'),
 	createTimeInputValidator('inquiry-time', 'inquiry time')
+);
+
+export const validateAddressKnown = createYesNoRadioValidator(
+	'addressKnown',
+	'Select yes if you know the address of where the inquiry will take place'
+);
+
+const maxLength = textInputCharacterLimits.defaultAddressInputLength;
+
+export const validateInquiryAddress = createValidator(
+	createAddressLine1Validator(),
+	createAddressLine2Validator(),
+	createTownValidator(),
+	createTextInputOptionalValidator(
+		'county',
+		maxLength,
+		`County must be ${maxLength} characters or less`
+	),
+	createPostcodeValidator()
 );
--- appeals/web/src/server/appeals/appeal-details/inquiry/setup/set-up-inquiry.controller.js
+++ appeals/web/src/server/appeals/appeal-details/inquiry/setup/set-up-inquiry.controller.js
@@ -1,4 +1,5 @@
-import { inquiryDatePage } from './set-up-inquiry.mapper.js';
+import { inquiryDatePage, addressDetailsPage, addressKnownPage } from './set-up-inquiry.mapper.js';
+import { isEmpty, has, pick } from 'lodash-es';
 
 /**
  * @param {string} path
@@ -69,4 +70,163 @@ export const postInquiryDate = async (request, response) => {
 	const { appealId } = request.currentAppeal;
 
 	return response.redirect(`/appeals-service/appeal-details/${appealId}/inquiry/setup/address`);
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const getInquiryAddress = async (request, response) => {
+	return renderInquiryAddress(request, response, 'setup', request.session.setUpInquiry || {});
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const getChangeInquiryAddress = async (request, response) => {
+	const appealDetails = request.currentAppeal;
+	const sessionValues = request.session.changeInquiry || {};
+
+	const values = has(sessionValues, 'addressKnown')
+		? sessionValues
+		: { addressKnown: appealDetails.inquiry?.address ? 'yes' : 'no' };
+
+	return renderInquiryAddress(request, response, 'change', values);
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ * @param {{addressKnown: string}} [values]
+ * @param {'change' | 'setup'} action
+ */
+export const renderInquiryAddress = async (request, response, action, values) => {
+	const { errors } = request;
+
+	const appealDetails = request.currentAppeal;
+
+	const mappedPageContent = await addressKnownPage(appealDetails, action, values);
+
+	return response.status(errors ? 400 : 200).render('patterns/change-page.pattern.njk', {
+		pageContent: mappedPageContent,
+		errors
+	});
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const postInquiryAddress = async (request, response) => {
+	if (request.errors) {
+		return renderInquiryAddress(request, response, 'setup');
+	}
+
+	const { appealId } = request.currentAppeal;
+
+	if (request.body.addressKnown === 'yes') {
+		return response.redirect(
+			`/appeals-service/appeal-details/${appealId}/inquiry/setup/address-details`
+		);
+	}
+
+	return response.redirect(`/appeals-service/appeal-details/${appealId}/inquiry/setup/timetable`);
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const postChangeInquiryAddress = async (request, response) => {
+	if (request.errors) {
+		return renderInquiryAddress(request, response, 'change');
+	}
+
+	const { appealId } = request.currentAppeal;
+
+	if (request.body.addressKnown === 'yes') {
+		return response.redirect(
+			`/appeals-service/appeal-details/${appealId}/inquiry/change/address-details`
+		);
+	}
+
+	return response.redirect(`/appeals-service/appeal-details/${appealId}/inquiry/change/timetable`);
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const getInquiryAddressDetails = async (request, response) => {
+	const values = request.session['setUpInquiry'] || {};
+
+	return renderInquiryAddressDetails(request, response, values, 'setup');
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const getChangeInquiryAddressDetails = async (request, response) => {
+	const existingAddress = request.currentAppeal.inquiry.address;
+	const sessionValues = pick(request.session['changeInquiry'], [
+		'addressLine1',
+		'addressLine2',
+		'town',
+		'county',
+		'postCode'
+	]);
+	const values = isEmpty(sessionValues)
+		? { ...existingAddress, postCode: existingAddress?.postcode }
+		: sessionValues;
+
+	return renderInquiryAddressDetails(request, response, values, 'change');
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ * @param {import('@pins/appeals').Address} values
+ * @param {'setup' | 'change'} action
+ */
+export const renderInquiryAddressDetails = async (request, response, values, action) => {
+	const { currentAppeal, errors } = request;
+
+	const mappedPageContent = await addressDetailsPage(currentAppeal, action, values, errors);
+
+	return response.status(errors ? 400 : 200).render('patterns/change-page.pattern.njk', {
+		pageContent: mappedPageContent,
+		errors
+	});
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const postInquiryAddressDetails = async (request, response) => {
+	if (request.errors) {
+		const values = request.session['setUpInquiry'] || {};
+		return renderInquiryAddressDetails(request, response, values, 'setup');
+	}
+
+	const { appealId } = request.currentAppeal;
+
+	return response.redirect(`/appeals-service/appeal-details/${appealId}/inquiry/setup/timetable`);
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const postChangeInquiryAddressDetails = async (request, response) => {
+	if (request.errors) {
+		const values = request.session['changeInquiry'] || {};
+		return renderInquiryAddressDetails(request, response, values, 'change');
+	}
+
+	const { appealId } = request.currentAppeal;
+
+	return response.redirect(`/appeals-service/appeal-details/${appealId}/inquiry/change/timetable`);
 };
