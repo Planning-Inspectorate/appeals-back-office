28fa184030b4a6ed5232d90272e23469ef401a4f
lachlandow1 <lachlan.dow@solirius.com>
2025-06-23 16:07:09 +0100

    feat(web): update decision letter notify

--- appeals/api/src/server/endpoints/decision/decision.service.js
+++ appeals/api/src/server/endpoints/decision/decision.service.js
@@ -9,6 +9,8 @@ import { createAuditTrail } from '#endpoints/audit-trails/audit-trails.service.j
 import { AUDIT_TRAIL_DECISION_ISSUED } from '@pins/appeals/constants/support.js';
 import stringTokenReplacement from '#utils/string-token-replacement.js';
 import { AUDIT_TRAIL_CORRECTION_NOTICE_ADDED } from '@pins/appeals/constants/support.js';
+import { formatAddressSingleLine } from '#endpoints/addresses/addresses.formatter.js';
+import { getRepresentations } from '#endpoints/representations/representations.service.js';
 
 /** @typedef {import('@pins/appeals.api').Schema.Appeal} Appeal */
 /** @typedef {import('@pins/appeals.api').Schema.InspectorDecision} Decision */
@@ -92,10 +94,50 @@ export const publishDecision = async (
  * @param {Appeal} appeal
  * @param {string} correctionNotice
  * @param {string} azureUserId
+ * @param {Date} decisionDate
+ * @param { import('#endpoints/appeals.js').NotifyClient } notifyClient
  * @returns
  */
-export const sendNewDecisionLetter = async (appeal, correctionNotice, azureUserId) => {
-	//TODO: add notify emails
+export const sendNewDecisionLetter = async (
+	appeal,
+	correctionNotice,
+	azureUserId,
+	notifyClient,
+	decisionDate
+) => {
+	const representations = await getRepresentations(appeal.id, 1, 1000, {
+		representationType: ['comment']
+	});
+
+	const relevantEmails = [
+		appeal.agent?.email,
+		appeal.appellant?.email,
+		appeal.lpa?.email,
+		...representations.comments.map((comment) => comment.represented?.email).filter(Boolean)
+	];
+
+	const uniqueEmails = [...new Set(relevantEmails)].filter(Boolean);
+
+	const personalisation = {
+		appeal_reference_number: appeal.reference,
+		lpa_reference: appeal.applicationReference || '',
+		site_address: appeal.address
+			? formatAddressSingleLine(appeal.address)
+			: 'Address not available',
+		correction_notice_reason: correctionNotice,
+		decision_date: formatDate(decisionDate, false)
+	};
+
+	await Promise.all(
+		uniqueEmails.map(async (email) => {
+			await notifySend({
+				templateName: 'correction-notice-decision',
+				notifyClient,
+				recipientEmail: email,
+				personalisation
+			});
+		})
+	);
 
 	await createAuditTrail({
 		appealId: appeal.id,
---
+++ appeals/api/src/server/notify/templates/__tests__/correction-notice-decision.test.js
@@ -0,0 +1,69 @@
+import { notifySend } from '#notify/notify-send.js';
+import { jest } from '@jest/globals';
+
+describe('correction-notice-decision.md', () => {
+	test('should call notify sendEmail with the correct data', async () => {
+		const notifySendData = {
+			doNotMockNotifySend: true,
+			templateName: 'correction-notice-decision',
+			notifyClient: {
+				sendEmail: jest.fn()
+			},
+			recipientEmail: 'test@136s7.com',
+			personalisation: {
+				lpa_reference: '48269/APP/2021/1482',
+				appeal_reference_number: '134526',
+				site_address: '96 The Avenue, Leftfield, Maidstone, Kent, MD21 5XY, United Kingdom',
+				start_date: '01 January 2025',
+				existing_appeal_type: 'Householder',
+				correction_notice_reason: 'There has been a mistake - but we fixed it thanks',
+				decision_date: '01 January 2025'
+			},
+			appeal: {
+				id: 'mock-appeal-generic-id',
+				reference: '134526'
+			},
+			startDate: new Date('2025-01-01')
+		};
+
+		const expectedContent = [
+			'We have corrected the appeal decision letter.',
+			'',
+			'# Appeal details',
+			'',
+			'^Appeal reference number: 134526',
+			'Address: 96 The Avenue, Leftfield, Maidstone, Kent, MD21 5XY, United Kingdom',
+			'Planning application reference: 48269/APP/2021/1482',
+			'',
+			'# Why we corrected the appeal decision letter',
+			'',
+			'There has been a mistake - but we fixed it thanks',
+			'',
+			'[Sign in to our service](https://appeals-service-test.planninginspectorate.gov.uk/manage-appeals/134526) to view the decision letter dated 01 January 2025.',
+			'',
+			'# The Planning Inspectorate’s role',
+			'',
+			'The Planning Inspectorate cannot change or revoke the decision. Only the High Court can change this decision.',
+			'',
+			'# Feedback',
+			'',
+			'We welcome your feedback on our appeals service. Tell us on this short [feedback form](https://forms.office.com/pages/responsepage.aspx?id=mN94WIhvq0iTIpmM5VcIjfMZj__F6D9LmMUUyoUrZDZUOERYMEFBN0NCOFdNU1BGWEhHUFQxWVhUUy4u).',
+			'',
+			'The Planning Inspectorate',
+			'caseofficers@planninginspectorate.gov.uk'
+		].join('\n');
+
+		await notifySend(notifySendData);
+
+		expect(notifySendData.notifyClient.sendEmail).toHaveBeenCalledWith(
+			{
+				id: 'mock-appeal-generic-id'
+			},
+			'test@136s7.com',
+			{
+				content: expectedContent,
+				subject: 'We have corrected the appeal decision letter: 134526'
+			}
+		);
+	});
+});
