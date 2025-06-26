854682de23154c69d74f95aa30508e1542e7ac01
Hardie Brown <hardie.brown@solirius.com>
2025-06-26 14:07:56 +0100

    test(appeals): uncomment code in linkappeal.spec and casehistory.spec

--- appeals/e2e/cypress/e2e/back-office-appeals/linkAppeals.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/linkAppeals.spec.js
@@ -15,22 +15,25 @@ describe('link appeals', () => {
 		cy.login(users.appeals.caseAdmin);
 	});
 
-	// it('Link an unlinked appeal to an unlinked appeal', { tags: tag.smoke }, () => {
-	// 	cy.createCase().then((caseRef) => {
-	// 		cy.createCase().then((caseRefToLink) => {
-	// 			happyPathHelper.assignCaseOfficer(caseRef);
-	// 			caseDetailsPage.clickAccordionByButton('Overview');
-	// 			caseDetailsPage.clickAddLinkedAppeal();
-	// 			caseDetailsPage.fillInput(caseRefToLink);
-	// 			caseDetailsPage.clickButtonByText('Continue');
-	// 			caseDetailsPage.selectRadioButtonByValue(caseRef);
-	// 			caseDetailsPage.clickButtonByText('Continue');
-	// 			caseDetailsPage.clickButtonByText('Add linked appeal');
-	// 			caseDetailsPage.validateBannerMessage('Success', 'This appeal is now the lead for appeal ' + caseRefToLink);
-	// 			caseDetailsPage.checkStatusOfCase('Child', 1);
-	// 		});
-	// 	});
-	// });
+	it('Link an unlinked appeal to an unlinked appeal', { tags: tag.smoke }, () => {
+		cy.createCase().then((caseRef) => {
+			cy.createCase().then((caseRefToLink) => {
+				happyPathHelper.assignCaseOfficer(caseRef);
+				caseDetailsPage.clickAccordionByButton('Overview');
+				caseDetailsPage.clickAddLinkedAppeal();
+				caseDetailsPage.fillInput(caseRefToLink);
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.selectRadioButtonByValue(caseRef);
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.clickButtonByText('Add linked appeal');
+				caseDetailsPage.validateBannerMessage(
+					'Success',
+					'This appeal is now the lead for appeal ' + caseRefToLink
+				);
+				caseDetailsPage.checkStatusOfCase('Child', 1);
+			});
+		});
+	});
 
 	it('click on the first linked appeal', () => {
 		cy.createCase().then((caseRef) => {
