c788a86112f2304fadf097e64f15a41e23ede741
lachlandow1 <lachlan.dow@solirius.com>
2025-07-01 11:17:29 +0100

    feat(web): updating highlighting and focus for 2 date error fields

--- appeals/web/src/server/lib/mappers/utils/create-class-for-date-errors.js
+++ appeals/web/src/server/lib/mappers/utils/create-class-for-date-errors.js
@@ -1,69 +1,111 @@
 /**
- *
+ * @typedef {'day' | 'month' | 'year'} DateField
+ */
+
+/**
+ * @typedef {Object} DateClasses
+ * @property {string} day - CSS classes for the day input.
+ * @property {string} month - CSS classes for the month input.
+ * @property {string} year - CSS classes for the year input.
+ */
+
+/**
  * @param {import("@pins/express").ValidationErrors | undefined} errors
  * @param {String} fieldName
- * @returns
+ * @returns {DateClasses} Classes object for day, month, and year inputs.
  */
 export const assembleClassesFromErrors = (errors, fieldName = 'date') => {
-	let classes = {
-		day: 'govuk-input govuk-date-input__input',
-		month: 'govuk-input govuk-date-input__input',
-		year: 'govuk-input govuk-date-input__input'
+	const baseClass = 'govuk-input govuk-date-input__input';
+	const errorClass = ' govuk-input--error';
+
+	const classes = {
+		day: baseClass,
+		month: baseClass,
+		year: baseClass
 	};
-	if (errors) {
-		if (
-			errors[fieldName + 'day']?.param === 'all-fields' ||
-			errors[fieldName + 'month']?.param === 'all-fields' ||
-			errors[fieldName + 'year']?.param === 'all-fields'
-		) {
-			classes['day'] += ' govuk-input--error';
-			classes['month'] += ' govuk-input--error';
-			classes['year'] += ' govuk-input--error';
-		}
-		if (errors[fieldName + 'day']) {
-			classes['day'] += ' govuk-input--error';
-		}
-		if (errors[fieldName + 'month']) {
-			classes['month'] += ' govuk-input--error';
-		}
-		if (errors[fieldName + 'year']) {
-			classes['year'] += ' govuk-input--error';
-		}
+
+	if (!errors) return classes;
+
+	/** @type {DateField[]} */
+	const fields = ['day', 'month', 'year'];
+	/** @type {[DateField, DateField][]} */
+	const pairs = [
+		['day', 'month'],
+		['day', 'year'],
+		['month', 'year']
+	];
+
+	const hasAllFieldsError = fields.some(
+		(field) => errors[fieldName + field]?.param === 'all-fields'
+	);
+
+	if (hasAllFieldsError) {
+		fields.forEach((field) => (classes[field] += errorClass));
+		return classes;
 	}
+
+	fields.forEach((field) => {
+		if (errors[fieldName + field]) {
+			classes[field] += errorClass;
+
+			const param = errors[fieldName + field].param;
+			pairs.forEach(([field1, field2]) => {
+				if (param === `${field1}-${field2}`) {
+					classes[field1] += errorClass;
+					classes[field2] += errorClass;
+				}
+			});
+		}
+	});
+
 	return classes;
 };
 /**
- *
  * @param {import("@pins/express").ValidationErrors | undefined} errors
  * @param {String} fieldName
- * @returns
+ * @returns {DateClasses} Classes object for day, month, and year inputs.
  */
 export const assembleDocumentDateClassesFromErrors = (errors, fieldName = 'date') => {
-	let classes = {
-		day: 'govuk-input govuk-date-input__input',
-		month: 'govuk-input govuk-date-input__input',
-		year: 'govuk-input govuk-date-input__input'
+	const baseClass = 'govuk-input govuk-date-input__input';
+	const errorClass = ' govuk-input--error';
+
+	const classes = {
+		day: baseClass,
+		month: baseClass,
+		year: baseClass
 	};
-	if (errors && errors[fieldName]) {
-		if (
-			errors[fieldName]?.param === 'all-fields' ||
-			errors[fieldName]?.param === 'all-fields-day' ||
-			errors[fieldName]?.param === 'all-fields-month' ||
-			errors[fieldName]?.param === 'all-fields-year'
-		) {
-			classes['day'] += ' govuk-input--error';
-			classes['month'] += ' govuk-input--error';
-			classes['year'] += ' govuk-input--error';
-		}
-		if (errors[fieldName].param === 'day') {
-			classes['day'] += ' govuk-input--error';
-		}
-		if (errors[fieldName].param === 'month') {
-			classes['month'] += ' govuk-input--error';
-		}
-		if (errors[fieldName].param === 'year') {
-			classes['year'] += ' govuk-input--error';
-		}
+
+	if (!errors || !errors[fieldName]) return classes;
+
+	const { param } = errors[fieldName];
+
+	if (param?.startsWith('all-fields')) {
+		/** @type {DateField[]} */ (Object.keys(classes)).forEach((field) => {
+			classes[field] += errorClass;
+		});
+		return classes;
 	}
+
+	/** @type {DateField[]} */
+	const fields = ['day', 'month', 'year'];
+	fields.forEach((field) => {
+		if (param === field) {
+			classes[field] += errorClass;
+		}
+	});
+
+	/** @type {[DateField, DateField][]} */
+	const pairs = [
+		['day', 'month'],
+		['day', 'year'],
+		['month', 'year']
+	];
+	pairs.forEach(([field1, field2]) => {
+		if (param === `${field1}-${field2}`) {
+			classes[field1] += errorClass;
+			classes[field2] += errorClass;
+		}
+	});
+
 	return classes;
 };
