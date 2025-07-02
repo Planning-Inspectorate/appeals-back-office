cb2cb48ed40c28f2cac681afc398035674fe428a
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-07-02 15:13:45 +0100

    fix(web): fixnig old decision journey regressions
    feat(web): cleaning up and refactoring comments

--- appeals/web/src/server/appeals/appeal-details/status-tags/status-tags.mapper.js
+++ appeals/web/src/server/appeals/appeal-details/status-tags/status-tags.mapper.js
@@ -7,6 +7,8 @@ import { isStatePassed } from '#lib/appeal-status.js';
 import { mapDecisionOutcome } from '#appeals/appeal-details/issue-decision/issue-decision.utils.js';
 import { renderPageComponentsToHtml } from '#lib/nunjucks-template-builders/page-component-rendering.js';
 import { getFileVersionsInfo } from '#appeals/appeal-documents/appeal.documents.service.js';
+import config from '#environment/config.js';
+import { generateDecisionDocumentDownloadHtml } from '#lib/mappers/data/appeal/common.js';
 
 /**
  * @param {{ appeal: MappedInstructions }} mappedData
@@ -99,7 +101,9 @@ export const generateStatusTags = async (mappedData, appealDetails, request) => 
 
 		if (appealDetails.decision?.outcome || hasCostsAppellantDecision || hasCostsLpaDecision) {
 			insetTextRows.push(
-				`<a class="govuk-link" href="/appeals-service/appeal-details/${appealDetails.appealId}/issue-decision/view-decision">View decision</a>`
+				config.featureFlags.featureFlagIssueDecision
+					? getViewDecisionLink(appealDetails.appealId)
+					: getViewDecisionLinkOld(appealDetails)
 			);
 		}
 
@@ -193,3 +197,20 @@ export const generateStatusTags = async (mappedData, appealDetails, request) => 
 	// @ts-ignore
 	return statusTagsComponentGroup;
 };
+
+/**
+ * @param {import('../appeal-details.types.js').WebAppeal} appealDetails
+ * @returns {string}
+ */
+const getViewDecisionLinkOld = (appealDetails) => {
+	const decisionDownloadHtml = generateDecisionDocumentDownloadHtml(appealDetails, 'View decision');
+
+	if (decisionDownloadHtml.includes('Virus')) {
+		return `<span class="govuk-body">View decision </span>${decisionDownloadHtml}`;
+	}
+
+	return decisionDownloadHtml;
+};
+
+const getViewDecisionLink = (/** @type {number} */ appealId) =>
+	`<a class="govuk-link" href="/appeals-service/appeal-details/${appealId}/issue-decision/view-decision">View decision</a>`;
