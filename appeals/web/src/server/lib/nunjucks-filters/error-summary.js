beed9ae465ea30537c7a4ee9bbdeabe2b4e41bd6
jimbali <magicode@outlook.com>
2025-07-04 13:45:59 +0100

    feat(web): wip

--- appeals/web/src/server/lib/nunjucks-filters/error-summary.js
+++ appeals/web/src/server/lib/nunjucks-filters/error-summary.js
@@ -4,7 +4,7 @@ import { kebabCase } from '../../lib/nunjucks-filters/index.js';
 /**
  * Map keyed errors to an array compatible with govuk error summary.
  *
- * @param {string | Object<string, {msg: string}>} errors - A dictionary of errors.
+ * @param {string | Object<string, {msg: string | {text: string; fieldId: string}}>} errors - A dictionary of errors.
  * @returns {Array<{ text: string; href: string }>} – The error summary errors.
  */
 
@@ -17,8 +17,18 @@ export function mapToErrorSummary(errors) {
 			}
 		];
 	}
-	return Object.keys(errors).map((errorName) => ({
-		text: errorMessage(errors[errorName])?.text || '',
-		href: `#${kebabCase(errorName)}`
-	}));
+	return Object.keys(errors).map((errorName) => {
+		const error = errors[errorName];
+		if (typeof error?.msg === 'string') {
+			return {
+				href: `#${kebabCase(errorName)}`,
+				text: errorMessage({ msg: error?.msg })?.text || ''
+			};
+		} else {
+			return {
+				href: `#${kebabCase(error?.msg?.fieldId || errorName)}`,
+				text: errorMessage({ msg: error?.msg?.text })?.text || ''
+			};
+		}
+	});
 }
