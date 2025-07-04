8956c0a362e86125bf6893083e52ef8b97a0b651
Hardie Brown <hardie.brown@solirius.com>
2025-07-04 13:19:31 +0100

    test(appeals): s20 journey progressS20toDecision.spec

---
+++ appeals/e2e/cypress/e2e/back-office-appeals/progressS20ToDecision.spec.js
@@ -0,0 +1,84 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { CaseDetailsPage } from '../../page_objects/caseDetailsPage.js';
+import { ListCasesPage } from '../../page_objects/listCasesPage';
+import { DateTimeSection } from '../../page_objects/dateTimeSection';
+import { urlPaths } from '../../support/urlPaths.js';
+import { tag } from '../../support/tag';
+import { happyPathHelper } from '../../support/happyPathHelper.js';
+
+const listCasesPage = new ListCasesPage();
+const dateTimeSection = new DateTimeSection();
+const caseDetailsPage = new CaseDetailsPage();
+
+describe('Progress S20 to decision', () => {
+	beforeEach(() => {
+		cy.login(users.appeals.caseAdmin);
+	});
+
+	it(`Completes a S78 appeal to decision`, { tags: tag.smoke }, () => {
+		let todaysDate = new Date();
+
+		cy.createCase({
+			caseType: 'Y'
+		}).then((caseRef) => {
+			cy.addLpaqSubmissionToCase(caseRef);
+			happyPathHelper.assignCaseOfficer(caseRef);
+			caseDetailsPage.checkStatusOfCase('Validation', 0);
+
+			happyPathHelper.reviewAppellantCase(caseRef);
+			caseDetailsPage.checkStatusOfCase('Ready to start', 0);
+
+			happyPathHelper.startCase(caseRef);
+			caseDetailsPage.checkStatusOfCase('LPA questionnaire', 0);
+
+			happyPathHelper.reviewS78Lpaq(caseRef);
+			caseDetailsPage.checkStatusOfCase('Statements', 0);
+
+			happyPathHelper.addThirdPartyComment(caseRef, true);
+			caseDetailsPage.clickBackLink();
+			happyPathHelper.addThirdPartyComment(caseRef, false);
+			caseDetailsPage.clickBackLink();
+
+			happyPathHelper.addLpaStatement(caseRef);
+			cy.simulateStatementsDeadlineElapsed(caseRef);
+			cy.reload();
+
+			caseDetailsPage.basePageElements.bannerLink().click();
+			caseDetailsPage.clickButtonByText('Confirm');
+			caseDetailsPage.checkStatusOfCase('Final comments', 0);
+
+			happyPathHelper.addLpaFinalComment(caseRef);
+			cy.loadAppealDetails(caseRef).then((appealData) => {
+				const serviceUserId = ((appealData?.appellant?.serviceUserId ?? 0) + 200000000).toString();
+				happyPathHelper.addAppellantFinalComment(caseRef, serviceUserId);
+			});
+			cy.simulateFinalCommentsDeadlineElapsed(caseRef);
+			cy.reload();
+			caseDetailsPage.basePageElements.bannerLink().click();
+			caseDetailsPage.clickButtonByText('Share final comments');
+			caseDetailsPage.checkStatusOfCase('Event', 0);
+
+			happyPathHelper.setupSiteVisitFromBanner(caseRef);
+			cy.simulateSiteVisit(caseRef).then((caseRef) => {
+				cy.reload();
+			});
+			caseDetailsPage.clickIssueDecision(caseRef);
+			caseDetailsPage.selectRadioButtonByValue(caseDetailsPage.exactMatch('Allowed'));
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.uploadSampleFile(caseDetailsPage.sampleFiles.pdf);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.selectRadioButtonByValue('No');
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.selectRadioButtonByValue('No');
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.clickButtonByText('Issue Decision');
+			caseDetailsPage.validateBannerMessage('Success', 'Decision issued');
+			caseDetailsPage.checkStatusOfCase('Complete', 0);
+			caseDetailsPage.checkDecisionOutcome('Allowed');
+			caseDetailsPage.viewDecisionLetter('View decision');
+		});
+	});
+});
