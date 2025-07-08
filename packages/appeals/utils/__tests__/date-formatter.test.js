d4e5014c583d7f52dd686a796a1a6e097f6ee944
Gavin Lilley <Gavin.Lilley@planninginspectorate.gov.uk>
2025-07-08 16:25:48 +0100

    test(appeals): refactor date formatting tests

--- packages/appeals/utils/__tests__/date-formatter.test.js
+++ packages/appeals/utils/__tests__/date-formatter.test.js
@@ -1,6 +1,5 @@
 // @ts-nocheck
 import formatDate, { formatTime, formatTime12h } from '../date-formatter.js';
-import { zonedTimeToUtc } from 'date-fns-tz';
 
 describe('date-formatter', () => {
 	describe('formatDate', () => {
@@ -21,31 +20,52 @@ describe('date-formatter', () => {
 
 	describe('formatTime', () => {
 		const tests = [
-			{ date: '2024-07-11T10:00Z', expected: '10:00' },
-			{ date: '2024-04-05T23:15Z', expected: '23:15' },
-			{ date: '2024-11-30T15:00Z', expected: '15:00' },
-			{ date: '2024-02-20T15:00Z', expected: '15:00' },
-			{ date: '2024-12-24T20:00Z', expected: '20:00' },
-			{ date: '2024-09-03T20:00Z', expected: '20:00' }
+			{ date: '2024-07-11T10:00:00Z', expected: '11:00' },
+			{ date: '2024-04-05T23:15:00Z', expected: '00:15' },
+			{ date: '2024-11-30T15:00:00Z', expected: '15:00' },
+			{ date: '2024-02-20T15:00:00Z', expected: '15:00' },
+			{ date: '2024-12-24T20:00:00Z', expected: '20:00' },
+			{ date: '2024-09-03T20:00:00Z', expected: '21:00' },
+			{ date: '2024-11-30T23:59:59Z', expected: '23:59' },
+			{ date: '2024-12-01T00:00:00Z', expected: '00:00' },
+			{ date: '2024-12-01T00:00:01Z', expected: '00:00' }
 		];
 
 		it.each(tests)('formats time in 24h clock format', ({ date, expected }) => {
-			expect(formatTime(zonedTimeToUtc(date, 'Europe/London'))).toEqual(expected);
+			expect(formatTime(date, 'Europe/London', 'HH:mm')).toEqual(expected);
 		});
 	});
 
 	describe('formatTime12h', () => {
 		const tests = [
-			{ date: '2024-07-11T10:00Z', expected: '10:00am' },
-			{ date: '2024-04-05T23:15Z', expected: '11:15pm' },
-			{ date: '2024-11-30T15:00Z', expected: '3:00pm' },
-			{ date: '2024-02-20T15:00Z', expected: '3:00pm' },
-			{ date: '2024-12-24T20:00Z', expected: '8:00pm' },
-			{ date: '2024-09-03T20:00Z', expected: '8:00pm' }
+			{ date: '2024-07-11T10:00:00Z', expected: '11:00am' },
+			{ date: '2024-04-05T23:15:00Z', expected: '12:15am' },
+			{ date: '2024-11-30T15:00:00Z', expected: '3:00pm' },
+			{ date: '2024-02-20T15:00:00Z', expected: '3:00pm' },
+			{ date: '2024-12-24T20:00:00Z', expected: '8:00pm' },
+			{ date: '2024-09-03T20:00:00Z', expected: '9:00pm' },
+			{ date: '2024-11-30T23:59:59Z', expected: '11:59pm' },
+			{ date: '2024-12-01T00:00:00Z', expected: '12:00am' },
+			{ date: '2024-12-01T00:00:01Z', expected: '12:00am' },
+			{ date: '2024-12-01T11:59:59Z', expected: '11:59am' },
+			{ date: '2024-12-01T12:00:00Z', expected: '12:00pm' },
+			{ date: '2024-12-01T12:00:01Z', expected: '12:00pm' }
 		];
 
 		it.each(tests)('formats time in 24h clock format', ({ date, expected }) => {
-			expect(formatTime12h(zonedTimeToUtc(date, 'Europe/London'))).toEqual(expected);
+			expect(formatTime12h(date, 'Europe/London', 'h:mmaaa').toLowerCase()).toEqual(expected);
+		});
+	});
+
+	describe('Daylight saving time BST/GMT', () => {
+		it('handles clocks go forward correctly', () => {
+			expect(formatTime('2024-03-31T01:00Z', 'Europe/London', 'HH:mm')).toEqual('02:00');
+			expect(formatTime12h('2024-03-31T01:00Z', 'Europe/London', 'HH:mm')).toEqual('2:00am');
+		});
+
+		it('handles clocks go back correctly', () => {
+			expect(formatTime('2024-10-27T01:00Z', 'Europe/London', 'HH:mm')).toEqual('01:00');
+			expect(formatTime12h('2024-10-27T01:00Z', 'Europe/London', 'HH:mm')).toEqual('1:00am');
 		});
 	});
 });
