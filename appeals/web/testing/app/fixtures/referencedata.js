fa102165f708d81b5a1ce37e38b7fa2efc065f29
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-07-10 11:01:55 +0100

    fix(appeals): added statementofcommonground docs to folder and page

--- appeals/web/testing/app/fixtures/referencedata.js
+++ appeals/web/testing/app/fixtures/referencedata.js
@@ -527,6 +527,11 @@ export const appellantCaseDataNotValidated = {
 			documents: [],
 			folderId: 70457,
 			path: 'appellant-case/originalApplicationForm'
+		},
+		statementOfCommonGround: {
+			documents: [],
+			folderId: 70458,
+			path: 'appellant-case/statementOfCommonGround'
 		}
 	},
 	hasAdvertisedAppeal: null,
