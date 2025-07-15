b9917f2ca31d9c8b9e743ad0d466a3ed67e90bc8
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-07-15 19:20:47 +0100

    feat(appeals): fix test (a2-2603)

--- appeals/pdf-service-api/src/lib/nunjucks-filters/__tests__/format-date.test.js
+++ appeals/pdf-service-api/src/lib/nunjucks-filters/__tests__/format-date.test.js
@@ -9,12 +9,12 @@ describe('format-date', () => {
 	});
 
 	it('should format a specified date to the default format', () => {
-		const result = formatDate('2025-10-21 10:15');
-		expect(result).toEqual('21 October 2025');
+		const result = formatDate('2025-11-21 10:15');
+		expect(result).toEqual('21 November 2025');
 	});
 
 	it('should format a specified date to a custom format', () => {
-		const result = formatDate('2025-10-21 10:15', 'dd MMM yyyy HH:mm');
-		expect(result).toEqual('21 Oct 2025 10:15');
+		const result = formatDate('2025-11-21 10:15', 'dd MMM yyyy HH:mm');
+		expect(result).toEqual('21 Nov 2025 10:15');
 	});
 });
