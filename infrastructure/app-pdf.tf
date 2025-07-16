35428d77ea496e34f5e38b223fd92a129d170039
Dave Ferguson <dave.ferguson.g3@planninginspectorate.gov.uk>
2025-07-16 16:23:47 +0100

    feat(appeals): pdf app service

---
+++ infrastructure/app-pdf.tf
@@ -0,0 +1,54 @@
+module "app_pdf" {
+  #checkov:skip=CKV_TF_1: Use of commit hash are not required for our Terraform modules
+  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=1.49"
+
+  resource_group_name = azurerm_resource_group.primary.name
+  location            = module.primary_region.location
+
+  # naming
+  app_name        = "pdf"
+  resource_suffix = var.environment
+  service_name    = local.service_name
+  tags            = local.tags
+
+  # service plan
+  app_service_plan_id                  = azurerm_service_plan.apps.id
+  app_service_plan_resource_group_name = azurerm_resource_group.primary.name
+
+  # container
+  container_registry_name = var.tooling_config.container_registry_name
+  container_registry_rg   = var.tooling_config.container_registry_rg
+  image_name              = "back-office/back-office-appeals-pdf"
+
+  # networking
+  app_service_private_dns_zone_id = data.azurerm_private_dns_zone.app_service.id
+  endpoint_subnet_id              = azurerm_subnet.main.id
+  inbound_vnet_connectivity       = var.apps_config.private_endpoint_enabled
+  integration_subnet_id           = azurerm_subnet.apps.id
+  outbound_vnet_connectivity      = true
+  public_network_access           = !var.apps_config.private_endpoint_enabled
+
+  # monitoring
+  action_group_ids                  = local.action_group_ids
+  log_analytics_workspace_id        = azurerm_log_analytics_workspace.main.id
+  monitoring_alerts_enabled         = var.alerts_enabled
+  health_check_path                 = "/health"
+  health_check_eviction_time_in_min = var.health_check_eviction_time_in_min
+
+  app_settings = {
+    APPLICATIONINSIGHTS_CONNECTION_STRING      = local.key_vault_refs["app-insights-connection-string"]
+    ApplicationInsightsAgent_EXTENSION_VERSION = "~3"
+    NODE_ENV                                   = var.apps_config.node_environment
+    FRONT_OFFICE_URL                           = var.apps_config.front_office_url
+
+    # documents
+    BO_BLOB_CONTAINER       = azurerm_storage_container.appeal_documents.name
+    BO_BLOB_STORAGE_ACCOUNT = azurerm_storage_account.documents.primary_blob_endpoint # TODO: replace with custom domain
+
+  }
+
+  providers = {
+    azurerm         = azurerm
+    azurerm.tooling = azurerm.tooling
+  }
+}
