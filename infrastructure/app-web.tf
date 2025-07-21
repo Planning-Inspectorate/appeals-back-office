f6c02a9844a73178081da00363abee187249d5de
Dave Ferguson <dave.ferguson.g3@planninginspectorate.gov.uk>
2025-07-21 14:43:47 +0100

    feat(appeals): added pdf generaotor url

--- infrastructure/app-web.tf
+++ infrastructure/app-web.tf
@@ -45,7 +45,7 @@ module "app_web" {
     APP_HOSTNAME = var.web_app_domain
 
     # PDF generator host
-    PDF_SERVICE_URL               = module.app_pdf.default_site_hostname
+    PDF_SERVICE_HOST              = "https://${module.app_pdf.default_site_hostname}"
   
     # auth
     APPEALS_CASE_OFFICER_GROUP_ID = var.apps_config.auth.group_ids.case_officer
@@ -95,6 +95,9 @@ module "app_web" {
     FEATURE_FLAG_CHANGE_APPEAL_TYPE       = var.apps_config.featureFlags.featureFlagChangeAppealType
     FEATURE_FLAG_PDF_DOWNLOAD             = var.apps_config.featureFlags.featureFlagPdfDownload
     FEATURE_FLAG_NOTIFY_CASE_HISTORY      = var.apps_config.featureFlags.featureFlagNotifyCaseHistory
+
+    # PDF service host
+    PDF_SERVICE_HOST             = var.apps_config.pdf_service_host
 
     #change LPA
     USE_SYSTEM_TEST_BC_FOR_CHANGE_LPA = var.apps_config.use_system_test_bc_for_change_lpa
