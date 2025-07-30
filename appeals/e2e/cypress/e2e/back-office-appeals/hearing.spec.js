b11d11e52a71c1d202c6a17fa58158944cf5c1b9
Hardie Brown <hardie.brown@solirius.com>
2025-07-30 16:13:43 +0100

    test(appeals): fix for hearing cause by net residences

--- appeals/e2e/cypress/e2e/back-office-appeals/hearing.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/hearing.spec.js
@@ -538,7 +538,7 @@ describe('Setup hearing and add hearing estimates', () => {
 		cy.checkNotifySent(caseRef, expectedNotifies);
 	});
 
-	it('should progress hearing case to decision', () => {
+	it.only('should progress hearing case to decision', () => {
 		hearingSectionPage.deleteHearingIfExists(caseRef);
 		happyPathHelper.reviewLPaStatement(caseRef);
 
@@ -583,7 +583,8 @@ describe('Setup hearing and add hearing estimates', () => {
 			// navigate to details page
 			caseDetailsPage.clickLinkByText(caseRef);
 
-			caseDetailsPage.clickHearingBannerLink();
+			// caseDetailsPage.clickHearingBannerLink();
+			caseDetailsPage.clickHearingBannerAddressLink();
 			hearingSectionPage.addHearingLocationAddress(originalAddress);
 			hearingSectionPage.verifyHearingHeader(headers.hearing.checkDetails);
 			caseDetailsPage.clickButtonByText('Update hearing');
