a90a62adff55ba3af1091428251cfece0c477f00
Hardie Brown <hardie.brown@solirius.com>
2025-07-30 15:43:02 +0100

    test(appeals): add more scenarios to related appeals

--- appeals/e2e/cypress/e2e/back-office-appeals/relatedAppeals.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/relatedAppeals.spec.js
@@ -17,71 +17,71 @@ describe('related appeals', () => {
 		cy.login(users.appeals.caseAdmin);
 	});
 
-	// it('relate an unrelated appeal to an unrelated appeal', () => {
-	// 	cy.createCase().then((caseRef) => {
-	// 		cy.createCase().then((caseRefToLink) => {
-	// 			happyPathHelper.assignCaseOfficer(caseRef);
-	// 			caseDetailsPage.clickAccordionByButton('Overview');
-	// 			caseDetailsPage.clickAddRelatedAppeals();
-	// 			caseDetailsPage.fillInput(caseRefToLink);
-	// 			caseDetailsPage.clickButtonByText('Continue');
-	// 			caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
-	// 			caseDetailsPage.clickButtonByText('Continue');
-	// 			caseDetailsPage.validateBannerMessage(
-	// 				'Success',
-	// 				`This appeal is now related to ${caseRefToLink}`
-	// 			);
-	// 		});
-	// 	});
-	// });
-	// it('relate an unrelated appeal to a related appeal', () => {
-	// 	cy.createCase().then((caseRef) => {
-	// 		cy.createCase().then((firstCaseRefToLink) => {
-	// 			cy.createCase().then((secondCaseRefToLink) => {
-	// 				happyPathHelper.assignCaseOfficer(caseRef);
-	// 				caseDetailsPage.clickAccordionByButton('Overview');
-	// 				caseDetailsPage.clickAddRelatedAppeals();
-	// 				caseDetailsPage.fillInput(firstCaseRefToLink);
-	// 				caseDetailsPage.clickButtonByText('Continue');
-	// 				caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
-	// 				caseDetailsPage.clickButtonByText('Continue');
-	// 				caseDetailsPage.validateBannerMessage(
-	// 					'Success',
-	// 					`This appeal is now related to ${firstCaseRefToLink}`
-	// 				);
-	// 				caseDetailsPage.clickAddRelatedAppeals();
-	// 				caseDetailsPage.fillInput(secondCaseRefToLink);
-	// 				caseDetailsPage.clickButtonByText('Continue');
-	// 				caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
-	// 				caseDetailsPage.clickButtonByText('Continue');
-	// 				caseDetailsPage.validateBannerMessage(
-	// 					'Success',
-	// 					`This appeal is now related to ${secondCaseRefToLink}`
-	// 				);
-	// 			});
-	// 		});
-	// 	});
-	// });
-	// it('relate a BO appeal to a Horizon appeal', () => {
-	// 	const horizonAppealId =
-	// 		Cypress.config('apiBaseUrl').indexOf('test') > -1
-	// 			? horizonTestAppeals.horizonAppealTest
-	// 			: horizonTestAppeals.horizonAppealMock;
+	it('relate an unrelated appeal to an unrelated appeal', () => {
+		cy.createCase().then((caseRef) => {
+			cy.createCase().then((caseRefToLink) => {
+				happyPathHelper.assignCaseOfficer(caseRef);
+				caseDetailsPage.clickAccordionByButton('Overview');
+				caseDetailsPage.clickAddRelatedAppeals();
+				caseDetailsPage.fillInput(caseRefToLink);
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
+				caseDetailsPage.clickButtonByText('Continue');
+				caseDetailsPage.validateBannerMessage(
+					'Success',
+					`This appeal is now related to ${caseRefToLink}`
+				);
+			});
+		});
+	});
+	it('relate an unrelated appeal to a related appeal', () => {
+		cy.createCase().then((caseRef) => {
+			cy.createCase().then((firstCaseRefToLink) => {
+				cy.createCase().then((secondCaseRefToLink) => {
+					happyPathHelper.assignCaseOfficer(caseRef);
+					caseDetailsPage.clickAccordionByButton('Overview');
+					caseDetailsPage.clickAddRelatedAppeals();
+					caseDetailsPage.fillInput(firstCaseRefToLink);
+					caseDetailsPage.clickButtonByText('Continue');
+					caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
+					caseDetailsPage.clickButtonByText('Continue');
+					caseDetailsPage.validateBannerMessage(
+						'Success',
+						`This appeal is now related to ${firstCaseRefToLink}`
+					);
+					caseDetailsPage.clickAddRelatedAppeals();
+					caseDetailsPage.fillInput(secondCaseRefToLink);
+					caseDetailsPage.clickButtonByText('Continue');
+					caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
+					caseDetailsPage.clickButtonByText('Continue');
+					caseDetailsPage.validateBannerMessage(
+						'Success',
+						`This appeal is now related to ${secondCaseRefToLink}`
+					);
+				});
+			});
+		});
+	});
+	it('relate a BO appeal to a Horizon appeal', () => {
+		const horizonAppealId =
+			Cypress.config('apiBaseUrl').indexOf('test') > -1
+				? horizonTestAppeals.horizonAppealTest
+				: horizonTestAppeals.horizonAppealMock;
 
-	// 	cy.createCase().then((caseRef) => {
-	// 		happyPathHelper.assignCaseOfficer(caseRef);
-	// 		caseDetailsPage.clickAccordionByButton('Overview');
-	// 		caseDetailsPage.clickAddRelatedAppeals();
-	// 		caseDetailsPage.fillInput(horizonAppealId);
-	// 		caseDetailsPage.clickButtonByText('Continue');
-	// 		caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
-	// 		caseDetailsPage.clickButtonByText('Continue');
-	// 		caseDetailsPage.validateBannerMessage(
-	// 			'Success',
-	// 			`This appeal is now related to ${horizonAppealId}`
-	// 		);
-	// 	});
-	// });
+		cy.createCase().then((caseRef) => {
+			happyPathHelper.assignCaseOfficer(caseRef);
+			caseDetailsPage.clickAccordionByButton('Overview');
+			caseDetailsPage.clickAddRelatedAppeals();
+			caseDetailsPage.fillInput(horizonAppealId);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.selectRadioButtonByValue('Yes, relate this appeal to ' + caseRef);
+			caseDetailsPage.clickButtonByText('Continue');
+			caseDetailsPage.validateBannerMessage(
+				'Success',
+				`This appeal is now related to ${horizonAppealId}`
+			);
+		});
+	});
 	it('Relate a case in “validation” status to a case in “validation” status', () => {
 		cy.createCase().then((caseRef) => {
 			cy.createCase().then((caseRefToLink) => {
