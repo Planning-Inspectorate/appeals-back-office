3e75c184bb921273584a0002aac56eeb8487f695
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-08-01 16:25:34 +0100

    feat(appeals): added inquiry case overview case detail page

--- appeals/web/src/server/appeals/appeal-details/accordions/common/case-overview.js
+++ appeals/web/src/server/appeals/appeal-details/accordions/common/case-overview.js
@@ -1,17 +1,31 @@
+import featureFlags from '#common/feature-flags.js';
 import { removeSummaryListActions } from '#lib/mappers/index.js';
 import { isDefined } from '#lib/ts-utilities.js';
+import { APPEAL_TYPE } from '@pins/appeals/constants/common.js';
+import { FEATURE_FLAG_NAMES } from '@pins/appeals/constants/common.js';
+import { DOCUMENT_STATUS_RECEIVED } from '@pins/appeals/constants/support.js';
+import { APPEAL_CASE_PROCEDURE } from '@planning-inspectorate/data-model';
+
+/** @typedef {import('#appeals/appeal-details/appeal-details.types.d.ts').WebAppeal} WebAppeal */
 
 /**
  * @param {{appeal: MappedInstructions}} mappedData
+ * @param {WebAppeal} appealDetails
  * @returns {PageComponent}
  */
-export const getCaseOverview = (mappedData) => ({
+export const getCaseOverview = (mappedData, appealDetails) => ({
 	type: 'summary-list',
 	parameters: {
 		rows: [
+			showSiteAddressAndLpa(appealDetails)
+				? mappedData.appeal.siteAddress.display.summaryListItem
+				: undefined,
+			showSiteAddressAndLpa(appealDetails)
+				? mappedData.appeal.localPlanningAuthority.display.summaryListItem
+				: undefined,
 			removeSummaryListActions(mappedData.appeal?.lpaReference?.display.summaryListItem),
 			mappedData.appeal.appealType.display.summaryListItem,
-			mappedData.appeal?.caseProcedure?.display.summaryListItem?.actions.items[0]?.href
+			showProcedureTypeChangeLink(appealDetails)
 				? mappedData.appeal?.caseProcedure?.display.summaryListItem
 				: removeSummaryListActions(mappedData.appeal?.caseProcedure?.display.summaryListItem),
 			mappedData.appeal?.allocationDetails?.display.summaryListItem,
@@ -25,3 +39,41 @@ export const getCaseOverview = (mappedData) => ({
 		].filter(isDefined)
 	}
 });
+
+/**
+ * @param {import('#lib/appeal-status.js').WebAppeal} appealDetails
+ * @returns {boolean}
+ */
+const showSiteAddressAndLpa = (appealDetails) => {
+	return (
+		appealDetails.procedureType?.toLowerCase() === APPEAL_CASE_PROCEDURE.INQUIRY &&
+		appealDetails.appealType === APPEAL_TYPE.S78
+	);
+};
+
+/**
+ * @param {import('#lib/appeal-status.js').WebAppeal} appealDetails
+ * @returns {boolean}
+ */
+const showProcedureTypeChangeLink = (appealDetails) => {
+	const procedureTypes = [
+		FEATURE_FLAG_NAMES.SECTION_78_HEARING,
+		FEATURE_FLAG_NAMES.SECTION_78_INQUIRY,
+		FEATURE_FLAG_NAMES.SECTION_78
+	];
+
+	const activeFlags = procedureTypes.map((p) => {
+		return featureFlags.isFeatureActive(p);
+	});
+
+	const areMultipleFlagsActive = activeFlags.filter((x) => x === true).length >= 2;
+
+	if (
+		appealDetails.appealType !== APPEAL_TYPE.S78 ||
+		!areMultipleFlagsActive ||
+		appealDetails.documentationSummary.lpaStatement?.status === DOCUMENT_STATUS_RECEIVED
+	)
+		return false;
+
+	return true;
+};
--- appeals/web/src/server/lib/mappers/data/appeal/submappers/__tests__/case-procedure.mapper.test.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/__tests__/case-procedure.mapper.test.js
@@ -2,23 +2,7 @@
 import { mapCaseProcedure } from '#lib/mappers/data/appeal/submappers/case-procedure.mapper.js';
 import { APPEAL_CASE_PROCEDURE } from '@planning-inspectorate/data-model';
 import { APPEAL_TYPE } from '@pins/appeals/constants/common.js';
-import { jest } from '@jest/globals';
-import {
-	DOCUMENT_STATUS_RECEIVED,
-	DOCUMENT_STATUS_NOT_RECEIVED
-} from '@pins/appeals/constants/support.js';
-import config from '#environment/config.js';
-
-jest.mock('#environment/config.js', () => ({
-	__esModule: true,
-	default: {
-		featureFlags: {
-			featureFlagS78Written: true,
-			featureFlagS78Hearing: true,
-			featureFlagS78Inquiry: false
-		}
-	}
-}));
+import { DOCUMENT_STATUS_RECEIVED } from '@pins/appeals/constants/support.js';
 
 describe('case-procedure.mapper', () => {
 	let params;
@@ -41,9 +25,6 @@ describe('case-procedure.mapper', () => {
 			request: { originalUrl: '/original-url' }
 		};
 	});
-	afterEach(() => {
-		jest.clearAllMocks();
-	});
 
 	it('Should not display case procedure type if no timetable', () => {
 		params.appealDetails.appealTimetable = undefined;
@@ -63,7 +44,7 @@ describe('case-procedure.mapper', () => {
 								attributes: {
 									'data-cy': 'change-case-procedure'
 								},
-								href: '',
+								href: '/appeal-questionnaire/change-appeal-details/case-procedure',
 								text: 'Change',
 								visuallyHiddenText: 'Appeal procedure'
 							}
@@ -80,44 +61,5 @@ describe('case-procedure.mapper', () => {
 			},
 			id: 'case-procedure'
 		});
-	});
-
-	it('Should display href link because lpastatement status is not received', () => {
-		params.appealDetails.documentationSummary.lpaStatement.status = DOCUMENT_STATUS_NOT_RECEIVED;
-		const result = mapCaseProcedure(params);
-		expect(result.display.summaryListItem.actions.items[0].href).toEqual(
-			'/appeal-questionnaire/change-appeal-details/case-procedure'
-		);
-	});
-
-	it('Should not display href link because lpastatement status is received', () => {
-		const result = mapCaseProcedure(params);
-		expect(result.display.summaryListItem.actions.items[0].href).toEqual('');
-	});
-
-	it('Should not display href link because appeal type is not S78', () => {
-		params.appealDetails.appealType = APPEAL_TYPE.HOUSEHOLDER;
-		const result = mapCaseProcedure(params);
-		expect(result.display.summaryListItem.actions.items[0].href).toEqual('');
-	});
-
-	it('Should display href link because 2 of of 3 procedure type flags are true', () => {
-		config.featureFlags.featureFlagS78Written = true;
-		config.featureFlags.featureFlagS78Hearing = true;
-		config.featureFlags.featureFlagS78Inquiry = false;
-		params.appealDetails.documentationSummary.lpaStatement.status = DOCUMENT_STATUS_NOT_RECEIVED;
-		const result = mapCaseProcedure(params);
-		expect(result.display.summaryListItem.actions.items[0].href).toEqual(
-			'/appeal-questionnaire/change-appeal-details/case-procedure'
-		);
-	});
-
-	it('Should not display href link because only 1 of of 3 procedure type flags are true', () => {
-		config.featureFlags.featureFlagS78Written = false;
-		config.featureFlags.featureFlagS78Hearing = true;
-		config.featureFlags.featureFlagS78Inquiry = false;
-		params.appealDetails.documentationSummary.lpaStatement.status = DOCUMENT_STATUS_NOT_RECEIVED;
-		const result = mapCaseProcedure(params);
-		expect(result.display.summaryListItem.actions.items[0].href).toEqual('');
 	});
 });
