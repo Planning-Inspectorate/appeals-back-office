3bf358ebfe73aedd76ca837b18322cbeeafd39e7
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-08-04 15:06:01 +0100

    fix(appeals): small refactor for awaiting appeals when statuses are different

--- appeals/api/src/server/utils/is-awaiting-linked-appeal.js
+++ appeals/api/src/server/utils/is-awaiting-linked-appeal.js
@@ -8,6 +8,9 @@ import { APPEAL_CASE_STATUS } from '@planning-inspectorate/data-model';
  * @returns {boolean}
  */
 export const isAwaitingLinkedAppeal = (appeal, linkedAppeals) => {
+	if (!allLinkedAppealsHaveSameAppealStatus(appeal, linkedAppeals)) {
+		return true;
+	}
 	switch (currentStatus(appeal)) {
 		case APPEAL_CASE_STATUS.LPA_QUESTIONNAIRE: {
 			const validationOutcome =
@@ -33,9 +36,45 @@ export const allLpaQuestionnaireOutcomesAreComplete = (linkedAppeals) => {
 		return true;
 	}
 	// @ts-ignore
-	return linkedAppeals.every((appeal) => {
+	return linkedAppeals.every((linkedAppeal) => {
+		// Make sure the linked appeal is the actual appeal and not a wrapper
+		const appeal = linkedAppeal.appeal || linkedAppeal;
 		const validationOutcome =
 			appeal.lpaQuestionnaire?.lpaQuestionnaireValidationOutcome?.name?.toLowerCase();
 		return validationOutcome === 'complete';
+	});
+};
+
+/**
+ *
+ * @param {*} appeal
+ * @param {*[]}linkedAppeals
+ * @returns {*|boolean}
+ */
+const allLinkedAppealsHaveSameAppealStatus = (appeal, linkedAppeals) => {
+	if (!linkedAppeals.length) {
+		return true;
+	}
+
+	const appealStatus = currentStatus(appeal);
+
+	return linkedAppeals.every((linkedAppeal) => {
+		// If the linked appeal is an appeal wrapper
+		if (linkedAppeal.appeal) {
+			//@ts-ignore
+			return linkedAppeal.appeal.appealStatus.find((item) => item.status === appealStatus);
+		}
+		if (linkedAppeal.child && linkedAppeal.parent) {
+			return (
+				//@ts-ignore
+				linkedAppeal.child.appealStatus.find((item) => item.status === appealStatus) &&
+				//@ts-ignore
+				linkedAppeal.parent.appealStatus.find((item) => item.status === appealStatus)
+			);
+		}
+		return (
+			appealStatus === linkedAppeal.currentStatus ||
+			linkedAppeal.completedStateList.includes(appealStatus)
+		);
 	});
 };
