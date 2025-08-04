95ec71acd134092fe85059f164cf225a77ba9365
Praveen Murugan <praveenmurugan743@gmail.com>
2025-08-04 13:37:15 +0100

    fix(web): parse appeal ID as a number to prevent server-side request forgery

--- appeals/web/src/server/appeals/appeal-details/linked-appeals/linked-appeals.service.js
+++ appeals/web/src/server/appeals/appeal-details/linked-appeals/linked-appeals.service.js
@@ -6,8 +6,9 @@
  * @returns {Promise<{}>}
  */
 export function postUnlinkRequest(apiClient, appealId, relationshipId) {
+	const appealIdNumber = parseInt(appealId, 10);
 	return apiClient
-		.delete(`appeals/${appealId}/unlink-appeal`, {
+		.delete(`appeals/${appealIdNumber}/unlink-appeal`, {
 			json: { relationshipId }
 		})
 		.json();
