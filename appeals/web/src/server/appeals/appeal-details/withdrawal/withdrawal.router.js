e65714e9b637ccb4cf4ad8ead36ccadab68c4a6a
lachlandow1 <lachlan.dow@solirius.com>
2025-08-06 16:15:57 +0100

    feat(web): creating new withdrawal flow (A2-4046 & A2-3972)

--- appeals/web/src/server/appeals/appeal-details/withdrawal/withdrawal.router.js
+++ appeals/web/src/server/appeals/appeal-details/withdrawal/withdrawal.router.js
@@ -1,38 +1,19 @@
 import { Router as createRouter } from 'express';
 import { asyncHandler } from '@pins/express';
 import * as controller from './withdrawal.controller.js';
-import * as validators from './withdrawal.validators.js';
-import { extractAndProcessDateErrors } from '#lib/validators/date-input.validator.js';
-import { dateFieldNamePrefix } from './withdrawl.constants.js';
 
 const router = createRouter({ mergeParams: true });
 
 router.route('/view').get(asyncHandler(controller.getViewWithdrawalDocumentFolder));
 
 router
-	.route('/start')
+	.route('/new')
 	.get(asyncHandler(controller.getWithdrawalRequestUpload))
 	.post(asyncHandler(controller.postWithdrawalRequestRequestUpload));
 
 router
-	.route('/withdrawal-request-date')
-	.get(asyncHandler(controller.getDateWithdrawalRequest))
-	.post(
-		validators.validateRequestDateFields,
-		validators.validateRequestDateValid,
-		validators.validateDueDateInPastOrToday,
-		extractAndProcessDateErrors({ fieldNamePrefix: dateFieldNamePrefix }),
-		asyncHandler(controller.postDateWithdrawalRequest)
-	);
-
-router
-	.route('/redaction-status')
-	.get(asyncHandler(controller.getRedactionStatus))
-	.post(validators.validateRedactionStatus, asyncHandler(controller.postRedactionStatus));
-
-router
-	.route('/check-your-answers')
+	.route('/check-details')
 	.get(asyncHandler(controller.getCheckYourAnswers))
-	.post(validators.validateCheckYourAnswers, asyncHandler(controller.postCheckYourAnswers));
+	.post(asyncHandler(controller.postCheckYourAnswers));
 
 export default router;
