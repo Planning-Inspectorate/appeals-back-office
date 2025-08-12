64f7806cc3e25cfc0447d1bf2933f4f04691b765
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-08-12 13:48:40 +0100

    feat(appeals): added accept review proof of evidence functionality

--- appeals/web/src/server/appeals/appeal-details/representations/proof-of-evidence/proof-of-evidence.service.js
+++ appeals/web/src/server/appeals/appeal-details/representations/proof-of-evidence/proof-of-evidence.service.js
@@ -1,22 +1,11 @@
 /**
  * @param {import('got').Got} apiClient
- * @param {string} appealId
- * @param {string} commentsId
- * */
-export const getFinalComments = (apiClient, appealId, commentsId) =>
-	apiClient.get(`appeals/${appealId}/reps/${commentsId}`).json();
-
-export const DOCUMENT_STAGE = 'representation';
-export const DOCUMENT_TYPE = 'representationAttachments';
-
-/**
- * @param {import('got').Got} apiClient
  * @param {number | string} appealId
  * @returns {Promise<import('@pins/appeals.api').Appeals.FolderInfo>}
  * */
 export const getAttachmentsFolder = async (apiClient, appealId) => {
 	const folders = await apiClient
-		.get(`appeals/${appealId}/document-folders?path=${DOCUMENT_STAGE}/${DOCUMENT_TYPE}`)
+		.get(`appeals/${appealId}/document-folders?path=representation/representationAttachments`)
 		.json();
 	if (!(folders && folders.length > 0)) {
 		throw new Error(`failed to find folder for appeal ID ${appealId}`);
