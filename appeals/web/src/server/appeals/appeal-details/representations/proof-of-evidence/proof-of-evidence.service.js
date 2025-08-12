1f713c772fc60b506bf6fe76d0c6177c966b0303
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-08-12 13:55:26 +0100

    feat(appeals): removed unused function

--- appeals/web/src/server/appeals/appeal-details/representations/proof-of-evidence/proof-of-evidence.service.js
+++ appeals/web/src/server/appeals/appeal-details/representations/proof-of-evidence/proof-of-evidence.service.js
@@ -1,3 +1,6 @@
+export const DOCUMENT_STAGE = 'representation';
+export const DOCUMENT_TYPE = 'representationAttachments';
+
 /**
  * @param {import('got').Got} apiClient
  * @param {number | string} appealId
@@ -5,7 +8,7 @@
  * */
 export const getAttachmentsFolder = async (apiClient, appealId) => {
 	const folders = await apiClient
-		.get(`appeals/${appealId}/document-folders?path=representation/representationAttachments`)
+		.get(`appeals/${appealId}/document-folders?path=${DOCUMENT_STAGE}/${DOCUMENT_TYPE}`)
 		.json();
 	if (!(folders && folders.length > 0)) {
 		throw new Error(`failed to find folder for appeal ID ${appealId}`);
