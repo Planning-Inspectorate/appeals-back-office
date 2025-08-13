265a37ad18b1889d0c54a1cbfabb795406981650
Medini-Aithal <Medini.Aithal.ZC@planninginspectorate.gov.uk>
2025-08-13 13:37:07 +0100

    feat(infrastructure): adding staging environment for appeals back office

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -3,7 +3,7 @@ resource "azurerm_servicebus_namespace" "main" {
   #checkov:skip=CKV_AZURE_201: TODO: Ensure that Azure Service Bus uses a customer-managed key to encrypt data
   #checkov:skip=CKV_AZURE_204: public network access only enabled in dev
   name                = var.environment == "staging" ? "${local.org}-sb-${local.service_name}-test" : "${local.org}-sb-${local.resource_suffix}"
-  resource_group_name = azurerm_resource_group.primary.name
+  resource_group_name = var.environment == "staging" ? "${local.org}-rg-${local.service_name}-test" : azurerm_resource_group.primary.name
   location            = module.primary_region.location
 
   sku                          = var.service_bus_config.sku
