5ae89efdc9398758e4ac6a4cba8e4ba15a6efdf9
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-08-13 13:25:05 +0100

    feat(web): adding case history update and state transition for cancel-invali appeal

--- packages/appeals/constants/support.js
+++ packages/appeals/constants/support.js
@@ -86,6 +86,7 @@ export const AUDIT_TRAIL_LPAQ_IMPORT_MSG = 'The LPA questionnaire was received';
 export const AUDIT_TRAIL_REP_IMPORT_MSG = '{replacement0} was received';
 export const AUDIT_TRAIL_PROGRESSED_TO_STATUS = 'The case has progressed to {replacement0}';
 export const AUDIT_TRAIL_SUBMISSION_INCOMPLETE = 'The {replacement0} was marked as incomplete';
+export const AUDIT_TRAIL_SUBMISSION_INVALID = 'Appeal marked as invalid:';
 export const AUDIT_TRAIL_REMOVED_CASE_OFFICER =
 	'The case officer {replacement0} was removed from the team';
 export const AUDIT_TRAIL_REMOVED_INSPECTOR =
