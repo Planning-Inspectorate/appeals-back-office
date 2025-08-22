8826a9147e362351b7dd877bf2e92e725ff20370
Hardie Brown <hardie.brown@solirius.com>
2025-08-22 15:18:50 +0100

    test(appeals): progress s20 fix

--- appeals/e2e/cypress/e2e/back-office-appeals/progressS20ToDecision.spec.js
+++ appeals/e2e/cypress/e2e/back-office-appeals/progressS20ToDecision.spec.js
@@ -48,7 +48,7 @@ describe('Progress S20 to decision', () => {
 
 			caseDetailsPage.basePageElements.bannerLink().click();
 			caseDetailsPage.clickButtonByText('Confirm');
-			caseDetailsPage.checkStatusOfCase('Site visit ready to set up', 0);
+			caseDetailsPage.checkStatusOfCase('Final comments', 0);
 
 			happyPathHelper.addLpaFinalComment(caseRef);
 			cy.loadAppealDetails(caseRef).then((appealData) => {
@@ -59,7 +59,7 @@ describe('Progress S20 to decision', () => {
 			cy.reload();
 			caseDetailsPage.basePageElements.bannerLink().click();
 			caseDetailsPage.clickButtonByText('Share final comments');
-			caseDetailsPage.checkStatusOfCase('Event', 0);
+			caseDetailsPage.checkStatusOfCase('Site visit ready to set up', 0);
 
 			happyPathHelper.setupSiteVisitFromBanner(caseRef);
 			cy.simulateSiteVisit(caseRef).then((caseRef) => {
