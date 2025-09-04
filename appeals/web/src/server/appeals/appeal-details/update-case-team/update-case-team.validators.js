431a80d3f48c123b427ba20fb618fc042f399a16
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-09-04 13:30:48 +0100

    feat(web): adding autocomplete feature to case team page and ui updates
    feat(web): fixing unit tests
    
    feat(web): adding back the unassign caseteam option

--- appeals/web/src/server/appeals/appeal-details/update-case-team/update-case-team.validators.js
+++ appeals/web/src/server/appeals/appeal-details/update-case-team/update-case-team.validators.js
@@ -2,5 +2,5 @@ import { createValidator } from '@pins/express';
 import { body } from 'express-validator';
 
 export const validateCaseTeamSelection = createValidator(
-	body('case-team').trim().notEmpty().withMessage('Select the case team')
+	body('case-team').trim().notEmpty().withMessage('Enter a team name or team email address')
 );
