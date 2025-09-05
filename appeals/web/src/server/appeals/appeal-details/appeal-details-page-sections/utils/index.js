c9fcadb8e18a7238cb15b919ba719afd7e091efc
lachlandow1 <lachlan.dow@solirius.com>
2025-09-05 16:26:39 +0100

    feat(web): renaming things

--- appeals/web/src/server/appeals/appeal-details/appeal-details-page-sections/utils/index.js
+++ appeals/web/src/server/appeals/appeal-details/appeal-details-page-sections/utils/index.js
@@ -1,2 +1,2 @@
 export { mapStatusDependentNotifications } from '#lib/mappers/utils/map-status-dependent-notifications.js';
-export { removeAppealDetailsSectionComponentsActions } from './remove-accordion-components-actions.js';
+export { removeAppealDetailsSectionComponentsActions } from './remove-appeal-details-sections-components-actions.js';
