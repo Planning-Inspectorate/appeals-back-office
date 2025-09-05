2b2563a49825826766afd6e1190c2f850a973a24
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2025-09-05 14:36:28 +0100

    feat(infrastructure): adding staging environment for appeals back office

--- appeals/functions/casedata-import/appellant-case/config.js
+++ appeals/functions/casedata-import/appellant-case/config.js
@@ -1,15 +1,6 @@
 import { loadEnvironment } from '@pins/platform';
 import joi from 'joi';
 
-module.exports = {
-	AZURE: {
-		BO_SERVICEBUS: {
-			TOPIC_APPELLANT_CASE: process.env.SB_TOPIC_NAME_APPEAL_FO_APPELLANT_SUBMISSION || 'appeal-fo-appellant-submission',
-			SUB_APPELLANT_CASE: process.env.SB_SUBSCRIPTION_NAME_APPEAL_FO_APPELLANT_SUBMISSION || 'appeal-fo-appellant-submission-sub'
-		}
-	}
-};
-
 const schema = joi.object({
 	NODE_ENV: joi.string().valid('development', 'production', 'test'),
 	API_HOST: joi.string()
