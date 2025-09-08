21a0fd9cc94bfa804c34d1950cd8e74c339edd52
lachlandow1 <lachlan.dow@solirius.com>
2025-09-08 10:57:02 +0100

    feat(api): update to no purge seed data to update lpa and team assignments (A2-3719)

--- appeals/api/src/database/seed/seed-no-purge.js
+++ appeals/api/src/database/seed/seed-no-purge.js
@@ -1,5 +1,10 @@
 import { databaseConnector } from '../../server/utils/database-connector.js';
 import { seedStaticData } from './data-static.js';
+import { localPlanningDepartmentList } from './LPAs/dev.js';
+import { mapLpasToTeams } from './map-lpa-and-teams.js';
+import { seedLPAs } from './seed-lpas.js';
+import { seedTeams } from './seed-teams.js';
+import { lpaTeamAssignments, teamsToCreate } from './teams/dev.js';
 
 /**
  * Clear the dev database, then add in the static and test data
@@ -10,6 +15,9 @@ import { seedStaticData } from './data-static.js';
 const seedWithoutPurge = async () => {
 	try {
 		await seedStaticData(databaseConnector);
+		await seedTeams(databaseConnector, teamsToCreate);
+		const mappedLPAs = mapLpasToTeams(localPlanningDepartmentList, lpaTeamAssignments);
+		await seedLPAs(databaseConnector, mappedLPAs);
 	} catch (error) {
 		console.error(error);
 		throw error;
