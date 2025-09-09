2e8d0f2e4332391683d48c0a2b043f8bd90d149a
lachlandow1 <lachlan.dow@solirius.com>
2025-09-09 10:42:17 +0100

    feat(api): update training seed data to include production data (A2-4373)

--- appeals/api/src/database/seed/seed-training.js
+++ appeals/api/src/database/seed/seed-training.js
@@ -4,8 +4,36 @@ import { localPlanningDepartmentList } from './LPAs/training.js';
 import { mapLpasToTeams } from './map-lpa-and-teams.js';
 import { seedLPAs } from './seed-lpas.js';
 import { seedTeams } from './seed-teams.js';
-import { lpaTeamAssignments, teamsToCreate } from './teams/training.js';
+import {
+	teamsToCreate as pordTeamsToCreate,
+	lpaTeamAssignments as prodLpaTeamAssignments
+} from './teams/prod.js';
+import {
+	lpaTeamAssignments as trainingLpaTeamAssignments,
+	teamsToCreate as trainingTeamsToCreate
+} from './teams/training.js';
 
+/** @typedef {import('#utils/db-client/index.js').Prisma.TeamCreateInput} Team */
+/** @typedef {Record<string, number|null>} lpaTeamAssignments */
+
+/**
+ *
+ * @param {lpaTeamAssignments} lpaAssignments1
+ * @param {lpaTeamAssignments} lpaAssignments2
+ * @param {Team[]} teamsToCreate1
+ * @param {Team[]} teamsToCreate2
+ */
+const combineTeamsAndTeamAssignments = (
+	lpaAssignments1,
+	lpaAssignments2,
+	teamsToCreate1,
+	teamsToCreate2
+) => {
+	return {
+		lpaAssignments: { ...lpaAssignments1, ...lpaAssignments2 },
+		teamsToCreate: [...teamsToCreate1, ...teamsToCreate2]
+	};
+};
 /**
  * Seed the training database with the required static data
  *
@@ -14,9 +42,15 @@ import { lpaTeamAssignments, teamsToCreate } from './teams/training.js';
  */
 const seedTraining = async () => {
 	try {
+		const { lpaAssignments, teamsToCreate } = combineTeamsAndTeamAssignments(
+			trainingLpaTeamAssignments,
+			prodLpaTeamAssignments,
+			trainingTeamsToCreate,
+			pordTeamsToCreate
+		);
 		await seedStaticData(databaseConnector);
 		await seedTeams(databaseConnector, teamsToCreate);
-		const mappedLPAs = mapLpasToTeams(localPlanningDepartmentList, lpaTeamAssignments);
+		const mappedLPAs = mapLpasToTeams(localPlanningDepartmentList, lpaAssignments);
 		await seedLPAs(databaseConnector, mappedLPAs);
 	} catch (error) {
 		console.error(error);
--- appeals/api/src/database/seed/teams/training.js
+++ appeals/api/src/database/seed/teams/training.js
@@ -1,8 +1,8 @@
 export const teamsToCreate = [
-	{ id: 1, name: 'Ops Test', email: 'opstest@planninginspectorate.co.uk' }
+	{ id: 10000001, name: 'Ops Test', email: 'opstest@planninginspectorate.co.uk' }
 ];
 export const lpaTeamAssignments = {
 	// Ops Test
-	Q9999: 1, // System Test Borough Council
-	Q1111: 1 // System Test Borough Council 2
+	Q9999: 10000001, // System Test Borough Council
+	Q1111: 10000001 // System Test Borough Council 2
 };
