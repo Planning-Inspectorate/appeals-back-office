ddb4eb889a40bc53df294aa7bf028b551ad14cd7
Marc Podgorney <marc.podgorney.27@planninginspectorate.gov.uk>
2025-09-11 15:00:43 +0100

    test(appeals): updating existing page objects

--- appeals/e2e/cypress/page_objects/caseDetailsPage.js
+++ appeals/e2e/cypress/page_objects/caseDetailsPage.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import { formatDateAndTime } from '../support/utils/formatDateAndTime';
+import { formatDateAndTime } from '../support/utils/format';
 import { Page } from './basePage';
 import { DateTimeSection } from './dateTimeSection.js';
 
