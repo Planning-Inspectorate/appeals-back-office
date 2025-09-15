3ad70ace52b1a96833e27669dde286b3fe0f3073
Michael Turner <109961022+pinsMT@users.noreply.github.com>
2025-09-15 10:05:31 +0100

    feat(web): correcting validators and back-link (a2-3834)

--- appeals/web/src/server/lib/validators/checkbox-text-items.validator.js
+++ appeals/web/src/server/lib/validators/checkbox-text-items.validator.js
@@ -54,7 +54,7 @@ export const createCheckboxTextItemsValidator = (checkboxIdsBodyKey) => {
  * @returns {boolean}
  * @throws {Error} If textItem is empty.
  */
-const textItemCustomValidator = (textItem) => {
+export const textItemCustomValidator = (textItem) => {
 	if (typeof textItem !== 'string' || textItem.trim().length === 0) {
 		throw new Error('Enter a reason');
 	}
