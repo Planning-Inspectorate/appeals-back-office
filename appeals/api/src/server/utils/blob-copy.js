6edcb90fcdcb20f165fedf13c53574104c9feb7f
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-09-16 16:14:47 +0100

    fix(appeals): use an event to move blobs between appeals (a2-4408) - patch for release

--- appeals/api/src/server/utils/blob-copy.js
+++ appeals/api/src/server/utils/blob-copy.js
@@ -1,7 +1,10 @@
 import config from '#config/config.js';
+import { eventClient } from '#infrastructure/event-client.js';
+import { producers } from '#infrastructure/topics.js';
 import logger from '#utils/logger.js';
 import { BlobServiceClient } from '@azure/storage-blob';
 import { BlobStorageClient } from '@pins/blob-storage-client';
+import { EventType } from '@pins/event-client';
 
 /**
  * Copies blobs from one location to another
@@ -9,11 +12,50 @@ import { BlobStorageClient } from '@pins/blob-storage-client';
  * @returns {Promise<Promise<Awaited<unknown>[]> | Promise<{[p: string]: Awaited<*>, [p: number]: Awaited<*>, [p: symbol]: Awaited<*>}>>}
  */
 export const copyBlobs = async (copyList) => {
-	const storageContainer = config.BO_BLOB_CONTAINER;
-	const storageClient = config.useBlobEmulator
-		? new BlobStorageClient(new BlobServiceClient(config.blobEmulatorSasUrl))
-		: BlobStorageClient.fromUrl(config.BO_BLOB_STORAGE_ACCOUNT);
+	if (config.useBlobEmulator) {
+		await copyBlobsUsingEmulator(copyList);
+	}
 
+	return copyBlobsUsingAzure(copyList);
+};
+
+/**
+ * Copies blobs from one location to another using Azure event client
+ * @param {{sourceBlobName:  string | null | undefined, destinationBlobName: string}[]} copyList
+ * @returns {Promise<Promise<Awaited<unknown>[]> | Promise<{[p: string]: Awaited<*>, [p: number]: Awaited<*>, [p: symbol]: Awaited<*>}>>}
+ */
+const copyBlobsUsingAzure = async (copyList) => {
+	const messages = copyList
+		.map((copyDetails) => {
+			const { sourceBlobName, destinationBlobName } = copyDetails;
+			if (!sourceBlobName || !destinationBlobName) {
+				return null;
+			}
+			const container = `${config.BO_BLOB_STORAGE_ACCOUNT}${config.BO_BLOB_CONTAINER}`;
+			return {
+				originalURI: `${container}/${sourceBlobName}`,
+				importedURI: `${container}/${destinationBlobName}`
+			};
+		})
+		.filter((message) => message !== null);
+	if (messages.length > 0) {
+		const topic = producers.boBlobMove;
+		const res = await eventClient.sendEvents(topic, messages, EventType.Create);
+		if (res) {
+			return Promise.resolve(res);
+		}
+	}
+	return Promise.resolve([]);
+};
+
+/**
+ * Copies blobs from one location to another within the blob emulator
+ * @param {{sourceBlobName:  string | null | undefined, destinationBlobName: string}[]} copyList
+ * @returns {Promise<Promise<Awaited<unknown>[]> | Promise<{[p: string]: Awaited<*>, [p: number]: Awaited<*>, [p: symbol]: Awaited<*>}>>}
+ */
+const copyBlobsUsingEmulator = async (copyList) => {
+	const storageContainer = config.BO_BLOB_CONTAINER;
+	const storageClient = new BlobStorageClient(new BlobServiceClient(config.blobEmulatorSasUrl));
 	return Promise.all(
 		// @ts-ignore
 		copyList.map(async (copyDetails) => {
