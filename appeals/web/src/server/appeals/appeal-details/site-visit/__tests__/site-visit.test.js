d1649f2b083bfa771ecb7bdd32b9f3570d8b59a6
lachlandow1 <lachlan.dow@solirius.com>
2025-09-29 12:30:18 +0100

    feat(appeals): add record missed site visit CYA page (A2-4232)

--- appeals/web/src/server/appeals/appeal-details/site-visit/__tests__/site-visit.test.js
+++ appeals/web/src/server/appeals/appeal-details/site-visit/__tests__/site-visit.test.js
@@ -27,6 +27,26 @@ const template = {
 		'',
 		'The Planning Inspectorate',
 		'caseofficers@planninginspectorate.gov.uk'
+	]
+};
+const appellantEmailTemplate = {
+	renderedHtml: [
+		`<div class="pins-notify-preview-border">`,
+		`<h2>Appeal details</h2>`,
+		`<div class="govuk-inset-text">`,
+		`  Appeal reference number: 134526 <br>`,
+		`  Address: 96 The Avenue, Leftfield, Maidstone, Kent, MD21 5XY, United Kingdom<br>`,
+		`  Planning application reference: 48269/APP/2021/1482<br>`,
+		`</div>`,
+		`<h2>Appeal withdrawn</h2>`,
+		`We have withdrawn the appeal following your request on 01 January 2025.<br><br>`,
+		`<h2>What happens next</h2>`,
+		`We have closed the appeal and cancelled the site visit.<br><br>`,
+		`<h2>Feedback</h2>`,
+		`We welcome your feedback on our appeals process. Tell us on this short <a href="https://forms.office.com/pages/responsepage.aspx?id=mN94WIhvq0iTIpmM5VcIjfMZj__F6D9LmMUUyoUrZDZUOERYMEFBN0NCOFdNU1BGWEhHUFQxWVhUUy4u" class="govuk-link">feedback form</a>.<br><br>`,
+		`The Planning Inspectorate<br><br>`,
+		`caseofficers@planninginspectorate.gov.uk<br><br>`,
+		`</div>`
 	].join('\n')
 };
 const appellantEmailTemplate = {
