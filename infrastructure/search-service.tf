b2b3daa05dbf04760004989748db3a059df44058
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-10-08 17:15:22 +0100

    feat(infrastructure): create temp doc storage for search service

--- infrastructure/search-service.tf
+++ infrastructure/search-service.tf
@@ -4,3 +4,37 @@ resource "azurerm_search_service" "appeals_search_service" {
   location            = module.primary_region.location
   sku                 = "free"
 }
+
+resource "azurerm_storage_account" "appeals_search_service_temp_storage" {
+  #TODO: Customer Managed Keys
+  #checkov:skip=CKV2_AZURE_1: Customer Managed Keys not implemented yet
+  #checkov:skip=CKV2_AZURE_18: Customer Managed Keys not implemented yet
+  #TODO: Logging
+  #checkov:skip=CKV_AZURE_33: Logging not implemented yet
+  #checkov:skip=CKV2_AZURE_8: Logging not implemented yet
+  #TODO: Access restrictions
+  #checkov:skip=CKV_AZURE_35: Network access restrictions
+  #checkov:skip=CKV2_AZURE_33: "Ensure storage account is configured with private endpoint"
+  #checkov:skip=CKV2_AZURE_38: "Ensure soft-delete is enabled on Azure storage account"
+  #checkov:skip=CKV2_AZURE_40: "Ensure storage account is not configured with Shared Key authorization"
+  #checkov:skip=CKV2_AZURE_41: "Ensure storage account is configured with SAS expiration policy"
+  name                             = "appealsbosearchtemp${local.environment}"
+  resource_group_name              = azurerm_resource_group.primary.name
+  location                         = module.primary_region.location
+  account_tier                     = "Standard"
+  account_replication_type         = "GRS"
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+  https_traffic_only_enabled       = true
+  min_tls_version                  = "TLS1_2"
+
+  tags = local.tags
+}
+
+resource "azurerm_storage_container" "appeals_search_service_temp_container" {
+  #TODO: Logging
+  #checkov:skip=CKV2_AZURE_21 Logging not implemented yet
+  name                  = "appeals-bo-search-temp"
+  storage_account_id    = azurerm_storage_account.appeals_search_service_temp_storage.id
+  container_access_type = "private"
+}
