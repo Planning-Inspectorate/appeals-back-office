3b89fe1dc9415255799819551223010b1db3fd61
musabadam-solirius <musab.adam.7c@planninginspectorate.gov.uk>
2025-10-14 13:44:45 +0100

    feat(web): added search-co page

--- appeals/web/src/server/appeals/appeals.router.js
+++ appeals/web/src/server/appeals/appeals.router.js
@@ -1,4 +1,5 @@
 import { assertUserHasPermission } from '#app/auth/auth.guards.js';
+import searchCaseOfficerRouter from '#appeals/search-case-officer/search-case-officer.router.js';
 import { permissionNames } from '#environment/permissions.js';
 import { Router as createRouter } from 'express';
 import nationalListRouter from '../appeals/national-list/national-list.router.js';
@@ -13,6 +14,12 @@ router.use('/all-cases', assertUserHasPermission(permissionNames.viewCaseList), 
 router.use('/personal-list', personalListRouter);
 
 router.use('/appeal-details', appealDetailsRouter);
+
+router.use(
+	'/search-case-officer',
+	assertUserHasPermission(permissionNames.viewCaseList),
+	searchCaseOfficerRouter
+);
 
 router.use('/error', errorRouter);
 
---
+++ appeals/web/src/server/appeals/search-case-officer/__tests__/search-case-officer.test.js
@@ -0,0 +1,51 @@
+import usersService from '#appeals/appeal-users/users-service.js';
+import { activeDirectoryUsersData } from '#testing/app/fixtures/referencedata.js';
+import { createTestEnvironment } from '#testing/index.js';
+import { jest } from '@jest/globals';
+import { parseHtml } from '@pins/platform';
+import supertest from 'supertest';
+
+const { app, installMockApi, teardown } = createTestEnvironment();
+const request = supertest(app);
+const baseUrl = '/appeals-service';
+
+describe('search-case-officer', () => {
+	beforeEach(() => {
+		installMockApi();
+		// @ts-ignore
+		usersService.getUsersByRole = jest.fn().mockResolvedValue(activeDirectoryUsersData);
+		// @ts-ignore
+		usersService.getUserByRoleAndId = jest.fn().mockResolvedValue(activeDirectoryUsersData[0]);
+	});
+	afterEach(teardown);
+
+	describe('GET /search-case-officer', () => {
+		it('should render the search for case officer page with expected content', async () => {
+			const response = await request.get(`${baseUrl}/search-case-officer`);
+			const element = parseHtml(response.text);
+
+			expect(element.innerHTML).toMatchSnapshot();
+			expect(element.innerHTML).toContain('Case officer</label></h1>');
+			expect(element.innerHTML).toContain('accessible-autocomplete" id="users" name="user"');
+			expect(element.innerHTML).toContain('Continue</button>');
+		});
+
+		it('should re-render the search case officer page with the expected error message if a CO is not provided', async () => {
+			const response = await request.post(`${baseUrl}/search-case-officer`).send({
+				user: ''
+			});
+			const element = parseHtml(response.text);
+
+			expect(element.innerHTML).toMatchSnapshot();
+			expect(element.innerHTML).toContain('Case officer</label></h1>');
+
+			const unprettifiedErrorSummaryHTML = parseHtml(response.text, {
+				rootElement: '.govuk-error-summary',
+				skipPrettyPrint: true
+			}).innerHTML;
+
+			expect(unprettifiedErrorSummaryHTML).toContain('There is a problem</h2>');
+			expect(unprettifiedErrorSummaryHTML).toContain('Enter a case officer</a>');
+		});
+	});
+});
