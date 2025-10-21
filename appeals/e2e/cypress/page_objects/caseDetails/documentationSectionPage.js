c88742073d35c9809f046576897aa9c0c749221c
Abiodun Ishola <abiodun.ishola@solirius.com>
2025-10-21 13:16:08 +0100

    test(appeals): verify POE acceptance for LPA

--- appeals/e2e/cypress/page_objects/caseDetails/documentationSectionPage.js
+++ appeals/e2e/cypress/page_objects/caseDetails/documentationSectionPage.js
@@ -6,4 +6,8 @@ export class DocumentationSectionPage extends CaseDetailsPage {
 	selectAddDocument(type) {
 		cy.getByData('add-' + type).click();
 	}
+
+	navigateToAddProofOfEvidenceReview(type) {
+		cy.getByData('review-' + type).click();
+	}
 }
