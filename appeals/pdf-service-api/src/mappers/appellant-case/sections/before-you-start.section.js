b3644bfa1c84f1c736824ce0c54c68321b84732d
Tom Pickering <tom.pickering@solirius.com>
2025-11-06 14:56:28 +0000

    fix(appeals): update original application type formatting in pdf service

--- appeals/pdf-service-api/src/mappers/appellant-case/sections/before-you-start.section.js
+++ appeals/pdf-service-api/src/mappers/appellant-case/sections/before-you-start.section.js
@@ -1,5 +1,26 @@
+import { APPEAL_TYPE_OF_PLANNING_APPLICATION } from '@planning-inspectorate/data-model';
 import { formatDate } from '../../../lib/nunjucks-filters/format-date.js';
 import { formatSentenceCase } from '../../../lib/nunjucks-filters/index.js';
+
+/**
+ * @param {string | null | undefined } typeOfPlanningApplication
+ * @returns {string}
+ */
+function formatApplicationType(typeOfPlanningApplication) {
+	if (!typeOfPlanningApplication) {
+		return 'Not provided';
+	}
+	switch (typeOfPlanningApplication) {
+		// note: although the constant refers to 'FULL_APPEAL'
+		// we are actually referring to the original planning application here
+		case APPEAL_TYPE_OF_PLANNING_APPLICATION.FULL_APPEAL:
+			return 'Full planning';
+		case APPEAL_TYPE_OF_PLANNING_APPLICATION.ADVERTISEMENT:
+			return 'Displaying an advertisement';
+		default:
+			return formatSentenceCase(typeOfPlanningApplication, 'Not provided');
+	}
+}
 
 export function beforeYouStartSection(templateData) {
 	const {
@@ -20,7 +41,7 @@ export function beforeYouStartSection(templateData) {
 			},
 			{
 				key: 'What type of application is your appeal about?',
-				text: formatSentenceCase(typeOfPlanningApplication, 'Not provided')
+				text: formatApplicationType(typeOfPlanningApplication)
 			},
 			{
 				key: 'Was your application granted or refused?',
