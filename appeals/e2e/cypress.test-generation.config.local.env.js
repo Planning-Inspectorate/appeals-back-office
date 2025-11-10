ab729aa54d958bf797cc28a4fe790981523e2183
Marc Podgorney <marc.podgorney.27@planninginspectorate.gov.uk>
2025-11-10 17:29:18 +0000

    test(appeals): poc for using cypress to generate test data

---
+++ appeals/e2e/cypress.test-generation.config.local.env.js
@@ -0,0 +1,24 @@
+// @ts-nocheck
+const { defineConfig } = require('cypress');
+const baseConfig = require('./cypress.config');
+
+require('dotenv').config();
+
+// use backoffice defaults if not set
+if (!baseConfig.e2e.baseUrl) {
+	baseConfig.e2e.baseUrl = 'https://localhost:8080/';
+}
+
+if (!baseConfig.e2e.apiBaseUrl) {
+	baseConfig.e2e.apiBaseUrl = 'http://localhost:3000/';
+}
+
+module.exports = defineConfig({
+	e2e: {
+		...baseConfig.e2e,
+		specPattern: 'cypress/e2e/back-office-test-generation/**/*.spec.js'
+	},
+	env: {
+		...baseConfig.env
+	}
+});
