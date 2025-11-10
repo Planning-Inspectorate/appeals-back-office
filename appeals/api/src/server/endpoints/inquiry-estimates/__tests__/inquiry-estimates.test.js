ce77afb40aa6b213d39fe46ef11f0c25e1e54bd3
jimbali <jim.bailey.fl@planninginspectorate.gov.uk>
2025-11-10 14:30:31 +0000

    feat(api): add tests for broadcasting inquiry and estimates
    A2-4763

--- appeals/api/src/server/endpoints/inquiry-estimates/__tests__/inquiry-estimates.test.js
+++ appeals/api/src/server/endpoints/inquiry-estimates/__tests__/inquiry-estimates.test.js
@@ -245,6 +245,12 @@ describe('appeal inquiry estimates routes', () => {
 					reportingTime: validInquiryEstimate.reportingTime
 				}
 			});
+			expect(mockBroadcasters.broadcastEventEstimates).toHaveBeenCalledWith(
+				1,
+				'inquiry',
+				'Create',
+				null
+			);
 		});
 
 		test('returns 200 when updating a inquiry estimate with valid values', async () => {
@@ -269,6 +275,12 @@ describe('appeal inquiry estimates routes', () => {
 					reportingTime: validInquiryEstimate.reportingTime
 				}
 			});
+			expect(mockBroadcasters.broadcastEventEstimates).toHaveBeenCalledWith(
+				1,
+				'inquiry',
+				'Update',
+				null
+			);
 		});
 
 		test('returns 200 when deleting a inquiry estimate', async () => {
@@ -287,6 +299,12 @@ describe('appeal inquiry estimates routes', () => {
 			expect(databaseConnector.inquiryEstimate.delete).toHaveBeenCalledWith({
 				where: { appealId: householdAppeal.id }
 			});
+			expect(mockBroadcasters.broadcastEventEstimates).toHaveBeenCalledWith(
+				1,
+				'inquiry',
+				'Delete',
+				appealWithInquiryEstimate.inquiryEstimate
+			);
 		});
 	});
 });
