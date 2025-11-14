ac2c89438493ef577348483de273d054d128b244
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-11-14 15:26:12 +0000

    chore(tooling): log db queries

--- appeals/api/src/server/utils/database-connector.js
+++ appeals/api/src/server/utils/database-connector.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from '#db-client';
+import logger from '#utils/logger.js';
 
 /** @type {PrismaClient} */
 let prismaClient;
@@ -8,7 +9,52 @@ let prismaClient;
  */
 function createPrismaClient() {
 	if (!prismaClient) {
-		prismaClient = new PrismaClient();
+		prismaClient = new PrismaClient({
+			log: [
+				{
+					emit: 'event',
+					level: 'query'
+				},
+				{
+					emit: 'event',
+					level: 'error'
+				}
+				// {
+				// 	emit: 'event',
+				// 	level: 'info'
+				// },
+				// {
+				// 	emit: 'event',
+				// 	level: 'warn'
+				// }
+			]
+		});
+
+		/** @param {import('#db-client').Prisma.QueryEvent} e */
+		const logQuery = (e) => {
+			logger.debug(
+				{ query: e.query, params: e.params, duration: e.duration },
+				'Prisma query executed'
+			);
+		};
+
+		/** @param {import('#db-client').Prisma.LogEvent} e */
+		const logError = (e) => logger.error({ e }, 'Prisma error');
+
+		/** @param {import('#db-client').Prisma.LogEvent} e */
+		const logInfo = (e) => logger.debug({ e });
+
+		/** @param {import('#db-client').Prisma.LogEvent} e */
+		const logWarn = (e) => logger.warn({ e });
+
+		// @ts-ignore
+		prismaClient.$on('query', logQuery);
+		// @ts-ignore
+		prismaClient.$on('error', logError);
+		// @ts-ignore
+		prismaClient.$on('info', logInfo);
+		// @ts-ignore
+		prismaClient.$on('warn', logWarn);
 	}
 
 	return prismaClient;
