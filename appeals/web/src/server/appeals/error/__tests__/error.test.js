d6d47e96c6a5caebf1795046f06dd4bd74f71912
Kade Neveling <kade.neveling@solirius.com>
2025-11-17 14:52:03 +0000

    fix(web): corrected error pages 404 and 500 text and reference links
    fix(web): corrected error 500 page and reference link
    
    fix(web): resolved unit tests and snapshots
    
    fix(web): implemented backdoor for error page rendering based on url
    
    fix(web): resolved error code security issue for potential cross-site scripting
    
    fix(web): updated and resolved
    
    fix(web): resolved
    
    fix(web): resolved

--- appeals/web/src/server/appeals/error/__tests__/error.test.js
+++ appeals/web/src/server/appeals/error/__tests__/error.test.js
@@ -2,9 +2,10 @@ import supertest from 'supertest';
 
 import { createTestEnvironment } from '#testing/index.js';
 import { parseHtml } from '@pins/platform';
+import nock from 'nock';
 
 const baseUrl = '/appeals-service/error';
-const { app } = createTestEnvironment();
+const { app, installMockApi, teardown } = createTestEnvironment();
 const request = supertest(app);
 
 describe('error page', () => {
@@ -27,5 +28,95 @@ describe('error page', () => {
 		expect(response.statusCode).toBe(500);
 		const element = parseHtml(response.text);
 		expect(element.innerHTML).toContain('Sorry, there is a problem with the service</h1>');
+	});
+});
+
+// Can create other error page tests as well
+
+describe('404 Not Found', () => {
+	/** @type {import('supertest').Response} */
+	let response;
+	/** @type {any} */
+	let element;
+
+	beforeAll(async () => {
+		response = await request.get('/url-that-does-not-exist');
+		element = parseHtml(response.text);
+	});
+
+	it('should return a 404 status code', () => {
+		expect(response.statusCode).toBe(404);
+	});
+
+	it('should render the custom 404 page content', () => {
+		const heading = element.querySelector('h1');
+		expect(heading).not.toBeNull();
+		expect(heading.textContent).toContain('Page not found');
+
+		expect(element.textContent).toContain('If you typed the web address, check it is correct.');
+		expect(element.textContent).toContain(
+			'If you pasted the web address, check you copied the entire address.'
+		);
+		expect(element.textContent).toContain(
+			'If the web address is correct or you selected a link or button, report an IT problem.'
+		);
+	});
+
+	it('should contain link that redirects to LittleFish support', () => {
+		const linkText = 'report an IT problem.';
+
+		const link = Array.from(element.querySelectorAll('a')).find(
+			(a) => a.textContent.trim() === linkText
+		);
+
+		expect(link).not.toBeNull();
+
+		expect(link.getAttribute('href')).toBe(
+			'https://intranet.planninginspectorate.gov.uk/task/report-it-problem/'
+		);
+	});
+});
+
+describe('500 Internal Server Error', () => {
+	/** @type {import('supertest').Response} */
+	let response;
+	/** @type {any} */
+	let element;
+
+	beforeAll(async () => {
+		installMockApi();
+		nock('http://test/').get('/appeals/1').reply(500, 'Internal API Error');
+
+		response = await request.get('/appeals-service/appeal-details/1');
+		element = parseHtml(response.text);
+
+		teardown();
+	});
+
+	it('should return a 500 status code', () => {
+		expect(response.statusCode).toBe(500);
+	});
+
+	it('should render the custom 500 page content', () => {
+		const heading = element.querySelector('h1');
+		expect(heading).not.toBeNull();
+		expect(heading.textContent).toContain('Sorry, there is a problem with the service');
+
+		expect(element.textContent).toContain('Try again later.');
+		expect(element.textContent).toContain('Report an IT problem if the problem continues.');
+	});
+
+	it('should contain link that redirects to LittleFish support', () => {
+		const linkText = 'Report an IT problem';
+
+		const link = Array.from(element.querySelectorAll('a')).find(
+			(a) => a.textContent.trim() === linkText
+		);
+
+		expect(link).not.toBeNull();
+
+		expect(link.getAttribute('href')).toBe(
+			'https://intranet.planninginspectorate.gov.uk/task/report-it-problem/'
+		);
 	});
 });
