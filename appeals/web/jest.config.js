c2a4d47a641b283e5b0ceec0839c5a1b64120966
Dave Ferguson <dave.ferguson.g3@planninginspectorate.gov.uk>
2025-11-17 13:26:01 +0000

    feat(appeals): changed babel and jest to resolve failues from jest upgrade

--- appeals/web/jest.config.js
+++ appeals/web/jest.config.js
@@ -1,7 +1,18 @@
 export default {
 	setupFiles: ['<rootDir>/setup-tests.js'],
 	testTimeout: 50000,
-	transform: {},
+	transform: {
+		// NEVER transform node_modules except the uuid ESM build
+		'^.+\\.[jt]sx?$': 'babel-jest',
+
+		// Add a dedicated transform for uuid’s ESM browser build
+		'^node_modules/uuid/.+\\.js$': 'babel-jest'
+	},
+
+	transformIgnorePatterns: [
+		// Ignore ALL node_modules except uuid
+		'/node_modules/(?!uuid/)'
+	],
 	moduleNameMapper: {
 		'^uuid$': 'uuid'
 	},
