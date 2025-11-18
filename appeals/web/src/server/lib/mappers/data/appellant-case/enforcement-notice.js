b4056b1074edb419648a509e66ba9ac1810deea1
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-11-18 16:40:55 +0000

    feat(appeals): enforcement appellant case bys section (a2-5065)

--- appeals/web/src/server/lib/mappers/data/appellant-case/enforcement-notice.js
+++ appeals/web/src/server/lib/mappers/data/appellant-case/enforcement-notice.js
@@ -1,6 +1,18 @@
 import { submaps as s78Submaps } from './s78.js';
+import { mapContactPlanningInspectorateDate } from './submappers/contact-planning-inspectorate-date.js';
+import { mapEnforcementEffectiveDate } from './submappers/enforcement-effective-date.js';
+import { mapEnforcementIssueDate } from './submappers/enforcement-issue-date.js';
+import { mapEnforcementNoticeListedBuilding } from './submappers/enforcement-notice-listed-building.js';
+import { mapEnforcementNotice } from './submappers/enforcement-notice.js';
+import { mapEnforcementReference } from './submappers/enforcement-reference.js';
 
 /** @type {Record<string, import('./mapper.js').SubMapper>} */
 export const submaps = {
-	...s78Submaps
+	...s78Submaps,
+	enforcementNotice: mapEnforcementNotice,
+	enforcementNoticeListedBuilding: mapEnforcementNoticeListedBuilding,
+	enforcementIssueDate: mapEnforcementIssueDate,
+	enforcementEffectiveDate: mapEnforcementEffectiveDate,
+	contactPlanningInspectorateDate: mapContactPlanningInspectorateDate,
+	enforcementReference: mapEnforcementReference
 };
