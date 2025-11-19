6508896916964ac0d417f47ca6eea8f97785ad75
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-11-19 16:26:19 +0000

    fix(appeals): hide change link if no data (a2-5065)

--- appeals/web/src/server/lib/mappers/data/appellant-case/submappers/enforcement-reference.js
+++ appeals/web/src/server/lib/mappers/data/appellant-case/submappers/enforcement-reference.js
@@ -1,11 +1,13 @@
 import { textSummaryListItem } from '#lib/mappers/index.js';
 
 /** @type {import('../mapper.js').SubMapper} */
-export const mapEnforcementReference = ({ appellantCaseData, currentRoute, userHasUpdateCase }) =>
-	textSummaryListItem({
+export const mapEnforcementReference = ({ appellantCaseData, currentRoute, userHasUpdateCase }) => {
+	const hasData = appellantCaseData.enforcementNotice?.reference !== null;
+	return textSummaryListItem({
 		id: 'enforcement-reference',
 		text: 'What is the reference number on the enforcement notice?',
 		value: appellantCaseData.enforcementNotice?.reference || 'No data',
 		link: `${currentRoute}/enforcement-reference/change`,
-		editable: userHasUpdateCase
+		editable: hasData && userHasUpdateCase
 	});
+};
