170b98200244a224e9348786da4b0e698c2cd7c2
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-11-20 12:22:58 +0000

    fix(appeals): success banner to be amended when progressing to inquiry

--- appeals/web/src/server/appeals/appeal-details/representations/representations.controller.js
+++ appeals/web/src/server/appeals/appeal-details/representations/representations.controller.js
@@ -82,13 +82,15 @@ export async function postShareRepresentations(request, response) {
 					? 'finalCommentsShared'
 					: 'caseProgressed';
 			case APPEAL_CASE_STATUS.EVIDENCE:
-				return publishedReps.filter(
-					(rep) =>
-						rep.representationType === APPEAL_REPRESENTATION_TYPE.LPA_PROOFS_EVIDENCE ||
-						rep.representationType === APPEAL_REPRESENTATION_TYPE.APPELLANT_PROOFS_EVIDENCE
-				).length > 0
-					? 'proofOfEvidenceShared'
-					: 'progressedToInquiry';
+				if (
+					publishedReps.filter(
+						(rep) =>
+							rep.representationType === APPEAL_REPRESENTATION_TYPE.LPA_PROOFS_EVIDENCE ||
+							rep.representationType === APPEAL_REPRESENTATION_TYPE.APPELLANT_PROOFS_EVIDENCE
+					).length > 0
+				) {
+					return 'progressedToInquiry';
+				}
 		}
 	})();
 
