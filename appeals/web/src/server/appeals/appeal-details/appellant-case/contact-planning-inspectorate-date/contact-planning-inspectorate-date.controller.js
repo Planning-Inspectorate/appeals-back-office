e28580e517b8262092bb861befe9e1608b835863
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-11-21 15:38:18 +0000

    feat(appeals): contact planning inspectorate date change page (a2-5176)

---
+++ appeals/web/src/server/appeals/appeal-details/appellant-case/contact-planning-inspectorate-date/contact-planning-inspectorate-date.controller.js
@@ -0,0 +1,129 @@
+import { dayMonthYearHourMinuteToISOString } from '#lib/dates.js';
+import logger from '#lib/logger.js';
+import { objectContainsAllKeys } from '#lib/object-utilities.js';
+import { addNotificationBannerToSession } from '#lib/session-utilities.js';
+import { HTTPError } from 'got';
+import { getAppellantCaseFromAppealId } from '../appellant-case.service.js';
+import { changeContactPlanningInspectorateDatePage } from './contact-planning-inspectorate-date.mapper.js';
+import { changeContactPlanningInspectorateDate } from './contact-planning-inspectorate-date.service.js';
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const getChangeContactPlanningInspectorateDate = async (request, response) => {
+	return renderChangeContactPlanningInspectorateDate(request, response);
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+const renderChangeContactPlanningInspectorateDate = async (request, response) => {
+	try {
+		const { errors, currentAppeal, apiClient, body } = request;
+		const appellantCaseData = await getAppellantCaseFromAppealId(
+			apiClient,
+			currentAppeal.appealId,
+			currentAppeal.appellantCaseId
+		);
+
+		const dateEntered = {
+			day: body['contact-planning-inspectorate-date-day'] || '',
+			month: body['contact-planning-inspectorate-date-month'] || '',
+			year: body['contact-planning-inspectorate-date-year'] || ''
+		};
+
+		const mappedPageContents = changeContactPlanningInspectorateDatePage(
+			currentAppeal,
+			appellantCaseData,
+			errors,
+			dateEntered
+		);
+
+		return response
+			.status(200)
+			.render('patterns/change-page.pattern.njk', { pageContent: mappedPageContents, errors });
+	} catch (error) {
+		logger.error(error);
+		delete request.session.contactPlanningInspectorateDate;
+		if (error instanceof HTTPError && error.response.statusCode === 404) {
+			return response.status(404).render('app/404.njk');
+		} else {
+			return response.status(500).render('app/500.njk');
+		}
+	}
+};
+
+/**
+ * @param {import('@pins/express/types/express.js').Request} request
+ * @param {import('@pins/express/types/express.js').RenderedResponse<any, any, Number>} response
+ */
+export const postChangeContactPlanningInspectorateDate = async (request, response) => {
+	if (request.errors) {
+		return renderChangeContactPlanningInspectorateDate(request, response);
+	}
+
+	if (
+		!objectContainsAllKeys(request.body, [
+			'contact-planning-inspectorate-date-day',
+			'contact-planning-inspectorate-date-month',
+			'contact-planning-inspectorate-date-year'
+		])
+	) {
+		return response.status(500).render('app/500.njk');
+	}
+
+	try {
+		const updatedApplicationDateDay = parseInt(
+			request.body['contact-planning-inspectorate-date-day'],
+			10
+		);
+		const updatedApplicationDateMonth = parseInt(
+			request.body['contact-planning-inspectorate-date-month'],
+			10
+		);
+		const updatedApplicationDateYear = parseInt(
+			request.body['contact-planning-inspectorate-date-year'],
+			10
+		);
+
+		if (
+			Number.isNaN(updatedApplicationDateDay) ||
+			Number.isNaN(updatedApplicationDateMonth) ||
+			Number.isNaN(updatedApplicationDateYear)
+		) {
+			return response.status(500).render('app/500.njk');
+		}
+
+		const { currentAppeal, apiClient } = request;
+
+		const { appealId, appellantCaseId } = currentAppeal;
+
+		await changeContactPlanningInspectorateDate(
+			apiClient,
+			appealId,
+			appellantCaseId,
+			dayMonthYearHourMinuteToISOString({
+				day: updatedApplicationDateDay,
+				month: updatedApplicationDateMonth,
+				year: updatedApplicationDateYear
+			})
+		);
+
+		addNotificationBannerToSession({
+			session: request.session,
+			bannerDefinitionKey: 'changePage',
+			appealId,
+			text: 'Date application submitted updated'
+		});
+
+		return response.redirect(
+			`/appeals-service/appeal-details/${currentAppeal.appealId}/appellant-case`
+		);
+	} catch (error) {
+		logger.error(error);
+	}
+
+	return response.status(500).render('app/500.njk');
+};
--- appeals/web/src/server/lib/mappers/data/appellant-case/submappers/contact-planning-inspectorate-date.js
+++ appeals/web/src/server/lib/mappers/data/appellant-case/submappers/contact-planning-inspectorate-date.js
@@ -1,3 +1,4 @@
+import { dateISOStringToDisplayDate } from '#lib/dates.js';
 import { textSummaryListItem } from '#lib/mappers/index.js';
 
 /** @type {import('../mapper.js').SubMapper} */
@@ -13,9 +14,13 @@ export const mapContactPlanningInspectorateDate = ({
 	return textSummaryListItem({
 		id: 'contact-planning-inspectorate-date',
 		text: 'When did you contact the Planning Inspectorate?',
-		value: hasData
-			? appellantCaseData.enforcementNotice?.contactPlanningInspectorateDate || 'Not answered'
-			: 'No data',
+		value: !hasData
+			? 'No data'
+			: actionText === 'Add'
+			? 'Not answered'
+			: dateISOStringToDisplayDate(
+					appellantCaseData.enforcementNotice?.contactPlanningInspectorateDate
+			  ),
 		link: `${currentRoute}/contact-planning-inspectorate-date/${actionText.toLowerCase()}`,
 		actionText,
 		editable: hasData && userHasUpdateCase
