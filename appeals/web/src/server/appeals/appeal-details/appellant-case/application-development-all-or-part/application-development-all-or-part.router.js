2672fc557f1e5a7817822226e0de60968681482f
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2025-12-23 14:52:11 +0000

    fix(appeals): add validation to all or part (a2-5415)

--- appeals/web/src/server/appeals/appeal-details/appellant-case/application-development-all-or-part/application-development-all-or-part.router.js
+++ appeals/web/src/server/appeals/appeal-details/appellant-case/application-development-all-or-part/application-development-all-or-part.router.js
@@ -1,12 +1,16 @@
 import { asyncHandler } from '@pins/express';
 import { Router as createRouter } from 'express';
 import * as controllers from './application-development-all-or-part.controller.js';
+import { validateDevelopmentAllOrPartPresent } from './application-development-all-or-part.validators.js';
 
 const router = createRouter({ mergeParams: true });
 
 router
 	.route('/change')
 	.get(asyncHandler(controllers.getChangeApplicationDevelopmentAllOrPart))
-	.post(asyncHandler(controllers.postChangeApplicationDevelopmentAllOrPart));
+	.post(
+		validateDevelopmentAllOrPartPresent,
+		asyncHandler(controllers.postChangeApplicationDevelopmentAllOrPart)
+	);
 
 export default router;
