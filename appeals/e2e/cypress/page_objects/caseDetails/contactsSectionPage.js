56c678b780047cee7c9e6d09d36408875808aad4
Abiodun Ishola <abiodun.ishola@solirius.com>
2025-12-24 11:26:03 +0000

    test(appeals): add and manage rule 6 contacts

--- appeals/e2e/cypress/page_objects/caseDetails/contactsSectionPage.js
+++ appeals/e2e/cypress/page_objects/caseDetails/contactsSectionPage.js
@@ -6,4 +6,8 @@ export class ContactsSectionPage extends CaseDetailsPage {
 	selectAddContact(type) {
 		cy.getByData('add-' + type).click();
 	}
+
+	manageContactDetails() {
+		cy.getByData('manage-rule-6-party-contact-details').click();
+	}
 }
