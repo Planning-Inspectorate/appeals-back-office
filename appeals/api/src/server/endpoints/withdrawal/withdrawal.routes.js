fcd7a9f594337ad4c3aed089238e4c282566e30d
gmmagruder <gabrielle.magruder@gmail.com>
2026-01-05 13:36:16 +0000

    refactor(api): reduce selected keys on get appeal for withdrawal endpoint

--- appeals/api/src/server/endpoints/withdrawal/withdrawal.routes.js
+++ appeals/api/src/server/endpoints/withdrawal/withdrawal.routes.js
@@ -1,4 +1,4 @@
-import { checkAppealExistsByIdAndAddToRequest } from '#middleware/check-appeal-exists-and-add-to-request.js';
+import { checkAppealExistsByIdAndAddPartialToRequest } from '#middleware/check-appeal-exists-and-add-to-request.js';
 import { asyncHandler } from '@pins/express';
 import { Router as createRouter } from 'express';
 import { postWithdrawal } from './withdrawal.controller.js';
@@ -30,7 +30,16 @@ router.post(
 		#swagger.responses[400] = {}
 		#swagger.responses[404] = {}
 	 */
-	checkAppealExistsByIdAndAddToRequest,
+	checkAppealExistsByIdAndAddPartialToRequest([
+		'address',
+		'agent',
+		'appellant',
+		'lpa',
+		'appealType',
+		'hearing',
+		'procedureType',
+		'siteVisit'
+	]),
 	getDateValidator,
 	asyncHandler(postWithdrawal)
 );
