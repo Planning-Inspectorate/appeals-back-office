a8d40632f1348ecd135dc935a0d48c43a187cf10
Dave Ferguson <dave.ferguson.g3@planninginspectorate.gov.uk>
2026-01-12 10:37:51 +0000

    feat(appeals): increase the file upload size from 100mb to 250mb

--- packages/appeals/constants/distance.js
+++ packages/appeals/constants/distance.js
@@ -1,5 +1,5 @@
 export const BYTES_IN_KB = 1024;
 export const BYTES_IN_MB = BYTES_IN_KB * 1024;
 export const BYTES_IN_GB = BYTES_IN_MB * 1024;
-export const MAX_FILE_SIZE = 100 * BYTES_IN_MB; // 100MB
+export const MAX_FILE_SIZE = 250 * BYTES_IN_MB; // 250MB
 export const TOTAL_MAX_FILE_SIZE = 1 * BYTES_IN_GB; // 1GB
