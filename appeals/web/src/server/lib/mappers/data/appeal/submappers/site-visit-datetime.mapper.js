9c961ab78a785dc5a12ca9a29e874122eef45a47
musabadam-pins <musab.adam.7c@planninginspectorate.gov.uk>
2026-01-19 14:27:36 +0000

    feat(web): created new site visit page (A2-5998)
    feat(web): merged missed site visit with new route (A2-5998)

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/site-visit-datetime.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/site-visit-datetime.mapper.js
@@ -1,39 +1,59 @@
 import config from '#environment/config.js';
 import { dateISOStringToDisplayDate, dateISOStringToDisplayTime12hr } from '#lib/dates.js';
 import { textSummaryListItem } from '#lib/mappers/index.js';
+import { addBackLinkQueryToUrl } from '#lib/url-utilities.js';
 
 /** @type {import('../mapper.js').SubMapper} */
-export const mapSiteVisitDate = ({ appealDetails, userHasUpdateCasePermission }) =>
-	textSummaryListItem({
+export const mapSiteVisitDate = ({ appealDetails, userHasUpdateCasePermission, request }) => {
+	const link = addBackLinkQueryToUrl(
+		request,
+		`/appeals-service/appeal-details/${appealDetails.appealId}/site-visit/schedule/schedule-visit-date`
+	);
+	return textSummaryListItem({
 		id: 'site-visit-date',
 		text: 'Date',
 		value: {
-			html: dateISOStringToDisplayDate(appealDetails.siteVisit?.visitDate)
+			html: appealDetails.siteVisit?.visitDate
+				? dateISOStringToDisplayDate(appealDetails.siteVisit?.visitDate)
+				: `<a class="govuk-link" href="${link}">Enter date</a>`
 		},
-		link: `/appeals-service/appeal-details/${appealDetails.appealId}/site-visit/visit-booked`,
+		link: link,
 		editable: userHasUpdateCasePermission
 	});
-
+};
 /** @type {import('../mapper.js').SubMapper} */
-export const mapSiteVisitStartTime = ({ appealDetails, userHasUpdateCasePermission }) =>
-	textSummaryListItem({
+export const mapSiteVisitStartTime = ({ appealDetails, userHasUpdateCasePermission, request }) => {
+	const link = addBackLinkQueryToUrl(
+		request,
+		`/appeals-service/appeal-details/${appealDetails.appealId}/site-visit/schedule/schedule-visit-date`
+	);
+	return textSummaryListItem({
 		id: 'site-visit-start-time',
 		text: config.featureFlags.featureFlagCancelSiteVisit ? 'Time' : 'Start time',
 		value: {
-			html: dateISOStringToDisplayTime12hr(appealDetails.siteVisit?.visitStartTime)
+			html: appealDetails.siteVisit?.visitStartTime
+				? dateISOStringToDisplayTime12hr(appealDetails.siteVisit?.visitStartTime)
+				: `<a class="govuk-link" href="${link}">Enter time</a>`
 		},
-		link: `/appeals-service/appeal-details/${appealDetails.appealId}/site-visit/visit-booked`,
+		link: link,
 		editable: userHasUpdateCasePermission
 	});
-
+};
 /** @type {import('../mapper.js').SubMapper} */
-export const mapSiteVisitEndTime = ({ appealDetails, userHasUpdateCasePermission }) =>
-	textSummaryListItem({
+export const mapSiteVisitEndTime = ({ appealDetails, userHasUpdateCasePermission, request }) => {
+	const link = addBackLinkQueryToUrl(
+		request,
+		`/appeals-service/appeal-details/${appealDetails.appealId}/site-visit/schedule/schedule-visit-date`
+	);
+	return textSummaryListItem({
 		id: 'site-visit-end-time',
-		text: 'End time',
+		text: 'End times',
 		value: {
-			html: dateISOStringToDisplayTime12hr(appealDetails.siteVisit?.visitEndTime)
+			html: appealDetails.siteVisit?.visitEndTime
+				? dateISOStringToDisplayTime12hr(appealDetails.siteVisit?.visitEndTime)
+				: `<a class="govuk-link" href="${link}">Enter time</a>`
 		},
-		link: `/appeals-service/appeal-details/${appealDetails.appealId}/site-visit/visit-booked`,
+		link: link,
 		editable: userHasUpdateCasePermission
 	});
+};
