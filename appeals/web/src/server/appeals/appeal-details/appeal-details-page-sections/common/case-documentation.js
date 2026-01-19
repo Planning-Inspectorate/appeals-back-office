a8d27b4d6b349a5a68f3f262a59b52135da1b90c
TurnerMichael <michael.turner@planninginspectorate.gov.uk>
2026-01-19 15:08:15 +0000

    fix(web): corrected row ordering and fixed ts check (a2-5338)

--- appeals/web/src/server/appeals/appeal-details/appeal-details-page-sections/common/case-documentation.js
+++ appeals/web/src/server/appeals/appeal-details/appeal-details-page-sections/common/case-documentation.js
@@ -1,5 +1,4 @@
 import { APPEAL_CASE_PRE_STATEMENTS_STATUS } from '#appeals/appeal.constants.js';
-import config from '#environment/config.js';
 import { isChildAppeal } from '#lib/mappers/utils/is-linked-appeal.js';
 import { isDefined } from '#lib/ts-utilities.js';
 import { APPEAL_CASE_PROCEDURE } from '@planning-inspectorate/data-model';
@@ -32,28 +31,24 @@ export const getCaseDocumentation = (mappedData, appealDetails) => {
 				caseStarted ? mappedData.appeal.lpaQuestionnaire.display.tableItem : [],
 				...(!isChildAppeal(appealDetails)
 					? [
+							mappedData.appeal.appellantStatement.display.tableItem || [],
 							caseStarted ? mappedData.appeal.lpaStatement.display.tableItem : [],
-							mappedData.appeal.appellantStatement.display.tableItem,
-							mappedData.appeal.lpaStatement.display.tableItem,
 							...(mappedData.appeal.rule6PartyStatements?.display?.tableItems || []),
 							caseStarted ? mappedData.appeal.ipComments.display.tableItem : [],
 							...(caseStarted && !isInquiryProcedureType && statementsCompleted
 								? [
 										mappedData.appeal.appellantFinalComments.display.tableItem,
 										mappedData.appeal.lpaFinalComments.display.tableItem
-								  ]
+									]
 								: []),
 							...(caseStarted && isInquiryProcedureType && inquiryEventSetUp
 								? [
-										mappedData.appeal.appellantProofOfEvidence.display.tableItem,
-										mappedData.appeal.lpaProofOfEvidence.display.tableItem
-								  ]
+										mappedData.appeal.appellantProofOfEvidence.display.tableItem || [],
+										mappedData.appeal.lpaProofOfEvidence.display.tableItem || []
+									]
 								: []),
-							...(mappedData.appeal.rule6PartyProofs?.display?.tableItems || []),
-							...(config.featureFlags.featureFlagAppellantStatement
-								? mappedData.appeal.appellantStatement.display.tableItem
-								: [])
-					  ]
+							...(mappedData.appeal.rule6PartyProofs?.display?.tableItems || [])
+						]
 					: []),
 				mappedData.appeal.environmentalAssessment.display.tableItem
 			].filter(isDefined),
