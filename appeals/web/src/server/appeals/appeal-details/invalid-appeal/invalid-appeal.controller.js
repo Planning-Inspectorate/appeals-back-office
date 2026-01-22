008df0c94324a7357363b9696c3228da9e8ae4b2
Matt <56436664+matt2415@users.noreply.github.com>
2026-01-22 13:21:58 +0000

    feat(web): allow other reasons for enforcement appeal invalid

--- appeals/web/src/server/appeals/appeal-details/invalid-appeal/invalid-appeal.controller.js
+++ appeals/web/src/server/appeals/appeal-details/invalid-appeal/invalid-appeal.controller.js
@@ -461,7 +461,7 @@ export const postEnforcementNoticeReason = async (request, response) => {
 				? [body.invalidReason].flat().map((reason) => ({
 						reasonSelected: Number(reason),
 						reasonText: body[`invalidReason-${reason}`]
-					}))
+				  }))
 				: null
 		};
 
