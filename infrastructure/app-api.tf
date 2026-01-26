39044d7fdb53fa46848561eac66f0f3e2297342e
lachlandow1 <lachlan.dow@solirius.com>
2026-01-26 10:29:46 +0000

    fix(api): add r6 statement to terraform app settings

--- infrastructure/app-api.tf
+++ infrastructure/app-api.tf
@@ -85,6 +85,7 @@ module "app_api" {
     FEATURE_FLAG_RULE_6_MVP              = var.apps_config.featureFlags.featureFlagRule6Mvp
     FEATURE_FLAG_RULE_6_POE              = var.apps_config.featureFlags.featureFlagRule6PoE
     FEATURE_FLAG_RULE_6_COSTS            = var.apps_config.featureFlags.featureFlagRule6Costs
+    FEATURE_FLAG_RULE_6_STATEMENT        = var.apps_config.featureFlags.featureFlagRule6Statement
     FEATURE_FLAG_EXPEDITED_APPEALS       = var.apps_config.featureFlags.featureFlagExpeditedAppeals
     FEATURE_FLAG_LDC                     = var.apps_config.featureFlags.featureFlagLDC
 
