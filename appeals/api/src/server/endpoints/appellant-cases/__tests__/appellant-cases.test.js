4a5b1eb673fc6925e987e0815d229ac8f4bcfb84
Matt <56436664+matt2415@users.noreply.github.com>
2026-01-27 17:05:43 +0000

    fix(api): save enforcment other details to case history

--- appeals/api/src/server/endpoints/appellant-cases/__tests__/appellant-cases.test.js
+++ appeals/api/src/server/endpoints/appellant-cases/__tests__/appellant-cases.test.js
@@ -1405,6 +1405,15 @@ describe('appellant cases routes', () => {
 					}
 				});
 
+				expect(databaseConnector.auditTrail.create).toHaveBeenCalledWith({
+					data: {
+						appealId: enforcementNoticeAppeal.id,
+						details: `Appeal reviewed as valid on 27 January 2026\nOther information`,
+						loggedAt: expect.any(Date),
+						userId: enforcementNoticeAppeal.caseOfficer.id
+					}
+				});
+
 				expect(mockNotifySend).not.toHaveBeenCalled();
 				expect(response.status).toEqual(200);
 			});
