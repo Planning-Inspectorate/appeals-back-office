cff9237e10089834b31850e4e41c67aaa4ce4864
Tom Pickering <tom.pickering@solirius.com>
2026-01-30 16:26:06 +0000

    fix(appeals): amend change enforcement notice reference page (A2-6552)

--- appeals/web/src/server/appeals/appeal-details/appellant-case/enforcement-reference/enforcement-reference.validators.js
+++ appeals/web/src/server/appeals/appeal-details/appellant-case/enforcement-reference/enforcement-reference.validators.js
@@ -1,12 +1,21 @@
-import { createTextInputValidator } from '#lib/validators/text-input-validator.js';
+import stringTokenReplacement from '@pins/appeals/utils/string-token-replacement.js';
 import { createValidator } from '@pins/express';
+import { body } from 'express-validator';
 
 export const validateEnforcementReference = createValidator(
-	createTextInputValidator(
-		'enforcementReference',
-		'Enter the reference number on the enforcement notice',
-		250,
-		'Reference number must be 250 characters or less',
-		/^[A-Za-z0-9 .,'!&/-]+$/
-	)
+	body('enforcementReference')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage((value, { req }) => {
+			if (req.params && req.params.userType) {
+				return stringTokenReplacement('Enter the reference number on the enforcement notice', [
+					req.params.userType
+				]);
+			} else {
+				return 'Enter the reference number on the enforcement notice';
+			}
+		})
+		.bail()
+		.isLength({ max: 250 })
+		.withMessage('Reference number must be 250 characters or less')
 );
