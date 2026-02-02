965df13e97d95965e1b069d595f41ca928cc3db6
jimbali <jim.bailey.fl@planninginspectorate.gov.uk>
2026-02-02 15:48:47 +0000

    feat(web): cancel enforcement invalid reasons page (A2-5978)

---
+++ appeals/web/src/server/appeals/appeal-details/cancel/enforcement/cancel-enforcement.mapper.js
@@ -0,0 +1,68 @@
+import { appealShortReference } from '#lib/appeals-formatter.js';
+import { renderPageComponentsToHtml } from '#lib/nunjucks-template-builders/page-component-rendering.js';
+
+/**
+ * @typedef {import('#appeals/appeal-details/appeal-details.types.js').WebAppeal} Appeal
+ */
+
+/**
+ * @param {Appeal} appeal
+ * @param {import('@pins/appeals.api/src/server/endpoints/appeals.js').ReasonOption[]} reasonOptions
+ * @param {string} backLinkUrl
+ * @param {Record<string, string>} [values]
+ * @param {import('@pins/express').ValidationErrors} [errors]
+ * @returns {PageContent}
+ */
+export const invalidReasonPage = (appeal, reasonOptions, backLinkUrl, values = {}, errors) => {
+	const shortAppealReference = appealShortReference(appeal.appealReference);
+	/** @type {import('#appeals/appeals.types.js').CheckboxItemParameter[]} */
+	const items = reasonOptions.map((reason) => ({
+		value: String(reason.id),
+		text: reason.name,
+		checked: values['invalidReason']?.includes(String(reason.id)),
+		conditional: reason.hasText
+			? {
+					html: renderPageComponentsToHtml([
+						{
+							type: 'input',
+							parameters: {
+								id: `invalid-reason-${reason.id}-1`,
+								name: `invalidReason-${reason.id}`,
+								value: values[`invalidReason-${reason.id}`] || '',
+								errorMessage: errors?.[`invalidReason-${reason.id}-1`]?.msg,
+								label: {
+									text: 'Enter a reason',
+									classes: 'govuk-label--s'
+								}
+							}
+						}
+					])
+				}
+			: undefined
+	}));
+
+	/** @type {PageContent} */
+	const pageContent = {
+		title: `Why is the appeal invalid?`,
+		backLinkUrl,
+		preHeading: `Appeal ${shortAppealReference}`,
+		heading: 'Why is the appeal invalid?',
+		hint: 'Select all that apply.',
+		pageComponents: [
+			{
+				type: 'checkboxes',
+				parameters: {
+					attributes: {
+						id: 'invalid-reason-checkboxes'
+					},
+					name: 'invalidReason',
+					idPrefix: 'invalid-reason',
+					items,
+					errorMessage: errors?.invalidReason && { text: errors.invalidReason.msg }
+				}
+			}
+		]
+	};
+
+	return pageContent;
+};
---
+++ appeals/web/src/server/appeals/appeal-details/cancel/enforcement/cancel-enforcement.validators.js
@@ -0,0 +1,21 @@
+import { createCheckboxTextItemsValidator } from '#lib/validators/checkbox-text-items.validator.js';
+import { LENGTH_1000, LENGTH_250 } from '@pins/appeals/constants/support.js';
+import { createValidator } from '@pins/express';
+import { body } from 'express-validator';
+
+export const validateInvalidReason = createValidator(
+	body('invalidReason')
+		.exists()
+		.withMessage('Select why the appeal is invalid')
+		.bail()
+		.notEmpty()
+		.withMessage('Select why the appeal is invalid')
+		.bail()
+		.isLength({ max: LENGTH_1000 })
+		.withMessage(`Reason must be ${LENGTH_1000} characters or less`)
+);
+
+export const validateInvalidReasonTextItems = createCheckboxTextItemsValidator(
+	'invalidReason',
+	LENGTH_250
+);
