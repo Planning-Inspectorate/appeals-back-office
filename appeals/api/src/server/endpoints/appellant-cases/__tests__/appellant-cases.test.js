010caa7925ece3702a30beeabf287b5a0643a2b8
jimbali <jim.bailey.fl@planninginspectorate.gov.uk>
2026-02-04 11:28:24 +0000

    feat(api): case valid notify for enforcement (A2-6559)

--- appeals/api/src/server/endpoints/appellant-cases/__tests__/appellant-cases.test.js
+++ appeals/api/src/server/endpoints/appellant-cases/__tests__/appellant-cases.test.js
@@ -1050,6 +1050,78 @@ describe('appellant cases routes', () => {
 				}
 			);
 
+			test('updates appellant case and sends a notify email for valid enforcement notice appeal', async () => {
+				databaseConnector.appeal.findUnique.mockResolvedValue({
+					...enforcementNoticeAppeal,
+					appealType: {
+						key: 'C',
+						type: 'Enforcement Notice appeal'
+					},
+					appealGrounds: [
+						{
+							ground: {
+								groundRef: 'b',
+								groundDescription: 'Ground B'
+							}
+						},
+						{
+							ground: {
+								groundRef: 'a',
+								groundDescription: 'Ground A'
+							}
+						}
+					]
+				});
+				databaseConnector.appellantCaseValidationOutcome.findUnique.mockResolvedValue(
+					appellantCaseValidationOutcomes[2]
+				);
+				databaseConnector.user.upsert.mockResolvedValue({ id: 1, azureAdUserId });
+				databaseConnector.documentVersion.findMany.mockResolvedValue([]);
+				databaseConnector.documentVersion.update.mockResolvedValue([]);
+				databaseConnector.documentRedactionStatus.findMany.mockResolvedValue([
+					{ id: 1, key: 'no_redaction_required' }
+				]);
+				databaseConnector.document.findUnique.mockResolvedValue(null);
+
+				const patchBody = {
+					validationOutcome: 'valid',
+					groundABarred: false,
+					otherInformation: 'Accio horcrux'
+				};
+				const { appellantCase, id } = enforcementNoticeAppeal;
+				const response = await request
+					.patch(`/appeals/${id}/appellant-cases/${appellantCase.id}`)
+					.send(patchBody)
+					.set('azureAdUserId', azureAdUserId);
+
+				expect(databaseConnector.appellantCase.update).toHaveBeenCalledWith({
+					where: { id: appellantCase.id },
+					data: { appellantCaseValidationOutcomeId: 3 }
+				});
+
+				expect(response.status).toEqual(200);
+				expect(mockNotifySend).toHaveBeenCalledTimes(1);
+				expect(mockNotifySend).toHaveBeenNthCalledWith(1, {
+					azureAdUserId: '6f930ec9-7f6f-448c-bb50-b3b898035959',
+					notifyClient: expect.anything(),
+					personalisation: {
+						appeal_reference_number: enforcementNoticeAppeal.reference,
+						lpa_reference: enforcementNoticeAppeal.applicationReference,
+						site_address: `${enforcementNoticeAppeal.address.addressLine1}, ${enforcementNoticeAppeal.address.addressLine2}, ${enforcementNoticeAppeal.address.addressTown}, ${enforcementNoticeAppeal.address.addressCounty}, ${enforcementNoticeAppeal.address.postcode}, ${enforcementNoticeAppeal.address.addressCountry}`,
+						feedback_link: FEEDBACK_FORM_LINKS.ENFORCEMENT_NOTICE,
+						team_email_address: 'caseofficers@planninginspectorate.gov.uk',
+						local_planning_authority: enforcementNoticeAppeal.lpa.name,
+						appeal_type: 'Enforcement Notice',
+						enforcement_reference: enforcementNoticeAppeal.appellantCase.enforcementReference,
+						appeal_grounds: ['a', 'b'],
+						ground_a_barred: false,
+						other_info: 'Accio horcrux'
+					},
+					recipientEmail: enforcementNoticeAppeal.agent.email,
+					templateName: 'appeal-confirmed-enforcement'
+				});
+			});
+
 			test('updates appellant case site area and procedure preferences', async () => {
 				databaseConnector.appeal.findUnique.mockResolvedValue(householdAppeal);
 				databaseConnector.user.upsert.mockResolvedValue({
@@ -1417,7 +1489,7 @@ describe('appellant cases routes', () => {
 					}
 				});
 
-				expect(mockNotifySend).not.toHaveBeenCalled();
+				expect(mockNotifySend).toHaveBeenCalled();
 				expect(response.status).toEqual(200);
 			});
 
