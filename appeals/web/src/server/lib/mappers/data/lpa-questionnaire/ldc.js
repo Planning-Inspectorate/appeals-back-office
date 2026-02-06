16214e1f67418bbabe3833a8aa986793ef6b52e8
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2026-02-06 12:37:29 +0000

    feat(web): ldc lpaq part 2 (A2-6106)

--- appeals/web/src/server/lib/mappers/data/lpa-questionnaire/ldc.js
+++ appeals/web/src/server/lib/mappers/data/lpa-questionnaire/ldc.js
@@ -1,5 +1,15 @@
 import { submaps as hasSubmaps } from './has.js';
+import { mapCommunityInfrastructureLevy } from './submappers/map-community-infrastructure-levy.js';
+import { mapHasCommunityInfrastructureLevy } from './submappers/map-has-community-infrastructure-levy.js';
+import { mapInfrastructureLevyAdoptedDate } from './submappers/map-infrastructure-levy-adopted-date.js';
+import { mapInfrastructureLevyExpectedDate } from './submappers/map-infrastructure-levy-expected-date.js';
+import { mapIsInfrastructureLevyFormallyAdopted } from './submappers/map-is-infrastructure-levy-formally-adopted.js';
 
 export const submaps = {
-	...hasSubmaps
+	...hasSubmaps,
+	hasCommunityInfrastructureLevy: mapHasCommunityInfrastructureLevy,
+	communityInfrastructureLevy: mapCommunityInfrastructureLevy,
+	isInfrastructureLevyFormallyAdopted: mapIsInfrastructureLevyFormallyAdopted,
+	infrastructureLevyAdoptedDate: mapInfrastructureLevyAdoptedDate,
+	infrastructureLevyExpectedDate: mapInfrastructureLevyExpectedDate
 };
