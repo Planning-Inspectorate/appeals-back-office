62a2de005827dc19b5dc78224ea3c13c5722513c
lachlandow1 <lachlan.dow@solirius.com>
2026-02-11 14:26:29 +0000

    fix(api): map rule 6 party name to audit trail statement (A2-6797)

--- appeals/api/src/server/endpoints/integrations/__tests__/integrations.test.js
+++ appeals/api/src/server/endpoints/integrations/__tests__/integrations.test.js
@@ -943,6 +943,15 @@ describe('/appeals/representation-submission', () => {
 					expect(databaseConnector.documentVersion.createMany).toHaveBeenCalled();
 					expect(databaseConnector.documentVersion.findMany).toHaveBeenCalled();
 					expect(databaseConnector.representationAttachment.createMany).toHaveBeenCalled();
+
+					expect(databaseConnector.auditTrail.create).toHaveBeenCalledWith(
+						expect.objectContaining({
+							data: expect.objectContaining({
+								details: 'Test Organisation statement added'
+							})
+						})
+					);
+
 					expect(response.status).toEqual(201);
 				});
 
@@ -1315,6 +1324,8 @@ const createIntegrationMocks = (/** @type {*} */ appealIngestionInput) => {
 	databaseConnector.team.findFirst.mockResolvedValue({
 		id: 1
 	});
+	// @ts-ignore
+	databaseConnector.user.upsert.mockResolvedValue({ id: 1 });
 	// @ts-ignore
 	databaseConnector.lPA.findUnique.mockResolvedValue({
 		teamId: 1
