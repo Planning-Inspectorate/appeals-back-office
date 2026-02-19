8995511a69e0a13b038a39ff847fc07b90858f2b
gmmagruder <gabrielle.magruder@gmail.com>
2026-02-19 11:05:20 +0000

    feat(infrastructure): remove opt in system for e2e pr labels (A2-6998)

--- bin/check-pr-labels.sh
+++ bin/check-pr-labels.sh
@@ -1,14 +1,3 @@
-checkPrLabel() {
-	label="$1"
-	filePath="$2"
-	if grep -q $label $filePath; then
-		echo "Found '$label' label in PR"
-		echo "##vso[task.setvariable variable=runE2E]true"
-		echo "##vso[task.setvariable variable=$label]true"
-	fi
-}
-
-labels=("") # Add future opt in e2e labels here
 filePath="$1"
 
 if grep -q "optOutE2eSmoke" $filePath; then
@@ -18,7 +7,3 @@ else
 	echo "##vso[task.setvariable variable=runE2E]true"
   echo "##vso[task.setvariable variable=e2eSmoke]true"
 fi
-
-for label in "${labels[@]}"; do
-	checkPrLabel $label $filePath
-done
