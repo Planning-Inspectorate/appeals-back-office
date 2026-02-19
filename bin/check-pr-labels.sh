1bf9c49215d49422b13a85aa31f4dd49ed2dc20f
gmmagruder <gabrielle.magruder@gmail.com>
2026-02-19 10:55:19 +0000

    feat(infrastructure): make e2e smoke pr checks opt out (A2-6998)

--- bin/check-pr-labels.sh
+++ bin/check-pr-labels.sh
@@ -8,8 +8,16 @@ checkPrLabel() {
 	fi
 }
 
-labels=("e2eSmoke")
+labels=("") # Add future opt in e2e labels here
 filePath="$1"
+
+if grep -q "optOutE2eSmoke" $filePath; then
+	echo "Opt out running e2e smoke tests"
+else
+	echo "Run e2e smoke tests"
+	echo "##vso[task.setvariable variable=runE2E]true"
+  echo "##vso[task.setvariable variable=e2eSmoke]true"
+fi
 
 for label in "${labels[@]}"; do
 	checkPrLabel $label $filePath
