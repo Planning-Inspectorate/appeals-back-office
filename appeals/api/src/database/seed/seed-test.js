aa97846b9cabdc766a069dd7fbe9254357cf7ec8
gmmagruder <gabrielle.magruder@gmail.com>
2026-02-20 17:03:28 +0000

    feat(infrastructure): add new seed file for e2e tests (A2-7058)

---
+++ appeals/api/src/database/seed/seed-test.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { createPrismaClient } from '../create-client.js';
+import { localPlanningDepartmentList } from './LPAs/dev.js';
+import { padsInspectorList } from './PADs/dev.js';
+import { seedLPAs } from './seed-lpas.js';
+import { seedPADSInspectors } from './seed-pads-inspectors.js';
+import { seedTeams } from './seed-teams.js';
+import { deleteExistingData } from './seed-truncate.js';
+import { teamsToCreate } from './teams/dev.js';
+
+/**
+ * Clear the test database, then add in the minimum test data
+ *
+ * @throws {Error} If any database operation fails.
+ * @returns {Promise<void>}
+ */
+const seedTest = async () => {
+	const databaseConnector = createPrismaClient();
+	try {
+		await deleteExistingData(databaseConnector);
+		await seedTeams(databaseConnector, teamsToCreate);
+		await seedLPAs(databaseConnector, localPlanningDepartmentList);
+		await seedPADSInspectors(databaseConnector, padsInspectorList);
+	} catch (error) {
+		console.error(error);
+		throw error;
+	} finally {
+		await databaseConnector.$disconnect();
+	}
+};
+
+await seedTest();
