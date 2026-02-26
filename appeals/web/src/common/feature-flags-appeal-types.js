4877af0e6cb1ff896218b671757dc58753b6aa80
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2026-02-26 09:46:11 +0000

    feat(appeals): s20 - change procedure (A2-6944)

--- appeals/web/src/common/feature-flags-appeal-types.js
+++ appeals/web/src/common/feature-flags-appeal-types.js
@@ -37,9 +37,6 @@ export const isAppealCaseTypeEnabled = (appealCaseType) => {
 		case APPEAL_CASE_TYPE.C: {
 			return isFeatureActive(FEATURE_FLAG_NAMES.ENFORCEMENT_NOTICE);
 		}
-		case APPEAL_CASE_TYPE.F: {
-			return true;
-		}
 		case APPEAL_CASE_TYPE.X: {
 			return isFeatureActive(FEATURE_FLAG_NAMES.LDC);
 		}
