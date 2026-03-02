1fe5863b7a51e40bc072068179a6a11b262fe68f
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2026-03-02 12:27:15 +0000

    fix(appeals): ldc lpaq reasons appeal invalid display (A2-7229)

--- appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/is-appeal-invalid/is-appeal-invalid.service.js
+++ appeals/web/src/server/appeals/appeal-details/lpa-questionnaire/is-appeal-invalid/is-appeal-invalid.service.js
@@ -4,13 +4,23 @@ import { convertFromYesNoToBoolean } from '#lib/boolean-formatter.js';
  * @param {import('got').Got} apiClient
  * @param {string} appealId
  * @param {string} lpaQuestionnaireId
- * @param {string} updatedData
+ * @param {{radio: string, details: string|null}} updatedData
  * @returns {Promise<{}>}
  */
 export function changeIsAppealInvalid(apiClient, appealId, lpaQuestionnaireId, updatedData) {
+	/** @type {string|null} */
+	const lpaConsiderAppealInvalid = updatedData.radio;
+	/** @type {string|null} */
+	let lpaAppealInvalidReasons = updatedData.details;
+
+	if (!convertFromYesNoToBoolean(lpaConsiderAppealInvalid)) {
+		lpaAppealInvalidReasons = null;
+	}
+
 	return apiClient.patch(`appeals/${appealId}/lpa-questionnaires/${lpaQuestionnaireId}`, {
 		json: {
-			lpaConsiderAppealInvalid: convertFromYesNoToBoolean(updatedData)
+			lpaConsiderAppealInvalid: convertFromYesNoToBoolean(lpaConsiderAppealInvalid),
+			lpaAppealInvalidReasons
 		}
 	});
 }
