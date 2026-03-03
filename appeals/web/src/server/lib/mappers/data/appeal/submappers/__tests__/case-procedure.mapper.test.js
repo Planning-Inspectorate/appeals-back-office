eeb020319841e7389e33817ae052a113e06850e3
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2026-03-03 15:04:12 +0000

    feat(appeals): enforcement - change procedure - Feature flags (A2-6945)

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/__tests__/case-procedure.mapper.test.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/__tests__/case-procedure.mapper.test.js
@@ -1,14 +1,23 @@
 // @ts-nocheck
 import { mapCaseProcedure } from '#lib/mappers/data/appeal/submappers/case-procedure.mapper.js';
 import { jest } from '@jest/globals';
+import config from '@pins/appeals.web/environment/config.js';
 import { APPEAL_TYPE } from '@pins/appeals/constants/common.js';
 import { DOCUMENT_STATUS_RECEIVED } from '@pins/appeals/constants/support.js';
 import { APPEAL_CASE_PROCEDURE } from '@planning-inspectorate/data-model';
 
 describe('case-procedure.mapper', () => {
 	let params;
+	const originalFeatureFlags = { ...config.featureFlags };
 
 	beforeEach(() => {
+		Object.assign(config.featureFlags, {
+			featureFlagEnforcementNotice: true,
+			featureFlagS78Written: true,
+			featureFlagEnforcementNoticeHearing: true,
+			featureFlagEnforcementNoticeInquiry: true
+		});
+
 		params = {
 			appealDetails: {
 				startedAt: new Date('2025-01-01'),
@@ -27,6 +36,7 @@ describe('case-procedure.mapper', () => {
 		};
 	});
 	afterEach(() => {
+		Object.assign(config.featureFlags, originalFeatureFlags);
 		jest.clearAllMocks();
 	});
 
@@ -65,6 +75,35 @@ describe('case-procedure.mapper', () => {
 			},
 			id: 'case-procedure'
 		});
+	});
+
+	it('Should display change link for enforcement notice appeal type when editable', () => {
+		params.appealDetails.appealType = APPEAL_TYPE.ENFORCEMENT_NOTICE;
+
+		const result = mapCaseProcedure(params);
+
+		expect(result?.display?.summaryListItem?.actions?.items).toEqual([
+			{
+				attributes: {
+					'data-cy': 'change-case-procedure'
+				},
+				href: '/appeal-questionnaire/change-appeal-procedure-type/change-selected-procedure-type',
+				text: 'Change',
+				visuallyHiddenText: 'Appeal procedure'
+			}
+		]);
+	});
+
+	it('Should not display change link for enforcement notice appeal type when enforcement hearing and inquiry flags are off', () => {
+		Object.assign(config.featureFlags, {
+			featureFlagEnforcementNoticeHearing: false,
+			featureFlagEnforcementNoticeInquiry: false
+		});
+		params.appealDetails.appealType = APPEAL_TYPE.ENFORCEMENT_NOTICE;
+
+		const result = mapCaseProcedure(params);
+
+		expect(result?.display?.summaryListItem?.actions?.items).toEqual([]);
 	});
 
 	it('Should not display change link if linked child appeal', () => {
