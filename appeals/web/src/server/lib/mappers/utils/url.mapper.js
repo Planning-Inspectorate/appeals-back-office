acad8cf81742afb64c8d37136517257c0b91e3e3
gmmagruder <gabrielle.magruder@gmail.com>
2026-03-19 14:57:14 +0000

    fix(web): back link from manage documents to share poe journey (A2-7297)

--- appeals/web/src/server/lib/mappers/utils/url.mapper.js
+++ appeals/web/src/server/lib/mappers/utils/url.mapper.js
@@ -18,10 +18,9 @@ export const constructUrl = (url = '/', appealId = undefined) => {
 		constructedUrl = urlMap[url];
 	} else if (appealId) {
 		// if not found in map and appealId exists return to appeal details subroute (or just appeal details page in case of "/")
-		constructedUrl =
-			url === `/appeals-service/appeal-details/${appealId}`
-				? url
-				: `/appeals-service/appeal-details/${appealId}${url}`;
+		constructedUrl = url.includes(`/appeals-service/appeal-details/${appealId}`)
+			? url
+			: `/appeals-service/appeal-details/${appealId}${url}`;
 	} else {
 		// if not found in map and appealId doesn't exist fall back to all cases page
 		constructedUrl = '/appeals-service/all-cases';
