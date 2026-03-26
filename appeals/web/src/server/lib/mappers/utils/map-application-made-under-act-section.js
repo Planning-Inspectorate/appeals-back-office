0e35adfb5e1e0b6215091ec9962a7bb1cbfff1e5
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2026-03-26 11:05:55 +0000

    feat(web): add detail to ldc certificate type (A2-7563)

---
+++ appeals/web/src/server/lib/mappers/utils/map-application-made-under-act-section.js
@@ -0,0 +1,16 @@
+import { APPEAL_APPEAL_UNDER_ACT_SECTION } from '@planning-inspectorate/data-model';
+
+/**
+ * @param {string | null | undefined} appealUnderActSection
+ * @returns {string | null | undefined}
+ */
+export const formatAppealUnderActSection = (appealUnderActSection) => {
+	switch (appealUnderActSection) {
+		case APPEAL_APPEAL_UNDER_ACT_SECTION.EXISTING_DEVELOPMENT:
+			return 'Existing development (section 191)';
+		case APPEAL_APPEAL_UNDER_ACT_SECTION.PROPOSED_CHANGES_TO_A_LISTED_BUILDING:
+			return 'Proposed changes to a listed building (section 192)';
+		case APPEAL_APPEAL_UNDER_ACT_SECTION.PROPOSED_USE_OF_A_DEVELOPMENT:
+			return 'Proposed use of a development (section 26H)';
+	}
+};
