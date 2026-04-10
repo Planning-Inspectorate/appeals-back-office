3668be7378345ed457f18e7bdf116bba47c33c60
lachlandow1 <lachlan.dow@solirius.com>
2026-04-10 15:51:01 +0100

    feat(appeals): data mapping update for expedited appeals (A2-7765)

--- appeals/api/src/server/mappers/integration/shared/s20s78/appellant-case-fields.js
+++ appeals/api/src/server/mappers/integration/shared/s20s78/appellant-case-fields.js
@@ -1,25 +1,30 @@
 /** @typedef {import('@planning-inspectorate/data-model').Schemas.LPAQuestionnaireCommand} LPAQuestionnaireCommand */
 /** @typedef {import('@planning-inspectorate/data-model').Schemas.AppellantSubmissionCommand} AppellantSubmissionCommand */
 
+const SIGNIFICANT_CHANGE_VALUE_MAP = {
+	'Local plan': 'adopted-a-new-local-plan',
+	'National policy': 'national-policy-change',
+	'Court judgment': 'court-judgement',
+	Other: 'other'
+};
+
 /**
  *
- * @param {string[]|undefined} changes
- * @param {string} type
+ * @param {{value: string, comment: string}[]|undefined} changes
+ * @param {keyof SIGNIFICANT_CHANGE_VALUE_MAP} type
  * @returns {string|null}
  */
 const extractSignificantChangeValue = (changes, type) => {
-	const change = changes?.find((s) => s.startsWith(`${type}:`));
-	if (change) {
-		return change.replace(`${type}:`, '').trim();
-	}
-	return null;
+	const mappedValue = SIGNIFICANT_CHANGE_VALUE_MAP[type];
+	const change = changes?.find((c) => c.value === mappedValue);
+	return change?.comment ?? null;
 };
 
 /**
  * @param {Pick<AppellantSubmissionCommand, 'casedata'>} command
  */
 export const createSharedS20S78Fields = (command) => {
-	const significantChanges = /** @type {string[] | undefined} */ (
+	const significantChanges = /** @type {{value: string, comment: string}[] | undefined} */ (
 		command.casedata.significantChangesAffectingApplicationAppellant
 	);
 	return {
