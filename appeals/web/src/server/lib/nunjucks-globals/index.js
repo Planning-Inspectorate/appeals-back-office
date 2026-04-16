f16b0c4a310129e57a498360bb058fd458307ab3
Tom Pickering <tom.pickering@solirius.com>
2026-04-16 10:28:09 +0100

    fix(web): add googleAnalyticsId njk global (A2-7537)

--- appeals/web/src/server/lib/nunjucks-globals/index.js
+++ appeals/web/src/server/lib/nunjucks-globals/index.js
@@ -1,4 +1,5 @@
 import config from '#environment/config.js';
 
 export const betaFeedbackUrl = config.betaFeedbackUrl;
+export const googleAnalyticsId = config.googleAnalyticsId;
 export { isArray, isUndefined } from 'lodash-es';
