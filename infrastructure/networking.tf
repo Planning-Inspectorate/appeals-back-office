33874bfe92f4b08502f5f448e6d181db6220ac2f
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-04-30 12:07:04 +0100

    feat(infrastructure): create private endpoint for key vault (no-ticket)

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -211,3 +211,14 @@ resource "azurerm_private_dns_zone_virtual_network_link" "synapse" {
 
   provider = azurerm.tooling
 }
+
+resource "azurerm_private_dns_zone_virtual_network_link" "keyvault" {
+  name                  = "${local.org}-vnetlink-keyvault-${local.resource_suffix}"
+  resource_group_name   = var.tooling_config.network_rg
+  private_dns_zone_name = data.azurerm_private_dns_zone.keyvault.name
+  virtual_network_id    = azurerm_virtual_network.main.id
+
+  tags = local.tags
+
+  provider = azurerm.tooling
+}
