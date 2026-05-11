3cf65b34b84753cdb9848bc1b2431aeaf7418fbe
musabadam-pins <musab.adam.7c@planninginspectorate.gov.uk>
2026-05-11 12:15:54 +0100

    feat(web): added s78 Expedite timetable changes (A2-7769)

--- appeals/web/src/server/lib/mappers/data/appeal/submappers/ip-comments-due-date.mapper.js
+++ appeals/web/src/server/lib/mappers/data/appeal/submappers/ip-comments-due-date.mapper.js
@@ -2,17 +2,27 @@ import { isStatePassed } from '#lib/appeal-status.js';
 import { dateISOStringToDisplayDate } from '#lib/dates.js';
 import { textSummaryListItem } from '#lib/mappers/index.js';
 import { isChildAppeal } from '#lib/mappers/utils/is-linked-appeal.js';
+import { isS78ExpeditedAppealType } from '@pins/appeals/utils/appeal-type-checks.js';
 import { APPEAL_CASE_STATUS } from '@planning-inspectorate/data-model';
 
 /** @type {import('../mapper.js').SubMapper} */
 export const mapIpCommentsDueDate = ({
 	appealDetails,
 	currentRoute,
-	userHasUpdateCasePermission
+	userHasUpdateCasePermission,
+	appellantCase
 }) => {
 	const id = 'ip-comments-due-date';
 
-	if (!appealDetails.startedAt) {
+	if (
+		!appealDetails.startedAt ||
+		isS78ExpeditedAppealType(
+			appealDetails.appealType,
+			appellantCase?.applicationDate,
+			appellantCase?.applicationDecision,
+			appellantCase?.typeOfPlanningApplication
+		)
+	) {
 		return { id, display: {} };
 	}
 
