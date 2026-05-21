4b0a6cb64f046a378fc112368673283ca783e7b5
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-05-21 08:26:27 +0100

    fix(infrastructure): adding resolution_policy for blob private dns zone vnet link (no-ticket)

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -228,6 +228,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "storage" {
   resource_group_name   = var.tooling_config.network_rg
   private_dns_zone_name = data.azurerm_private_dns_zone.storage.name
   virtual_network_id    = azurerm_virtual_network.main.id
+  resolution_policy     = "NxDomainRedirect"
 
   provider = azurerm.tooling
 }
