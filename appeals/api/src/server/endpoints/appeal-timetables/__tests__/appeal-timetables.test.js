3380940d4750f5ae4ae6bc6c97b88dd2076ea844
TurnerMichael <michael.turner@planninginspectorate.gov.uk>
2026-06-03 14:41:32 +0100

    fix(web): removed incorrect snapshot update (a2-8409)

--- appeals/api/src/server/endpoints/appeal-timetables/__tests__/appeal-timetables.test.js
+++ appeals/api/src/server/endpoints/appeal-timetables/__tests__/appeal-timetables.test.js
@@ -1296,7 +1296,7 @@ describe('appeal timetables routes', () => {
 				expect(mockNotifySend).toHaveBeenNthCalledWith(
 					1,
 					expect.objectContaining({
-						templateName: 'appeal-valid-start-case-s78-appellant',
+						templateName: 'appeal-valid-start-case-s78-expedited-appellant',
 						personalisation: expect.objectContaining({
 							procedure_type: 'written representations',
 							site_visit: true,
@@ -1307,7 +1307,7 @@ describe('appeal timetables routes', () => {
 				expect(mockNotifySend).toHaveBeenNthCalledWith(
 					2,
 					expect.objectContaining({
-						templateName: 'appeal-valid-start-case-s78-lpa',
+						templateName: 'appeal-valid-start-case-s78-expedited-lpa',
 						personalisation: expect.objectContaining({
 							procedure_type: 'written representations'
 						})
@@ -1330,8 +1330,8 @@ describe('appeal timetables routes', () => {
 					{ statement_of_common_ground_deadline: '10 July 2024', planning_obligation_deadline: '' },
 					'appeal-valid-start-case-s78-appellant',
 					'appeal-valid-start-case-s78-lpa',
-					'appeal-valid-start-case-s78-appellant-hearing',
-					'appeal-valid-start-case-s78-lpa-hearing'
+					'appeal-valid-start-case-s78-hearing-appellant',
+					'appeal-valid-start-case-s78-hearing-lpa'
 				],
 				[
 					'listedBuilding',
@@ -1347,8 +1347,8 @@ describe('appeal timetables routes', () => {
 					{},
 					'appeal-valid-start-case-s78-appellant',
 					'appeal-valid-start-case-s78-lpa',
-					'appeal-valid-start-case-s78-appellant-hearing',
-					'appeal-valid-start-case-s78-lpa-hearing'
+					'appeal-valid-start-case-s78-hearing-appellant',
+					'appeal-valid-start-case-s78-hearing-lpa'
 				],
 				[
 					'advertisementAppeal',
@@ -1364,8 +1364,8 @@ describe('appeal timetables routes', () => {
 					{},
 					'appeal-valid-start-case-advertisement-appellant',
 					'appeal-valid-start-case-advertisement-lpa',
-					'appeal-valid-start-case-advertisement-appellant-hearing',
-					'appeal-valid-start-case-advertisement-lpa-hearing'
+					'appeal-valid-start-case-advertisement-hearing-appellant',
+					'appeal-valid-start-case-advertisement-hearing-lpa'
 				],
 				[
 					'ldcAppeal',
@@ -1381,8 +1381,8 @@ describe('appeal timetables routes', () => {
 					{},
 					'appeal-valid-start-case-advertisement-appellant',
 					'appeal-valid-start-case-advertisement-lpa',
-					'appeal-valid-start-case-advertisement-appellant-hearing',
-					'appeal-valid-start-case-advertisement-lpa-hearing'
+					'appeal-valid-start-case-advertisement-hearing-appellant',
+					'appeal-valid-start-case-advertisement-hearing-lpa'
 				]
 			])(
 				'for a %s appeal',
