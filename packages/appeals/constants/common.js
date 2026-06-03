897ee28b5b931fdf4bae0618efcb329e842cb524
TurnerMichael <michael.turner@planninginspectorate.gov.uk>
2026-06-03 14:41:32 +0100

    fix(web): removed incorrect snapshot update (a2-8409)

--- packages/appeals/constants/common.js
+++ packages/appeals/constants/common.js
@@ -86,6 +86,13 @@ export const PROCEDURE_TYPE_MAP = Object.freeze({
 	inquiry: 'an inquiry'
 });
 
+export const PROCEDURE_TYPE_KEY = Object.freeze({
+	WRITTEN_PART_2: 'written',
+	WRITTEN_PART_1: 'writtenPart1',
+	HEARING: 'hearing',
+	INQUIRY: 'inquiry'
+});
+
 export const PROCEDURE_TYPE_NAME = Object.freeze({
 	WRITTEN_PART_2: 'Written',
 	WRITTEN_PART_1: 'Part 1',
