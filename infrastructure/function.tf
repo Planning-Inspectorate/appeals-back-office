827f37ef19994181dff4c399eff290e474b01d82
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-06-08 10:00:57 +0100

    feat(infrastructure): function storage have private endpoints on (no-ticket)

--- infrastructure/function.tf
+++ infrastructure/function.tf
@@ -12,22 +12,15 @@ resource "azurerm_service_plan" "functions" {
 }
 
 resource "azurerm_storage_account" "functions" {
-  #TODO: Customer Managed Keys
-  #checkov:skip=CKV2_AZURE_1: Customer Managed Keys not implemented yet
-  #checkov:skip=CKV2_AZURE_18: Customer Managed Keys not implemented yet
-  #TODO: Logging
   #checkov:skip=CKV_AZURE_33: Logging not implemented yet
-  #checkov:skip=CKV2_AZURE_8: Logging not implemented yet
-  #TODO: Access restrictions
-  #checkov:skip=CKV_AZURE_35: Network access restrictions
   #checkov:skip=CKV_AZURE_43: "Ensure Storage Accounts adhere to the naming rules"
-  #checkov:skip=CKV_AZURE_59: TODO: Ensure that Storage accounts disallow public access
-  #checkov:skip=CKV_AZURE_206: TODO: Ensure that Storage Accounts use replication
-  #checkov:skip=CKV2_AZURE_33: "Ensure storage account is configured with private endpoint"
+  #checkov:skip=CKV_AZURE_206: "Ensure that Storage Accounts use replication"
+  #checkov:skip=CKV2_AZURE_1: Customer Managed Keys not implemented yet
+  #checkov:skip=CKV2_AZURE_8: Logging not implemented yet
+  #checkov:skip=CKV2_AZURE_18: Customer Managed Keys not implemented yet
   #checkov:skip=CKV2_AZURE_38: "Ensure soft-delete is enabled on Azure storage account"
   #checkov:skip=CKV2_AZURE_40: "Ensure storage account is not configured with Shared Key authorization"
   #checkov:skip=CKV2_AZURE_41: "Ensure storage account is configured with SAS expiration policy"
-
   name                             = "pinsstfuncappealsbo${local.environment}" # local will shorten training to train so storage account name length is =< 24 chars
   resource_group_name              = azurerm_resource_group.primary.name
   location                         = module.primary_region.location
@@ -37,6 +30,33 @@ resource "azurerm_storage_account" "functions" {
   cross_tenant_replication_enabled = false
   https_traffic_only_enabled       = true
   min_tls_version                  = "TLS1_2"
+  public_network_access_enabled    = false
+
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "functions_storage" {
+  name                = "${local.org}-pe-st-funcstorage-${local.resource_suffix}"
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_dns_zone_group {
+    name                 = "${local.org}-pdns-${local.service_name}-funcstorage-${var.environment}"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.storage.id]
+  }
+
+  private_service_connection {
+    name                           = "${local.org}-psc-funcstorage-${local.resource_suffix}"
+    private_connection_resource_id = azurerm_storage_account.functions.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
 
   tags = local.tags
 }
