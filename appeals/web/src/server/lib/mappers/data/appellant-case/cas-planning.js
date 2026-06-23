d925b49ed118055cf53c61fb535add1ef9983e3a
TurnerMichael <michael.turner@planninginspectorate.gov.uk>
2026-06-23 10:49:44 +0100

    fix(web): removed mapper from data chain and removed mapping command (a2-8458)

--- appeals/web/src/server/lib/mappers/data/appellant-case/cas-planning.js
+++ appeals/web/src/server/lib/mappers/data/appellant-case/cas-planning.js
@@ -1,9 +1,7 @@
 import { submaps as hasSubmaps } from './has.js';
-import { mapDesignAccessStatement } from './submappers/design-access-statement.js';
 import { mapSupportingDocuments } from './submappers/supporting-documents.js';
 
 export const submaps = {
 	...hasSubmaps,
-	supportingDocuments: mapSupportingDocuments,
-	designAndAccessStatement: mapDesignAccessStatement
+	supportingDocuments: mapSupportingDocuments
 };
