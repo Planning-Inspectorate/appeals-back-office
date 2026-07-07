32cb7db6695e6e4c2032268929472e7f0fb6f363
David White <david.white@planninginspectorate.gov.uk>
2026-07-07 13:40:43 +0100

    fix(api): fixes incorrect merge conflict in timetable service (A2-6158)

--- appeals/api/src/server/endpoints/appeal-timetables/appeal-timetables.service.js
+++ appeals/api/src/server/endpoints/appeal-timetables/appeal-timetables.service.js
@@ -655,7 +655,7 @@ const updateAppealTimetable = async (
 	);
 
 	if (result) {
-		await setPersonalList(appeal.id);
+		await setPersonalList({ appealId: appeal.id });
 
 		if (!isChildAppeal) {
 			let details = 'Timetable updated:';
