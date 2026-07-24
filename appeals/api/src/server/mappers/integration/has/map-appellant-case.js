d5f2d60d75f95f1a97e0f2a84c588071f45754b6
TurnerMichael <michael.turner@planninginspectorate.gov.uk>
2026-07-24 17:31:01 +0100

    feat(appeals): corrected has lpaq and appellant case values (a2-8699)

--- appeals/api/src/server/mappers/integration/has/map-appellant-case.js
+++ appeals/api/src/server/mappers/integration/has/map-appellant-case.js
@@ -2,8 +2,6 @@
 /** @typedef {import('@planning-inspectorate/data-model').Schemas.AppealS78Case} AppealS78Case */
 /** @typedef {import('#mappers/mapper-factory.js').MappingRequest} MappingRequest */
 
-import { mapAppellantCaseSharedFields } from '#mappers/integration/shared/s20s78/map-appellant-case.js';
-
 /**
  *
  * @param {MappingRequest} data
@@ -14,7 +12,51 @@ export const mapAppellantCase = (data) => {
 	const casedata = appeal.appellantCase;
 
 	return {
-		...mapAppellantCaseSharedFields(data),
-		hasLandownersPermission: casedata?.landownerPermission ?? null
+		// @ts-ignore
+		applicationDecision: casedata?.applicationDecision ?? null,
+		siteAreaSquareMetres: casedata?.siteAreaSquareMetres
+			? Number(casedata.siteAreaSquareMetres)
+			: null,
+		floorSpaceSquareMetres: casedata?.floorSpaceSquareMetres
+			? Number(casedata.floorSpaceSquareMetres)
+			: null,
+		ownsAllLand: casedata?.ownsAllLand ?? null,
+		ownsSomeLand: casedata?.ownsSomeLand ?? null,
+		advertisedAppeal: casedata?.hasAdvertisedAppeal ?? null,
+		appellantCostsAppliedFor: casedata?.appellantCostsAppliedFor ?? null,
+		originalDevelopmentDescription: casedata?.originalDevelopmentDescription ?? null,
+		changedDevelopmentDescription: casedata?.changedDevelopmentDescription ?? null,
+		// @ts-ignore
+		knowsAllOwners: casedata?.knowsAllOwners?.name ?? null,
+		// @ts-ignore
+		knowsOtherOwners: casedata?.knowsOtherOwners?.name ?? null,
+		ownersInformed: casedata?.ownersInformed ?? null,
+		enforcementNotice: casedata?.enforcementNotice ?? null,
+		isGreenBelt: casedata?.isGreenBelt ?? null,
+		// @ts-ignore
+		typeOfPlanningApplication: casedata?.typeOfPlanningApplication ?? null,
+		reasonForAppealAppellant: casedata?.reasonForAppealAppellant ?? null,
+		/** @type {any[]} */
+		significantChangesAffectingApplicationAppellant:
+			casedata?.anySignificantChanges === 'Yes'
+				? [
+						{
+							value: 'adopted-a-new-local-plan',
+							comment: casedata?.anySignificantChanges_localPlanSignificantChanges ?? null
+						},
+						{
+							value: 'national-policy-change',
+							comment: casedata?.anySignificantChanges_nationalPolicySignificantChanges ?? null
+						},
+						{
+							value: 'court-judgement',
+							comment: casedata?.anySignificantChanges_courtJudgementSignificantChanges ?? null
+						},
+						{
+							value: 'other',
+							comment: casedata?.anySignificantChanges_otherSignificantChanges ?? null
+						}
+					].filter((c) => c.comment !== null)
+				: []
 	};
 };
