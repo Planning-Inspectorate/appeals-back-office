232ef6b1bf0e13b2252d6d8d6fa11ee21eec58b5
Sam Laskowski <sam.laskowski@outlook.com>
2026-09-16 14:15:12 +0100

    fix(api): hearing, inq doc stage returns null and inviteResponses fix (A2-8535)

--- appeals/web/src/server/appeals/appeal-details/inquiry/documents/inquiry-documents.controller.js
+++ appeals/web/src/server/appeals/appeal-details/inquiry/documents/inquiry-documents.controller.js
@@ -404,7 +404,7 @@ export const postShareDocumentCheckAndConfirm = async (request, response) => {
 				id: documentId,
 				isShared: true
 			},
-			inviteMainPartyComments: request.session?.inviteMainPartyComments === 'yes',
+			inviteResponses: request.session?.inviteMainPartyComments === 'yes',
 			sharingDocumentType: `inquiry/document`
 		};
 
